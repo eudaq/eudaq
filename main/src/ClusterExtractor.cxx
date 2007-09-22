48cf1d63edeebdd23ffb3728f6e0fc231e050b32
Emlyn Corrin <emlyn.corrin@gmail.com>
2007-09-22 00:22:48 +0000

    ClusterExtractor won't die on bad data size

--- main/src/ClusterExtractor.cxx
+++ main/src/ClusterExtractor.cxx
@@ -75,67 +75,54 @@ int main(int /*argc*/, char ** argv) {
         } else if (ev->IsEORE()) {
           std::cout << "Found EORE" << std::endl;
         } else {
-
-          unsigned boardnum = 0;
-          if (dev->GetEventNumber() % 100 == 0) {
-            std::cout << "Event " << dev->GetEventNumber() << std::endl;
-          }
-          for (size_t i = 0; i < dev->NumEvents(); ++i) {
-            eudaq::Event * subev = dev->GetEvent(i);
-            eudaq::EUDRBEvent * eudev = dynamic_cast<eudaq::EUDRBEvent*>(subev);
-            if (eudev) {
-              if (!decoder.get()) EUDAQ_THROW("Missing EUDRB BORE, cannot decode events");
-              for (size_t j = 0; j < eudev->NumBoards(); ++j) {
-                eudaq::EUDRBBoard & brd = eudev->GetBoard(j);
-                //std::cout << "boardnum " << boardnum << ", id " << brd.GetID() << std::endl;
-                eudaq::EUDRBDecoder::arrays_t<short, short> a = decoder->GetArrays<short, short>(brd);
-                const size_t npixels = 256*256;
-                std::vector<short> cds(npixels);
-                if (decoder->NumFrames(brd) > 1) {
-                  for (size_t i = 0; i < cds.size(); ++i) {
-                    short x = delmarker(a.m_x[i]);
-                    if (x < 0) continue;
-                    size_t idx = x + 256*a.m_y[i];
-                    cds[idx] = a.m_adc[0][i] * (a.m_pivot[i])
-                             + a.m_adc[1][i] * (1-2*a.m_pivot[i])
-                             + a.m_adc[2][i] * (a.m_pivot[i]-1);
-                  }
-                } else {
-                  for (size_t i = 0; i < a.m_adc.size(); ++i) {
-                    short x = delmarker(a.m_x[i]);
-                    if (x < 0) continue;
-                    size_t idx = x + 256*a.m_y[i];
-                    cds[idx] = a.m_adc[0][i];
-                  }
-                }
-                std::vector<Seed> seeds;
-                size_t i = 0;
-                for (short y = 0; y < 256; ++y) {
-                  for (short x = 0; x < 256; ++x) {
-                    if (cds[i] > noise.Value() * thresh_seed.Value()) {
-                      seeds.push_back(Seed(x, y, cds[i]));
+          try {
+            unsigned boardnum = 0;
+            if (dev->GetEventNumber() % 100 == 0) {
+              std::cout << "Event " << dev->GetEventNumber() << std::endl;
+            }
+            for (size_t i = 0; i < dev->NumEvents(); ++i) {
+              eudaq::Event * subev = dev->GetEvent(i);
+              eudaq::EUDRBEvent * eudev = dynamic_cast<eudaq::EUDRBEvent*>(subev);
+              if (eudev) {
+                if (!decoder.get()) EUDAQ_THROW("Missing EUDRB BORE, cannot decode events");
+                for (size_t j = 0; j < eudev->NumBoards(); ++j) {
+                  eudaq::EUDRBBoard & brd = eudev->GetBoard(j);
+                  //std::cout << "boardnum " << boardnum << ", id " << brd.GetID() << std::endl;
+                  eudaq::EUDRBDecoder::arrays_t<short, short> a = decoder->GetArrays<short, short>(brd);
+                  const size_t npixels = 256*256;
+                  std::vector<short> cds(npixels);
+                  if (decoder->NumFrames(brd) > 1) {
+                    for (size_t i = 0; i < cds.size(); ++i) {
+                      short x = delmarker(a.m_x[i]);
+                      if (x < 0) continue;
+                      size_t idx = x + 256*a.m_y[i];
+                      cds[idx] = a.m_adc[0][i] * (a.m_pivot[i])
+                        + a.m_adc[1][i] * (1-2*a.m_pivot[i])
+                        + a.m_adc[2][i] * (a.m_pivot[i]-1);
                     }
-                    ++i;
-                  }
-                }
-                std::sort(seeds.begin(), seeds.end(), &Seed::compare);
-                std::vector<std::pair<short, short> > clusters;
-                for (size_t i = 0; i < seeds.size(); ++i) {
-                  int charge = 0;
-                  bool badseed = false;
-                  for (int dy = -1; dy < 2; ++dy) {
-                    int y = seeds[i].m_y + dy;
-                    if (y < 0 || y >= 256) continue;
-                    for (int dx = -1; dx < 2; ++dx) {
-                      int x = seeds[i].m_x + dx;
-                      if (x < 0 || x >= 256) continue;
-                      size_t idx = 256 * y + x;
-                      if (cds[idx] == BADPIX) badseed = true;
-                      charge += cds[idx];
+                  } else {
+                    for (size_t i = 0; i < a.m_adc.size(); ++i) {
+                      short x = delmarker(a.m_x[i]);
+                      if (x < 0) continue;
+                      size_t idx = x + 256*a.m_y[i];
+                      cds[idx] = a.m_adc[0][i];
                     }
                   }
-                  if (!badseed) {
-                    clusters.push_back(std::make_pair(seeds[i].m_x, seeds[i].m_y));
+                  std::vector<Seed> seeds;
+                  size_t i = 0;
+                  for (short y = 0; y < 256; ++y) {
+                    for (short x = 0; x < 256; ++x) {
+                      if (cds[i] > noise.Value() * thresh_seed.Value()) {
+                        seeds.push_back(Seed(x, y, cds[i]));
+                      }
+                      ++i;
+                    }
+                  }
+                  std::sort(seeds.begin(), seeds.end(), &Seed::compare);
+                  std::vector<std::pair<short, short> > clusters;
+                  for (size_t i = 0; i < seeds.size(); ++i) {
+                    int charge = 0;
+                    bool badseed = false;
                     for (int dy = -1; dy < 2; ++dy) {
                       int y = seeds[i].m_y + dy;
                       if (y < 0 || y >= 256) continue;
@@ -143,22 +130,37 @@ int main(int /*argc*/, char ** argv) {
                         int x = seeds[i].m_x + dx;
                         if (x < 0 || x >= 256) continue;
                         size_t idx = 256 * y + x;
-                        cds[idx] = BADPIX;
+                        if (cds[idx] == BADPIX) badseed = true;
+                        charge += cds[idx];
+                      }
+                    }
+                    if (!badseed) {
+                      clusters.push_back(std::make_pair(seeds[i].m_x, seeds[i].m_y));
+                      for (int dy = -1; dy < 2; ++dy) {
+                        int y = seeds[i].m_y + dy;
+                        if (y < 0 || y >= 256) continue;
+                        for (int dx = -1; dx < 2; ++dx) {
+                          int x = seeds[i].m_x + dx;
+                          if (x < 0 || x >= 256) continue;
+                          size_t idx = 256 * y + x;
+                          cds[idx] = BADPIX;
+                        }
                       }
                     }
                   }
-                }
-                if (clusters.size()) {
-                  *files[brd.GetID()] << dev->GetEventNumber() << "\t" << clusters.size() << "\n";
-                  for (size_t i = 0; i < clusters.size(); ++i) {
-                    *files[brd.GetID()] << " " << clusters[i].first << "\t" << clusters[i].second << "\n";
+                  if (clusters.size()) {
+                    *files[brd.GetID()] << dev->GetEventNumber() << "\t" << clusters.size() << "\n";
+                    for (size_t i = 0; i < clusters.size(); ++i) {
+                      *files[brd.GetID()] << " " << clusters[i].first << "\t" << clusters[i].second << "\n";
+                    }
                   }
+                  boardnum++;
                 }
-                boardnum++;
               }
             }
+          } catch (const eudaq::Exception & e) {
+            std::cerr << "Exception: " << e.what() << std::endl;
           }
-
         }
       }
     }
