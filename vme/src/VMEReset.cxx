48795e06c9bd00f827c67e38b89b0a86d7cdaf3c
Emlyn Corrin <emlyn.corrin@gmail.com>
2008-02-25 14:29:28 +0000

    Added VMEReset utility

---
+++ vme/src/VMEReset.cxx
@@ -0,0 +1,41 @@
+#include "eudaq/OptionParser.hh"
+#include "eudaq/Exception.hh"
+#include "eudaq/Utils.hh"
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+using eudaq::uchar_cast;
+
+int main(int /*argc*/, const char ** argv) {
+  eudaq::OptionParser op("EUDAQ EUDRB Resetter", "1.0", "Reset an EUDRB board");
+  eudaq::OptionFlag sys(op, "s", "sysreset",
+                        "Perform a SYSRESET on the crate");
+  eudaq::OptionFlag loc(op, "l", "localreset",
+                        "Perform a LOCALRESET");
+  eudaq::OptionFlag dbg(op, "d", "debug",
+                        "Print debug information");
+  try {
+    op.Parse(argv);
+    if (!sys.IsSet() && !loc.IsSet() && !dbg.IsSet()) throw eudaq::OptionException("Must specify either SYS- or LOCAL-reset");
+    int fd = open("/dev/vme_regs", O_RDWR);
+    if (fd == -1) {
+      close(fd);
+      EUDAQ_THROW("Unable to open VME registers device.");
+    }
+    lseek(fd, 0x238, SEEK_SET);
+    unsigned long data;
+    read(fd, uchar_cast(&data), sizeof data);
+    if (dbg.IsSet()) std::cout << "DEBUG: VCTRL register read " << eudaq::hexdec(data) << std::endl;
+    if (sys.IsSet()) data |= 1 << 17;
+    if (loc.IsSet()) data |= 1 << 16;
+    lseek(fd, 0x238, SEEK_SET);
+    if (dbg.IsSet()) std::cout << "DEBUG: VCTRL register writing " << eudaq::hexdec(data) << std::endl;
+    write(fd, uchar_cast(&data), sizeof data);
+    close(fd);
+    std::cout << "Done." << std::endl;
+  } catch (...) {
+    return op.HandleMainException();
+  }
+  return 0;
+}
