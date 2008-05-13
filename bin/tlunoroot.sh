ebd419115cdfd3ae94f10c3363bf7c9a1690b175
Emlyn Corrin <emlyn.corrin@gmail.com>
2008-05-13 12:43:56 +0000

    Improved tlunoroot script

--- bin/tlunoroot.sh
+++ bin/tlunoroot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if which lsusb > /dev/null; then
+if which lsusb &> /dev/null; then
   USBINF=`lsusb -d 0x165d:0x0001`
-elif which /sbin/lsusb > /dev/null; then
+elif which /sbin/lsusb &> /dev/null; then
   USBINF=`/sbin/lsusb -d 0x165d:0x0001`
 else
   echo "No lsusb command detected, please install usbutils"
@@ -20,6 +20,9 @@ USBDEV=`echo $USBINF | sed 's/^Bus \([0-9]*\) Device \([0-9]*\).*/\2/'`
 echo "Found TLU on Bus $USBBUS, device $USBDEV, making it accessible as non-root"
 echo "You may be prompted for the password here..."
 
-sudo chmod a+w /proc/bus/usb/$USBBUS/$USBDEV
+if sudo chmod a+w /proc/bus/usb/$USBBUS/$USBDEV; then
+  echo "Done."
+else
+  echo "Failed. If you don't have sudo set up, you may need to run this as root."
+fi
 
-echo "Done."
