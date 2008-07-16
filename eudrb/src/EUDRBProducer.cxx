855f8307fe4b4ed347538c2bf378c39ad0ed3886
Emlyn Corrin <emlyn.corrin@gmail.com>
2008-07-16 16:33:07 +0000

    Can set marker pixels in config

--- eudrb/src/EUDRBProducer.cxx
+++ eudrb/src/EUDRBProducer.cxx
@@ -223,6 +223,14 @@ public:
           if (n_eudrb==boards.size()-1 || unsync) data = 0xd0000000;
           vme_A32_D32_User_Data_SCT_write(fdOut, data, address+0x10);
           eudaq::mSleep(100);
+          int marker1 = param.Get("Board" + to_string(n_eudrb) + ".Marker1", "Marker1", -1);
+          int marker2 = param.Get("Board" + to_string(n_eudrb) + ".Marker2", "Marker1", -1);
+          if (marker1 >= 0 || marker2 >= 0) {
+            if (marker1 < 0) marker1 = marker2;
+            if (marker2 < 0) marker2 = marker1;
+            vme_A32_D32_User_Data_SCT_write(fdOut, 0x48100800 | (marker1 & 0xff), address+0x10);
+            vme_A32_D32_User_Data_SCT_write(fdOut, 0x48100700 | (marker2 & 0xff), address+0x10);
+          }
           vme_A32_D32_User_Data_SCT_write(fdOut, 0x48d10000, address+0x10);
         } else if (m_version == 2) {
           const int adcdelay = 0x7 & param.Get("Board" + to_string(n_eudrb) + ".AdcDelay", "AdcDelay", 2);
