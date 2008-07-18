9f96ad716c7ffc9f41f3c8596d9edccc48be5748
Emlyn Corrin <emlyn.corrin@gmail.com>
2008-07-18 08:55:59 +0000

    Tweaked marker digits in printout

--- eudrb/src/EUDRBProducer.cxx
+++ eudrb/src/EUDRBProducer.cxx
@@ -272,7 +272,7 @@ public:
           if (marker1 < 0) marker1 = marker2;
           if (marker2 < 0) marker2 = marker1;
           std::cout << "Setting board " << n_eudrb << " markers to "
-                    << eudaq::hexdec(marker1, 8) << ", " << eudaq::hexdec(marker2, 8) << std::endl;
+                    << eudaq::hexdec(marker1, 2) << ", " << eudaq::hexdec(marker2, 2) << std::endl;
           vme_A32_D32_User_Data_SCT_write(fdOut, 0x48110800 | (marker1 & 0xff), address+0x10);
           vme_A32_D32_User_Data_SCT_write(fdOut, 0x48110700 | (marker2 & 0xff), address+0x10);
         }
