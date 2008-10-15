91cd4c05fe531a1e3ae0cbdd44e35f4f82e81fb9
Emlyn Corrin <emlyn.corrin@gmail.com>
2008-10-15 08:16:47 +0000

    Improved Timer

--- main/include/eudaq/Timer.hh
+++ main/include/eudaq/Timer.hh
@@ -16,14 +16,20 @@ namespace eudaq {
     void Stop() {
       m_stop = Time::Current();
     }
+    Time Elapsed() const {
+      return StopTime() - m_start;
+    }
     double Seconds() const {
-      return (StopTime() - m_start).Seconds();
+      return Elapsed().Seconds();
     }
     double mSeconds() const {
       return 1e3 * Seconds();
     }
     double uSeconds() const {
       return 1e6 * Seconds();
+    }
+    std::string Formatted(const std::string & format = Time::DEFAULT_FORMAT) const {
+      return Elapsed().Formatted(format);
     }
   private:
     Time StopTime() const { return m_stop == Time(0) ? Time::Current() : m_stop; }
