e7708f71cb9fc0f7679fd7c84f5b7f694c43ae53
Emlyn Corrin <emlyn.corrin@gmail.com>
2008-11-14 15:23:59 +0000

    Added Info.hh header

---
+++ main/include/eudaq/Info.hh
@@ -0,0 +1,11 @@
+#ifndef EUDAQ_INCLUDED_Info
+#define EUDAQ_INCLUDED_Info
+
+/*
+ * This file contains information about which capabilities this version of EUDAQ supports,
+ * to enable users of the library to conditionally enable functionality.
+ */
+
+#define EUDAQ_DEPFET_DECODER 1
+
+#endif // EUDAQ_INCLUDED_Info
