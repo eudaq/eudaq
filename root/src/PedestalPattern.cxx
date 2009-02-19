bb0b1a37d0f8ec41f1c6ba22108db2686fc35279
Emlyn Corrin <emlyn.corrin@gmail.com>
2009-02-19 17:12:48 +0000

    Added predefined sensor sizes to PedestalPattern

--- root/src/PedestalPattern.cxx
+++ root/src/PedestalPattern.cxx
@@ -18,19 +18,24 @@ typedef int TH2D;
 static const char tab = '\t';
 
 void pattern_zs(unsigned w, unsigned h, ostream & out, TH2D & histo) {
+  //unsigned numpix = 0;
   for (unsigned j = 0; j < h; ++j) {
     double y = 2*j / (h - 1.0) - 1;
     for (unsigned i = 0; i < w; ++i) {
       double x = 2*i / (w - 1.0) - 1;
       double r = x*x + y*y;
       double v = r - 0.5; // -0.5 -> 0.5
-      if (fabs(v) > fabs(1.2 * x - y)) v = 1.2 * x - y;
+      double u = 1.2 * x - y + 0.2;
+      if (fabs(v) > fabs(u)) v = u;
+      u = 1.2 * x - y - 0.4;
+      if (fabs(v) > fabs(u)) v = u;
 
       v = v*v; // 0 -> 0.25
       v = -50000 * v; // -... -> 0
       v += 30; // ... -> 30
-      if (v < 0) v = 0; // 0 -> 30
-      int ped = -(int)v;
+      if (v <= 0) v = 0; // 0 -> 30
+      //else numpix++;
+      int ped = -(int)(v+0.5);
       int noise = 5; //v > 2 ? 0 : 10;
 #if USE_ROOT
       histo.Fill(i, j, ped);
@@ -41,10 +46,13 @@ void pattern_zs(unsigned w, unsigned h, ostream & out, TH2D & histo) {
       out << 0 << tab << i << tab << j << tab << ped << tab << noise << std::endl;
     }
   }
+  //std::cout << "numpix " << numpix << std::endl;
 }
 
 int main(int /*argc*/, const char ** argv) {
   eudaq::OptionParser op("EUDAQ Pedestal Pattern Generator", "1.0", "");
+  eudaq::Option<std::string> sensor(op, "s", "sensor", "mimotel", "name",
+                                     "The type of sensor (sets default size: mimotel=256x256, mimosa18=508x512)");
   eudaq::Option<unsigned> width(op, "x", "width", 256, "pixels",
                                 "The width of the sensor in pixels");
   eudaq::Option<unsigned> height(op, "y", "height", 256, "pixels",
@@ -63,16 +71,28 @@ int main(int /*argc*/, const char ** argv) {
       throw eudaq::OptionException("Must specify output file");
     }
     if (pattern.Value() != "zs") throw eudaq::OptionException("Unrecognised pattern: " + pattern.Value());
+    unsigned w = 0, h = 0;
+    if (eudaq::lcase(sensor.Value()) == "mimotel") {
+      w = 256;
+      h = 256;
+    } else if (eudaq::lcase(sensor.Value()) == "mimosa18") {
+      w = 508;
+      h = 512;
+    } else if (sensor.Value() != "") {
+      throw eudaq::OptionException("Unrecognised sensor: " + sensor.Value());
+    }
+    if (width.IsSet()) w = width.Value();
+    if (height.IsSet()) h = height.Value();
 #if USE_ROOT
-    TH2D pat("pat", "Pattern", width.Value(), 0.0, width.Value(), height.Value(), 0.0, height.Value());
+    TH2D pat("pat", "Pattern", w, 0.0, w, h, 0.0, h);
 #else
     TH2D pat = 0;
 #endif
     std::ofstream fout(outfile.Value().c_str());
     fout << "# Automatically generated pedestal and noise file\n"
-         << "# Pattern: " << pattern.Value() << ", width: " << width.Value()
-         << ", height: " << height.Value() << std::endl;
-    pattern_zs(width.Value(), height.Value(), fout, pat);
+         << "# Pattern: " << pattern.Value() << ", width: " << w
+         << ", height: " << h << std::endl;
+    pattern_zs(w, h, fout, pat);
 #if USE_ROOT
     if (rootfile.Value() != "") {
       TFile froot(rootfile.Value().c_str(), "RECREATE", "", 2);
