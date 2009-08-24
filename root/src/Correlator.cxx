f93454175d4c6d608c4f8c26a36d7a0bd6178854
Emlyn Corrin <emlyn.corrin@gmail.com>
2009-08-24 12:04:58 +0000

    Start of improvements to Correlator

--- root/src/Correlator.cxx
+++ root/src/Correlator.cxx
@@ -25,13 +25,24 @@ int main(int /*argc*/, char ** argv) {
                          2, 2);
   eudaq::Option<std::string> fnameout(op, "o", "output-file", "correlation.root", "filename",
                                    "The output Root file");
-  eudaq::Option<int> width(op, "x", "width", 256, "pixels",
-                                   "The width of the detector in pixels");
-  eudaq::Option<int> height(op, "y", "height", 256, "pixels",
-                                   "The height of the detector in pixels");
+  eudaq::Option<int> width1(op, "x1", "width1", 256, "pixels",
+                                   "The width of the first detector in pixels");
+  eudaq::Option<int> width2(op, "x2", "width2", 0, "pixels",
+                                   "The width of the second detector in pixels");
+  eudaq::Option<int> height1(op, "y1", "height1", 256, "pixels",
+                                   "The height of the first detector in pixels");
+  eudaq::Option<int> height2(op, "y2", "height2", 0, "pixels",
+                                   "The height of the second detector in pixels");
+  eudaq::Option<int> pitch1(op, "p1", "pitch1", 1, "",
+                                   "The pitch of the first detector");
+  eudaq::Option<int> pitch2(op, "p2", "pitch2", 1, "",
+                                   "The pitch of the second detector");
   try {
     op.Parse(argv);
-    const unsigned w = width.Value(), h = height.Value();
+#define IFELSE(a, b) ((a) ? (a) : (b))
+    const unsigned w1 = width1.Value(), w2 = IFELSE(width2.Value(), w1), h1 = height1.Value(), h2 = IFELSE(height2.Value(), h1);
+#undef IFELSE
+
     std::ifstream file1(op.GetArg(0).c_str()), file2(op.GetArg(1).c_str());
     if (!file1.is_open()) {
       throw eudaq::OptionException("Unable to open first file \'" + op.GetArg(0) + "\'");
@@ -39,7 +50,7 @@ int main(int /*argc*/, char ** argv) {
     if (!file2.is_open()) {
       throw eudaq::OptionException("Unable to open second file \'" + op.GetArg(1) + "\'");
     }
-    std::vector<double> correlx(w*w), correly(h*h);
+    std::vector<double> correlx(w1*w2), correly(h1*h2);
     std::cout << "Reading and histogramming clusters..." << std::endl;
     for (;;) {
       eudaq::Clusters c1(file1), c2(file2);
@@ -49,41 +60,45 @@ int main(int /*argc*/, char ** argv) {
         if (file1.eof() || file2.eof()) break;
       }
       if (file1.eof() || file2.eof()) break;
+      //std::cout << "Event " << c1.EventNum() << std::endl;
       for (int i1 = 0; i1 < c1.NumClusters(); ++i1) {
         unsigned x1 = c1.ClusterX(i1);
         unsigned y1 = c1.ClusterY(i1);
-        if (x1 >= w || y1 >= h) {
+        if (x1 >= w1 || y1 >= h1) {
           std::cout << "Event " << c1.EventNum() << ", x1=" << x1 << ", y1=" << y1 << std::endl;
+          continue;
         }
         for (int i2 = 0; i2 < c2.NumClusters(); ++i2) {
           unsigned x2 = c2.ClusterX(i2);
           unsigned y2 = c2.ClusterY(i2);
-          if (i2 == 0 && (x2 >= w || y2 >= h)) {
-            std::cout << "Event " << c2.EventNum() << ", x2=" << x2 << ", y2=" << y2 << std::endl;
+          if (x2 >= w2 || y2 >= h2) {
+            if (i1 == 0) std::cout << "Event " << c2.EventNum() << ", x2=" << x2 << ", y2=" << y2 << std::endl;
+            continue;
           }
-          ++correlx[x1 + w * x2];
-          ++correly[y1 + h * y2];
+          ++correlx[x1 + w1 * x2];
+          ++correly[y1 + h1 * y2];
         }
       }
     }
     std::cout << "OK, saving correlation histograms..." << std::endl;
     TFile fout(fnameout.Value().c_str(), "RECREATE", "", 2);
-    TH2D hx("correlx", "X Correlation", w, 0.0, w, w, 0.0, w);
+    TH2D hx("correlx", "X Correlation", w1, 0.0, w1, w2, 0.0, w2);
     double * xp = &correlx[0];
-    for (unsigned x2 = 0; x2 < w; ++x2) {
-      for (unsigned x1 = 0; x1 < w; ++x1) {
+    for (unsigned x2 = 0; x2 < w2; ++x2) {
+      for (unsigned x1 = 0; x1 < w1; ++x1) {
         hx.SetBinContent(x1+1, x2+1, *xp++);
       }
     }
-    TH2D hy("correly", "Y Correlation", h, 0.0, h, h, 0.0, h);
+    TH2D hy("correly", "Y Correlation", h1, 0.0, h1, h2, 0.0, h2);
     double * yp = &correly[0];
-    for (unsigned y2 = 0; y2 < h; ++y2) {
-      for (unsigned y1 = 0; y1 < h; ++y1) {
+    for (unsigned y2 = 0; y2 < h2; ++y2) {
+      for (unsigned y1 = 0; y1 < h1; ++y1) {
         hy.SetBinContent(y1+1, y2+1, *yp++);
       }
     }
     hx.Write("correlx", TObject::kOverwrite);
     hy.Write("correly", TObject::kOverwrite);
+    const double w = (w1+w2)/2.0;
     std::cout << "Calculating Radon transform for X..." << std::endl;
     unsigned Tx = (int)std::ceil(pi * w), Rx = 2*w;
     std::vector<double> radonx(Tx*Rx);
@@ -100,6 +115,7 @@ int main(int /*argc*/, char ** argv) {
     radon(&radonx[0], &correlx[0], param);
 
     std::cout << "Calculating Radon transform for Y..." << std::endl;
+    const double h = (h1+h2)/2.0;
     unsigned Ty = (int)std::ceil(pi * h), Ry = 2*h;
     std::vector<double> radony(Ty*Ry);
     param[0] = h;
