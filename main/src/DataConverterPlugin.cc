e6e183964a4a57cc9a5f3549fae5880e523f5c43
Emlyn Corrin <emlyn.corrin@gmail.com>
2009-09-01 16:23:58 +0000

    Removed some debug output

--- main/src/DataConverterPlugin.cc
+++ main/src/DataConverterPlugin.cc
@@ -8,14 +8,14 @@ namespace eudaq {
   DataConverterPlugin::DataConverterPlugin(std::string subtype)
     : m_eventtype(make_pair(Event::str2id("_RAW"), subtype))
   {
-    std::cout << "DEBUG: Registering DataConverterPlugin: " << Event::id2str(m_eventtype.first) << ":" << m_eventtype.second << std::endl;
+    //std::cout << "DEBUG: Registering DataConverterPlugin: " << Event::id2str(m_eventtype.first) << ":" << m_eventtype.second << std::endl;
     PluginManager::GetInstance().RegisterPlugin(this);
   }
 
   DataConverterPlugin::DataConverterPlugin(unsigned type, std::string subtype)
     : m_eventtype(make_pair(type, subtype))
   {
-    std::cout << "DEBUG: Registering DataConverterPlugin: " << Event::id2str(m_eventtype.first) << ":" << m_eventtype.second << std::endl;
+    //std::cout << "DEBUG: Registering DataConverterPlugin: " << Event::id2str(m_eventtype.first) << ":" << m_eventtype.second << std::endl;
     PluginManager::GetInstance().RegisterPlugin(this);
   }
 
