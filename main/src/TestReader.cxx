e48fb272e23b14ddfeda985b758e2429373fe359
Igor Rubinsky <rubinsky@mail.desy.de>
2009-10-15 11:41:48 +0000

    several improvements: - can convert to the root file without other keys - improved the output INFO output - the output root filename is named now just the raw file (the ending is replaced with .root, of course) - code layout improved readability

--- main/src/TestReader.cxx
+++ main/src/TestReader.cxx
@@ -9,6 +9,7 @@
 #include "TFile.h"
 #include "TTree.h"
 #include "TRandom.h"
+#include "TString.h"
 
 
 
@@ -19,8 +20,10 @@ using eudaq::StandardEvent;
 using eudaq::from_string;
 using eudaq::to_string;
 using eudaq::split;
+using namespace std;
 
 // Book variables for the Event_to_TTree conversion 
+static bool do_ttree    = false; // static variable to see if the tree is open already for writing  
 TFile *tfile            = 0 ; // book the pointer to a file (to store the otuput)
 TTree *ttree            = 0 ; // book the tree (to store the needed event info)
 Int_t id_plane          = 0 ; // plane id, where the hit is 
@@ -32,6 +35,7 @@ unsigned i_run          = 0 ; // a run  number
 unsigned i_event        = 0 ; // an event number 
 //
 unsigned long long int i_time_stamp = 0 ; // the time stampe 
+unsigned int           i_last       = 0 ; // imply event count in the raw file
 //
 
 std::vector<unsigned> parsenumbers(const std::string & s) {
@@ -67,8 +71,11 @@ bool DoEvent(unsigned /*ndata*/, const eudaq::DetectorEvent & dev, bool do_proce
     
     //std::cout << "DEBUG " << ndata << ", " << do_display << ", " << do_zs << std::endl;
 
-    if (do_process || do_display || do_dump || do_zs) {
+    // always do this 
+    if (do_process || do_display || do_dump || do_zs || do_ttree) {
         
+
+        // do this if an event range is given
         if (do_display) {
             
             std::cout << dev << std::endl;
@@ -101,95 +108,142 @@ bool DoEvent(unsigned /*ndata*/, const eudaq::DetectorEvent & dev, bool do_proce
             }
         }
     
+        // do this if 
         if (do_process) {
-        unsigned boardnum = 0;
-        const StandardEvent & sev = eudaq::PluginManager::ConvertToStandard(dev);
-        if (do_display) std::cout << "Standard Event: " << sev << std::endl;
-        for (size_t iplane = 0; iplane < sev.NumPlanes(); ++iplane) 
-        {
-            const eudaq::StandardPlane & plane = sev.GetPlane(iplane);
-            std::vector<double> cds = plane.GetPixels<double>();
-            //std::cout << "DBG " << eudaq::hexdec(plane.m_flags);
+            
+            unsigned boardnum = 0;
+            const StandardEvent & sev = eudaq::PluginManager::ConvertToStandard(dev);
+            
+            if (do_display) std::cout << "Standard Event: " << sev << std::endl;
+            
+            for (size_t iplane = 0; iplane < sev.NumPlanes(); ++iplane) {
+                
+                const eudaq::StandardPlane & plane = sev.GetPlane(iplane);
+                std::vector<double> cds = plane.GetPixels<double>();
+                //std::cout << "DBG " << eudaq::hexdec(plane.m_flags);
 
-            bool bad = false;
-            for (size_t ipix = 0; ipix < cds.size(); ++ipix) 
-            {
+                bool bad = false;
+//                for (size_t ipix = 0; ipix < cds.size(); ++ipix) {
                 //          if (ipix < 10) std::cout << ", " << plane.m_pix[0][ipix] << ";" << cds[ipix]
+//                }
+                //std::cout << (bad ? "***" : "") << std::endl;
 
-                id_plane = plane.ID();          
-                id_hit = ipix;
-                id_x = plane.GetX(ipix);
-                id_y = plane.GetY(ipix);
-                i_time_stamp =  sev.GetTimestamp();
-//          printf("%#x \n", i_time_stamp);  
-                i_tlu = plane.TLUEvent();   
-                i_run = sev.GetRunNumber();
-                i_event = sev.GetEventNumber();                  
-                ttree->Fill(); 
-            }
+                if (bad) std::cout << "***" << std::endl;
+                
+                if (do_zs) {
+                    
+                    for (size_t ipix = 0; ipix < 20 && ipix < cds.size(); ++ipix) {
+                        
+                        std::cout << ipix << ": " << eudaq::hexdec(cds[ipix]) << std::endl;
+                    }
 
-        //std::cout << (bad ? "***" : "") << std::endl;
-
-            if (bad) std::cout << "***" << std::endl;
-            if (do_zs) 
-            {
-                for (size_t ipix = 0; ipix < 20 && ipix < cds.size(); ++ipix) 
-                {
-                    std::cout << ipix << ": " << eudaq::hexdec(cds[ipix]) << std::endl;
+                    //   std::cout << "  Plane: " << plane << std::endl;
+                    //   for (size_t p = 0; p < plane.m_pix[0].size(); ++p) {
+                    //     static const char tab = '\t';
+                    //     std::cout << "    " << ndata << tab << boardnum << tab << plane.m_x[p] << tab << plane.m_y[p] << tab << plane.m_pix[0][p] << std::endl;
+                    //   }
                 }
-
-                //   std::cout << "  Plane: " << plane << std::endl;
-                //   for (size_t p = 0; p < plane.m_pix[0].size(); ++p) {
-                //     static const char tab = '\t';
-                //     std::cout << "    " << ndata << tab << boardnum << tab << plane.m_x[p] << tab << plane.m_y[p] << tab << plane.m_pix[0][p] << std::endl;
-                //   }
+                boardnum++;
             }
-            boardnum++;
         }
-    }
-    
-    ttree->Write();
 
-  }
+        // if the tree is open do this
+    
+//        cout <<   do_ttree <<endl; 
+        if( do_ttree ){
+            
+            const StandardEvent & sev = eudaq::PluginManager::ConvertToStandard(dev);
+            
+//            cout << "Standard Event: " << sev << endl;
+            
+            for (size_t iplane = 0; iplane < sev.NumPlanes(); ++iplane) {
+                
+                const eudaq::StandardPlane & plane = sev.GetPlane(iplane);
+                std::vector<double> cds = plane.GetPixels<double>();
+                
+                for (size_t ipix = 0; ipix < cds.size(); ++ipix) {
+
+                    //          if (ipix < 10) std::cout << ", " << plane.m_pix[0][ipix] << ";" << cds[ipix]
+
+                    id_plane = plane.ID();          
+                    id_hit = ipix;
+                    id_x = plane.GetX(ipix);
+                    id_y = plane.GetY(ipix);
+                    i_time_stamp =  sev.GetTimestamp();
+                    //          printf("%#x \n", i_time_stamp);  
+                    i_tlu = plane.TLUEvent();   
+                    i_run = sev.GetRunNumber();
+                    i_event = sev.GetEventNumber();                  
+                    ttree->Fill(); 
+                }               
+            }  
+       }
+        
+    }
   return do_display;
 }
 
-int main(int /*argc*/, char ** argv) {
-  eudaq::OptionParser op("EUDAQ Raw Data file reader", "1.0",
-                         "A command-line tool for printing out a summary of a raw data file",
-                         1);
-  eudaq::Option<std::string> ipat(op, "i", "inpattern", "../data/run$6R.raw", "string", "Input filename pattern");
-  eudaq::OptionFlag do_bore(op, "b", "bore", "Display the BORE event");
-  eudaq::OptionFlag do_eore(op, "e", "eore", "Display the EORE event");
-  eudaq::OptionFlag do_proc(op, "p", "process", "Process data from displayed events");
-  eudaq::OptionFlag do_pall(op, "a", "process-all", "Process data from all events");
-  eudaq::Option<std::string> do_data(op, "d", "display", "", "numbers", "Event numbers to display (eg. '1-10,99,-1')");
-  eudaq::OptionFlag do_dump(op, "u", "dump", "Dump raw data for displayed events");
-  eudaq::OptionFlag do_zs(op, "z", "zsdump", "Print pixels for zs events");
-  eudaq::OptionFlag do_event_to_ttree(op, "r", "event-to-ttree", "Convert a file into a TTree .root format");
-  try {
-    op.Parse(argv);
-    EUDAQ_LOG_LEVEL("INFO");
-    std::vector<unsigned> displaynumbers = parsenumbers(do_data.Value());
-    //for (unsigned i = 0; i < displaynumbers.size(); ++i) std::cout << "+ " << displaynumbers[i] << std::endl;
-    bool showlast = std::find(displaynumbers.begin(), displaynumbers.end(), (unsigned)-1) != displaynumbers.end();
-    counted_ptr<eudaq::DetectorEvent> lastevent;
+void StripAllDirs(TString &fstring){
+// 
+// Removes everything before the last slash '/' (including the slash
+// 
+    fstring.Remove(0,fstring.Last('/')+1);
 
-    if(do_event_to_ttree.IsSet()){
-        std::cout << "Converting the inputfile into a TTree "   << std::endl;
-        tfile = new TFile("output.root","RECREATE");
-        ttree = new TTree("tree","a simple Tree with simple variables");
+}
+
+
+int main(int /*argc*/, char ** argv) {
+    
+    eudaq::OptionParser op("EUDAQ Raw Data file reader", "1.0",
+            "A command-line tool for printing out a summary of a raw data file",
+            1);
+    eudaq::Option<std::string> ipat(op, "i", "inpattern", "../data/run$6R.raw", "string", "Input filename pattern");
+    eudaq::OptionFlag do_bore(op, "b", "bore", "Display the BORE event");
+    eudaq::OptionFlag do_eore(op, "e", "eore", "Display the EORE event");
+    eudaq::OptionFlag do_proc(op, "p", "process", "Process data from displayed events");
+    eudaq::OptionFlag do_pall(op, "a", "process-all", "Process data from all events");
+    eudaq::Option<std::string> do_data(op, "d", "display", "", "numbers", "Event numbers to display (eg. '1-10,99,-1')");
+    eudaq::OptionFlag do_dump(op, "u", "dump", "Dump raw data for displayed events");
+    eudaq::OptionFlag do_zs(op, "z", "zsdump", "Print pixels for zs events");
+    eudaq::OptionFlag do_event_to_ttree(op, "r", "event-to-ttree", "Convert a file into a TTree .root format");
+    
+    try {
         
-        ttree->Branch("id_plane",&id_plane,"id_plane/I");
-        ttree->Branch("id_hit",&id_hit,"id_hit/I");
-        ttree->Branch("id_x",&id_x,"id_x/D");
-        ttree->Branch("id_y",&id_y,"id_y/D");
-        ttree->Branch("i_time_stamp",&i_time_stamp,"i_time_stamp/LLU");
-        ttree->Branch("i_tlu",&i_tlu,"i_tlu/I");
-        ttree->Branch("i_run",&i_run,"i_run/I");
-        ttree->Branch("i_event",&i_event,"i_event/I");
+        op.Parse(argv);
+        EUDAQ_LOG_LEVEL("INFO");
+        std::vector<unsigned> displaynumbers = parsenumbers(do_data.Value());
+        //for (unsigned i = 0; i < displaynumbers.size(); ++i) std::cout << "+ " << displaynumbers[i] << std::endl;
+
+        bool showlast = std::find(displaynumbers.begin(), displaynumbers.end(), (unsigned)-1) != displaynumbers.end();
+        counted_ptr<eudaq::DetectorEvent> lastevent;
+
+        if(do_event_to_ttree.IsSet() && do_ttree == false ){
+ 
+            EUDAQ_INFO("Converting the inputfile into a TTree " );
+         
+            eudaq::FileReader reader(op.GetArg(0), ipat.Value());
+            TString foutput = reader.Filename();           
+            StripAllDirs(foutput);
+            if(foutput.Contains(".raw")) foutput.ReplaceAll("raw" , "root");
+            
+            EUDAQ_INFO("Preparing the outputfile: " +  to_string(foutput.Data()) );
+
+            if(!foutput.Contains(".root")) foutput = "output.root";
+                
+            do_ttree = true;
+           tfile = new TFile(foutput,"RECREATE");
+            ttree = new TTree("tree","a simple Tree with simple variables");
+            
+            ttree->Branch("id_plane",&id_plane,"id_plane/I");
+            ttree->Branch("id_hit",&id_hit,"id_hit/I");
+            ttree->Branch("id_x",&id_x,"id_x/D");
+            ttree->Branch("id_y",&id_y,"id_y/D");
+            ttree->Branch("i_time_stamp",&i_time_stamp,"i_time_stamp/LLU");
+            ttree->Branch("i_tlu",&i_tlu,"i_tlu/I");
+            ttree->Branch("i_run",&i_run,"i_run/I");
+            ttree->Branch("i_event",&i_event,"i_event/I");
   
-        //fill the tree
+            //fill the tree
 /*
         for (Int_t i=0;i<10000;i++) {
             id_plane      = i;
@@ -206,62 +260,114 @@ int main(int /*argc*/, char ** argv) {
 
         ttree->Write();
         */
+        
+        }
+    
+        
+        for (size_t i = 0; i < op.NumArgs(); ++i) {
+                         
+            eudaq::FileReader reader(op.GetArg(i), ipat.Value());
+            EUDAQ_INFO("Reading: " + reader.Filename());
+
+//            cout << i << " " << reader.Filename()  << endl;
+
+            unsigned ndata = 0, ndatalast = 0, nnondet = 0, nbore = 0, neore = 0;
+            
+            do {
+          
+                const eudaq::DetectorEvent & dev = reader.Event();
+                if (dev.IsBORE()) {
+                    
+                    nbore++;
+          
+                    if (nbore > 1) {
+                        
+                        EUDAQ_WARN("Multiple BOREs (" + to_string(nbore) + ")");
+                    }
+          
+
+                    if (do_bore.IsSet()) {
+              
+                        std::cout << dev << std::endl;
+                    }
+          
+                    eudaq::PluginManager::Initialize(dev);
+        
+                } else if (dev.IsEORE()) {
+            
+                    neore++;
+                    
+                    if (neore > 1) {
+              
+                        EUDAQ_WARN("Multiple EOREs (" + to_string(neore) + ")");
+                    }
+          
+                    if (do_eore.IsSet()) {          
+
+                        std::cout << dev << std::endl;
+                    }
+        
+                } else {
+            
+                    ndata++;
+                    // TODO: check event number matches ndata
+
+                    bool show = std::find(displaynumbers.begin(), displaynumbers.end(), ndata) != displaynumbers.end();
+                    bool proc = do_pall.IsSet() || (show && do_proc.IsSet());
+                    bool dump = (do_dump.IsSet());
+                    bool shown = DoEvent(ndata, dev, proc, show, do_zs.IsSet(), dump);
+                    
+                    if (showlast) {
+              
+                        if (shown) {
+                
+                            lastevent = 0;
+                        } else {
+                            
+                            lastevent = counted_ptr<eudaq::DetectorEvent>(new eudaq::DetectorEvent(dev));
+                            ndatalast = ndata;
+                        }
+                    }
+                }
+            
+                i_last++; // keep in mind the last event read from the file
+               
+            } while (reader.NextEvent());
+      
+            
+            if (lastevent.get()) DoEvent(ndatalast, *lastevent.get(), false, true, do_zs.IsSet(), false);
+      
+            EUDAQ_INFO("Number of data events: " + to_string(ndata));
+            if (nnondet) std::cout << "Warning: Non-DetectorEvents found: " << nnondet << std::endl;
+            if (!nbore) {
+                
+                std::cout << "Warning: No BORE found" << std::endl;
+            } else if (nbore > 1) {
+            
+                std::cout << "Warning: Multiple BOREs found: " << nbore << std::endl;
+            }
+            
+            if (!neore) {
+                
+                std::cout << "Warning: No EORE found, possibly truncated file." << std::endl;
+            } else if (neore > 1) {
+                
+                std::cout << "Warning: Multiple EOREs found: " << nbore << std::endl;
+            }
+            
+            if (nnondet || (nbore != 1) || (neore > 1)) std::cout << "Probably corrupt file." << std::endl;
+        }
+
+//        cout << "Last event read is # "<< i_last << " (should be by 2 more than Event in the file counting the EORE & BORE)" << endl;
+    
+    } catch (...) {
+        
+        return op.HandleMainException();
     }
     
-    for (size_t i = 0; i < op.NumArgs(); ++i) {
-      eudaq::FileReader reader(op.GetArg(i), ipat.Value());
-      EUDAQ_INFO("Reading: " + reader.Filename());
+    // finish by writing the ttree
+    if( do_ttree ) ttree->Write();
+ 
+    return 0;
 
-      unsigned ndata = 0, ndatalast = 0, nnondet = 0, nbore = 0, neore = 0;
-      do {
-        const eudaq::DetectorEvent & dev = reader.Event();
-        if (dev.IsBORE()) {
-          nbore++;
-          if (nbore > 1) {
-            EUDAQ_WARN("Multiple BOREs (" + to_string(nbore) + ")");
-          }
-          if (do_bore.IsSet()) std::cout << dev << std::endl;
-          eudaq::PluginManager::Initialize(dev);
-        } else if (dev.IsEORE()) {
-          neore++;
-          if (neore > 1) {
-            EUDAQ_WARN("Multiple EOREs (" + to_string(neore) + ")");
-          }
-          if (do_eore.IsSet()) std::cout << dev << std::endl;
-        } else {
-          ndata++;
-          // TODO: check event number matches ndata
-          bool show = std::find(displaynumbers.begin(), displaynumbers.end(), ndata) != displaynumbers.end();
-          bool proc = do_pall.IsSet() || (show && do_proc.IsSet());
-          bool dump = (do_dump.IsSet());
-          bool shown = DoEvent(ndata, dev, proc, show, do_zs.IsSet(), dump);
-          if (showlast) {
-            if (shown) {
-              lastevent = 0;
-            } else {
-              lastevent = counted_ptr<eudaq::DetectorEvent>(new eudaq::DetectorEvent(dev));
-              ndatalast = ndata;
-            }
-          }
-        }
-      } while (reader.NextEvent());
-      if (lastevent.get()) DoEvent(ndatalast, *lastevent.get(), false, true, do_zs.IsSet(), false);
-      EUDAQ_INFO("Number of data events: " + to_string(ndata));
-      if (nnondet) std::cout << "Warning: Non-DetectorEvents found: " << nnondet << std::endl;
-      if (!nbore) {
-        std::cout << "Warning: No BORE found" << std::endl;
-      } else if (nbore > 1) {
-        std::cout << "Warning: Multiple BOREs found: " << nbore << std::endl;
-      }
-      if (!neore) {
-        std::cout << "Warning: No EORE found, possibly truncated file." << std::endl;
-      } else if (neore > 1) {
-        std::cout << "Warning: Multiple EOREs found: " << nbore << std::endl;
-      }
-      if (nnondet || (nbore != 1) || (neore > 1)) std::cout << "Probably corrupt file." << std::endl;
-    }
-  } catch (...) {
-    return op.HandleMainException();
-  }
-  return 0;
 }
