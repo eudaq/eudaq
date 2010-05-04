c5f4009d11b8ff1fd5c6c2413c98ffa4366ae2e7
Emlyn Corrin <emlyn.corrin@gmail.com>
2010-05-04 07:47:13 +0000

    Added skeleton MVDConverterPlugin

---
+++ main/src/MVDConverterPlugin.cc
@@ -0,0 +1,16 @@
+#include "eudaq/DataConverterPlugin.hh"
+
+namespace eudaq {
+
+  static const std::string EVENT_TYPE = "MVD";
+
+  // Test Events have nothing to convert
+  // This class is only here to prevent a runtime errors
+  class MVDConverterPlugin : public DataConverterPlugin {
+    MVDConverterPlugin() : DataConverterPlugin(EVENT_TYPE) {}
+    static MVDConverterPlugin m_instance;
+  };
+
+  MVDConverterPlugin MVDConverterPlugin::m_instance;
+
+}
