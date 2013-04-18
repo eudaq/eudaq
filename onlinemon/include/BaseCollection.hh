6adc1a54a526d3153bf47c90274b06f1dfd40359
Phillip Hamnett <phillip.hamnett@desy.de>
2013-04-18 13:02:32 +0000

    Added basic documentation. Removed all function definitions from header file and put them in source file

--- onlinemon/include/BaseCollection.hh
+++ onlinemon/include/BaseCollection.hh
@@ -22,6 +22,19 @@ const unsigned int MONITORPERFORMANCE_COLLECTION_TYPE = 3;
 const unsigned int EUDAQMONITOR_COLLECTION_TYPE = 4;
 const unsigned int UNKNOWN_COLLECTION_TYPE = 9999;
 
+//!BaseCollection class
+/*!
+ *  This class is inherited by:
+ \li \class CorrelationCollection CorrelationCollection.hh "CorrelationCollection.hh"
+ \li \class EUDAQMonitorCollection EUDAQMonitorCollection.hh "EUDAQMonitorCollection.hh"
+ \li \class HitmapCollection HitmapCollection.hh "HitmapCollection.hh"
+ \li \class MonitorPerformanceCollection MonitorPerformanceCollection.hh "MonitorPerformanceCollection.hh"
+
+ It contains the parameters:
+ \li @param _reduce 
+ \li @param _mon This is the online monitor. It is from the class \class RootMonitor RootMonitor.cxx "../../root/src/RootMonitor.cxx"
+ \li @param _CollectionType This is used to specify what kind of collection we have
+ */
 class BaseCollection
 {
   protected:
@@ -29,12 +42,32 @@ class BaseCollection
     RootMonitor *_mon;
     unsigned int CollectionType;
   public:
+    //!Constructor
+    /*!This sets the parameter _reduce to 1, _mon to NULL and CollectionType to UNKNOWN_COLLECTION_TYPE*/
     BaseCollection();
+
+    //!Write
+    /*!This is a pure virtual function which writes the data to a TFile called @param file*/
     virtual void Write(TFile *file) = 0;
+
+    //!Calculate
+    /*!This is a pure virtual function which calculates the values for the inherited classes for the current event number*/
     virtual void Calculate(const unsigned int currentEventNumber) = 0;
+
+    //!Fill
+    /*!This is a pure virtual function which fills the monitor's histograms with the new data*/
     virtual void Fill(const SimpleStandardEvent& simpev) = 0;
+
+    //!Reset
+    /*!This resets all the histograms ready for a new run*/
     virtual void Reset() = 0;
+
+    //!Set Reduce
+    /*!This sets a new value for the parameter _reduce*/
     void setReduce(const unsigned int red);
+
+    //!Get Collection Type
+    /*!This returns the parameter CollectionType*/
     unsigned int getCollectionType();
 };
 
