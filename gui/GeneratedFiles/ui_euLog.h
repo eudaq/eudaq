05ef2cf079d45ade3ab677e8f549797c803b7cbe
RPeschke <Richard.Peschke@desy.de>
2013-10-28 19:45:12 +0100

    the Binaries that where made with qt 5 do not work on other computers. i could figure out what the problem is therefore i go back to qt 4.8. needs to be tested

--- gui/GeneratedFiles/ui_euLog.h
+++ gui/GeneratedFiles/ui_euLog.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'euLog.ui'
 **
-** Created: Wed 23. Oct 16:25:23 2013
+** Created: Mon 28. Oct 18:56:24 2013
 **      by: Qt User Interface Compiler version 4.8.4
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
