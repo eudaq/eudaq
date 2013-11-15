f353a5834f09ff7051819aa40a8c4a3ff0b6008b
Felix Reidt <felix.reidt@cern.ch>
2013-11-15 14:22:26 +0100

    slowed down the TLU communication in order to avoid problems

--- producers/explorer/srs-software/slow_control/tlu_triggering.py
+++ producers/explorer/srs-software/slow_control/tlu_triggering.py
@@ -14,8 +14,8 @@ dADC = SlowControl.SlowControl(1)
 # 10: 4 clk div
 # 18:12 wait length
 trg_set = 2 # = TLU + busy w/o driver
-trg_set = trg_set | (0x0c << 4)  # clk divider (0x18) (minimum working 0x5 in the lab)
-trg_set = trg_set | (0x00 << 12) # wait length (0x2f)
+trg_set = trg_set | (0x18 << 4)  # clk divider (0x18) (minimum working 0x5 in the lab)
+trg_set = trg_set | (0x2f << 12) # wait length (0x2f)
 SlowControl.write_list(dHLVDS, 6039, [ 24 ], [ trg_set ], False)
 SlowControl.write_list(dADC,   6039, [  2 ], [ trg_set ], False)
 
