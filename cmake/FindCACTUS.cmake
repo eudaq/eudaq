17e004fb633eb3ce2bbbe8b7cf2540b1e001ee07
Francesco Crescioli <francesco.crescioli@lpnhe.in2p3.fr>
2013-12-12 13:39:18 +0100

    script to find CACTUS package from rpm installation

--- cmake/FindCACTUS.cmake
+++ cmake/FindCACTUS.cmake
@@ -1,7 +1,7 @@
 # - Try to find cactus
 
 #macro(find_cactus_in_extern arg)
-# find_path(CACTUS_ROOT cactuscore
+# find_path(CACTUS_ROOT uhal.hpp
 #    HINTS ${PROJECT_SOURCE_DIR}/extern/cactus ${arg})
 #endmacro()
 
@@ -14,28 +14,26 @@ if (NOT CACTUS_ROOT)
   MESSAGE(WARNING "Could not find CACTUS package required by miniTLU producer. Please refer to the documentation on how to obtain the software.")
 endif()
 
-set(CACTUS_BUILD_ROOT ${CACTUS_ROOT} )
-
-set(EXTERN_ERLANG_PREFIX ${CACTUS_BUILD_ROOT}/cactuscore/extern/erlang/RPMBUILD/SOURCES )
+set(EXTERN_ERLANG_PREFIX ${CACTUS_ROOT}/lib/erlang )
 set(EXTERN_ERLANG_BIN_PREFIX ${EXTERN_ERLANG_PREFIX}/bin )
 
-set(EXTERN_BOOST_PREFIX ${CACTUS_BUILD_ROOT}/cactuscore/extern/boost/RPMBUILD/SOURCES )
+set(EXTERN_BOOST_PREFIX ${CACTUS_ROOT} )
 set(EXTERN_BOOST_INCLUDE_PREFIX ${EXTERN_BOOST_PREFIX}/include )
 set(EXTERN_BOOST_LIB_PREFIX ${EXTERN_BOOST_PREFIX}/lib )
 
-set(EXTERN_PUGIXML_PREFIX ${CACTUS_BUILD_ROOT}/cactuscore/extern/pugixml/RPMBUILD/SOURCES )
-set(EXTERN_PUGIXML_INCLUDE_PREFIX ${EXTERN_PUGIXML_PREFIX}/include )
+set(EXTERN_PUGIXML_PREFIX ${CACTUS_ROOT}  )
+set(EXTERN_PUGIXML_INCLUDE_PREFIX ${EXTERN_PUGIXML_PREFIX}/include/pugixml )
 set(EXTERN_PUGIXML_LIB_PREFIX ${EXTERN_PUGIXML_PREFIX}/lib )
 
-set(UHAL_GRAMMARS_PREFIX ${CACTUS_BUILD_ROOT}/cactuscore/uhal/grammars/ )
-set(UHAL_GRAMMARS_INCLUDE_PREFIX ${UHAL_GRAMMARS_PREFIX}/include )
+set(UHAL_GRAMMARS_PREFIX ${CACTUS_ROOT} )
+set(UHAL_GRAMMARS_INCLUDE_PREFIX ${UHAL_GRAMMARS_PREFIX}/include/uhal/grammars )
 set(UHAL_GRAMMARS_LIB_PREFIX ${UHAL_GRAMMARS_PREFIX}/lib )
 
-set(UHAL_LOG_PREFIX ${CACTUS_BUILD_ROOT}/cactuscore/uhal/log/ )
-set(UHAL_LOG_INCLUDE_PREFIX ${UHAL_LOG_PREFIX}/include )
+set(UHAL_LOG_PREFIX ${CACTUS_ROOT} )
+set(UHAL_LOG_INCLUDE_PREFIX ${UHAL_LOG_PREFIX}/include/uhal )
 set(UHAL_LOG_LIB_PREFIX ${UHAL_LOG_PREFIX}/lib )
 
-set(UHAL_UHAL_PREFIX ${CACTUS_BUILD_ROOT}/cactuscore/uhal/uhal/ )
+set(UHAL_UHAL_PREFIX ${CACTUS_ROOT} )
 set(UHAL_UHAL_INCLUDE_PREFIX ${UHAL_UHAL_PREFIX}/include )
 set(UHAL_UHAL_LIB_PREFIX ${UHAL_UHAL_PREFIX}/lib )
 
