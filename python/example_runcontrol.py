363bf02eeec857b1a0d491140beaf38d5c983f4c
Hanno Perrey <hanno.perrey@desy.de>
2014-03-25 14:27:33 +0100

    added example for using python wrapper to script run control

---
+++ python/example_runcontrol.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python2
+execfile('PyEUDAQWrapper.py') # load ctypes wrapper
+from time import sleep
+
+print "Starting RunControl"
+prc = PyRunControl("44000") # listen to address 44000
+sleep(1)
+print "Number of active connections: ", prc.NumConnections
+
+# wait for more than one active connection to appear
+while True:
+    sleep(1)
+    print "Number of active connections: ", prc.NumConnections
+    if prc.NumConnections > 1:
+        break
+
+# load configuration file
+prc.Configure("ExampleConfig")
+sleep(2) # wait for a couple of seconds so that the connections can receive the config
+
+# counter variables for simple timeout
+i = 0
+maxwait = 100
+waittime = .5
+# wait until all connections answer with 'OK'
+while i<maxwait and not prc.AllOk:
+    sleep(waittime)
+    print "Waiting for configure for ",i*waittime," seconds, number of active connections: ", prc.NumConnections
+    i+=1
+# check if all are configured
+if prc.AllOk:
+    print "Successfullly configured!"
+else:
+    print "Not all connections returned 'OK' after ",maxwait*waittime," seconds!"
+    abort()
+# start the run
+prc.StartRun()
+
+# wait for run start
+i = 0
+while i<maxwait and not prc.AllOk:
+    sleep(waittime)
+    print "Waiting for run start for ",i*waittime," seconds, number of active connections: ", prc.NumConnections
+    i+=1
+# check if all connected commandreceivers started up
+if prc.AllOk:
+    print "Successfullly started run!"
+else:
+    print "Not all connections returned 'OK' after ",maxwait*waittime," seconds!"
+    abort()
+
+# main run loop
+while True:
+    sleep(.1)
+
+# stop run
+prc.StopRun()
