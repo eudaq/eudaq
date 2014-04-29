127be49c0184f3650df17657ff3802e6ac14f3ac
Hanno Perrey <hanno.perrey@desy.de>
2014-04-29 14:37:11 +0200

    removed left-over counted_ptr instance

--- main/python/src/PyRunControl.cpp
+++ main/python/src/PyRunControl.cpp
@@ -8,7 +8,7 @@ class PyRunControl : public eudaq::RunControl {
     PyRunControl(const std::string & listenaddress)
       : eudaq::RunControl(listenaddress)
     {}
-    void OnReceive(const eudaq::ConnectionInfo & id, counted_ptr<eudaq::Status> status) {
+  void OnReceive(const eudaq::ConnectionInfo & id, std::shared_ptr<eudaq::Status> status) {
       std::cout << "Receive:    " << *status << " from " << id << std::endl;
     }
     void OnConnect(const eudaq::ConnectionInfo & id) {
