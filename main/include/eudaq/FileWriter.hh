3fd643f8a4ebcd549df397a69d545d0e8ae81cdb
Rpeschke <Richard.Peschke@desy.de>
2014-04-30 15:31:17 +0200

    Introduced some Helper functions to make use of the new Processor Template class

--- main/include/eudaq/FileWriter.hh
+++ main/include/eudaq/FileWriter.hh
@@ -4,6 +4,8 @@
 #include "eudaq/DetectorEvent.hh"
 #include <vector>
 #include <string>
+#define TAGNAME_OUTPUTPATTER "outpattern"
+
 
 namespace eudaq {
 
@@ -11,13 +13,26 @@ namespace eudaq {
     public:
       FileWriter();
       virtual void StartRun(unsigned runnumber) = 0;
-      virtual void WriteEvent(const DetectorEvent &) = 0;
+	  virtual void WriteEvent(const DetectorEvent &) = 0;
       virtual unsigned long long FileBytes() const = 0;
       void SetFilePattern(const std::string & p) { m_filepattern = p; }
       virtual ~FileWriter() {}
     protected:
       std::string m_filepattern;
   };
+
+ 
+  inline void helper_setParameter(FileWriter& writer,const std::string& tagName,const std::string& tagValue){
+	if (tagName.compare(TAGNAME_OUTPUTPATTER)==0)
+	{
+		//std::cout<<"set output pattern to: "<<tagValue<<std::endl;
+		writer.SetFilePattern(tagValue);
+	}
+	  
+  }
+  inline void helper_ProcessEvent(FileWriter& writer,const DetectorEvent &ev){writer.WriteEvent(ev);}
+  inline void helper_StartRun(FileWriter& writer,unsigned runnumber){writer.StartRun(runnumber);}
+  inline void helper_EndRun(FileWriter& writer){};
 
 
   class DLLEXPORT FileWriterFactory {
