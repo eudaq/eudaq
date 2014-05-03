1351181c0a01d765b17717fb2ca4da8dd1de70ac
RPeschke <Richard.Peschke@desy.de>
2014-05-03 22:41:54 +0200

    renamed files

--- rootInterface/src/makeDict.bat
+++ rootInterface/src/makeDict.bat
@@ -1,5 +1,5 @@
 path=C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\bin;C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE;%path%
-del *SCTDict.*
+del *ROOTProducer_Dict.*
 
-%ROOTSYS%\bin\rootcint.exe SCTDict.cxx -c -p ..\inc\SCTProducer.h
+%ROOTSYS%\bin\rootcint.exe ROOTProducer_Dict.cxx -c -p ..\inc\ROOTProducer.h
 pause
