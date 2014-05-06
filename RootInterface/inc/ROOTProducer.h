246ff78f722596422c09c653e81c41e5bbcb90eb
RPeschke <Richard.Peschke@desy.de>
2014-05-06 17:54:25 +0200

    added feature: now one can set a pointer to a data array which will be sent on SendEvent()

--- RootInterface/inc/ROOTProducer.h
+++ RootInterface/inc/ROOTProducer.h
@@ -24,12 +24,11 @@ class DLLEXPORT ROOTProducer
 	RQ_OBJECT("ROOTProducer")
 public:
 
-	// The constructor must call the eudaq::Producer constructor with the name
-	// and the runcontrol connection string, and initialize any member variables.
+
 	ROOTProducer(const char* name,const char* runcontrol);
 	ROOTProducer();
 	~ROOTProducer();
-	// This is just an example, adapt it to your hardware
+	
 
 	void Connect2RunControl(const char* name,const char* runcontrol);
 	bool getConnectionStatus();
@@ -41,21 +40,26 @@ public:
 	bool ConfigurationSatus();
 	int getConfiguration(const char* tag, int DefaultValue);
 	int getConfiguration(const char* tag, const char* defaultValue,char* returnBuffer,Int_t sizeOfReturnBuffer);
+  void getConfiguration(const char* tag);
+  void emitConfiguration(const char* answer);
 
 
 
 	void createNewEvent();
-	void setTimeStamp(unsigned long long TimeStamp);
+	void setTimeStamp(ULong64_t TimeStamp);
 	void setTimeStamp2Now();
 	void setTag(const char* tag,const char* Value);
 	void AddPlane2Event(unsigned plane,const std::vector<unsigned char>& inputVector);
 	void AddPlane2Event(unsigned plane,const bool* inputVector,size_t Elements);
   void AddPlane2Event(unsigned MODULE_NR, int ST_STRIPS_PER_LINK , bool* evtr_strm0,bool* evtr_strm1);
     
+  void addDataPointer(unsigned plane,const bool* inputVector,size_t Elements);
+
+
 	void sendEvent();
 
 	// signals
-	void send_start_run();    //async
+
 	void send_onStart();      //sync 
 	void send_onConfigure();  // sync
 	void send_onStop();       // sync
@@ -67,19 +71,19 @@ public:
 	//status flags
 	void checkStatus();
 
-	void getOnStart(bool* onStart);
+
   bool getOnStart();
 	void setOnStart(bool newStat);
 
-	void  getOnConfigure(bool* onConfig);
+
   bool  getOnConfigure();
 	void setOnconfigure(bool newStat);
 
-	void getOnStop(bool* onStop);
+
   bool getOnStop();
 	void setOnStop(bool newStat);
 
-	void getOnTerminate(bool* onTerminate);
+
   bool getOnTerminate();
 	void setOnTerminate(bool newStat);
 
