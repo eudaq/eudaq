5e5d37591116c8684cc98977267abde3ee5cb999
Hanno Perrey <hanno.perrey@desy.de>
2014-05-06 18:38:53 +0200

    cosmetic changes to pydatacollector output messages

--- main/python/src/PyDataCollector.cpp
+++ main/python/src/PyDataCollector.cpp
@@ -14,12 +14,12 @@ class PyDataCollector : public eudaq::DataCollector {
   {}
     void OnConnect(const eudaq::ConnectionInfo & id) {
       DataCollector::OnConnect(id);
-      std::cout << "Connect:    " << id << std::endl;
+      std::cout << "[PyDataCollector] Connect:    " << id << std::endl;
     }
     virtual void OnConfigure(const eudaq::Configuration & param) {
-      std::cout << "Configuring (" << param.Name() << ")..." << std::endl;
+      std::cout << "[PyDataCollector] Configuring (" << param.Name() << ")..." << std::endl;
       DataCollector::OnConfigure(param);
-      std::cout << "...Configured (" << param.Name() << ")" << std::endl;
+      std::cout << "[PyDataCollector] ...Configured (" << param.Name() << ")" << std::endl;
       SetStatus(eudaq::Status::LVL_OK, "Configured (" + param.Name() + ")");
     }
     virtual void OnStartRun(unsigned param) {
@@ -35,11 +35,11 @@ class PyDataCollector : public eudaq::DataCollector {
     //     return false;
     //   }
     virtual void OnTerminate() {
-      std::cout << "Terminating" << std::endl;
+      std::cout << "[PyDataCollector] Terminating" << std::endl;
     }
 
     virtual void OnReset() {
-      std::cout << "Reset" << std::endl;
+      std::cout << "[PyDataCollector] Reset" << std::endl;
       SetStatus(eudaq::Status::LVL_OK);
     }
     //   virtual void OnStatus() {
@@ -47,7 +47,7 @@ class PyDataCollector : public eudaq::DataCollector {
     //     //std::cout << "Status - " << m_status << std::endl;
     //   }
     virtual void OnUnrecognised(const std::string & cmd, const std::string & param) {
-      std::cout << "Unrecognised: (" << cmd.length() << ") " << cmd;
+      std::cout << "[PyDataCollector] Unrecognised: (" << cmd.length() << ") " << cmd;
       if (param.length() > 0) std::cout << " (" << param << ")";
       std::cout << std::endl;
       SetStatus(eudaq::Status::LVL_WARN, "Just testing");
