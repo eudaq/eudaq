a9196c77688842d92f69619942e5a77101c3c73b
Hanno Perrey <hanno.perrey@desy.de>
2014-05-06 18:37:58 +0200

    added proper connection management to python run control interface wrapper
    - now handles the status of the connection correct
    - allows printing of connection status

--- main/python/src/PyRunControl.cpp
+++ main/python/src/PyRunControl.cpp
@@ -3,32 +3,78 @@
 #include "eudaq/OptionParser.hh"
 #include <iostream>
 
+class RCConnections {
+public:
+  RCConnections(const eudaq::ConnectionInfo & id): m_id(id.Clone()){};
+  eudaq::Status GetStatus() const { return m_status; }
+  int GetLevel() const { return m_status.GetLevel(); }
+  bool IsConnected() const { return m_id->IsEnabled(); }
+  void SetConnected(bool con) { m_id->SetState(2*con - 1); }
+  const eudaq::ConnectionInfo & GetId() const { return *m_id; }
+  void SetStatus(const eudaq::Status & status) { m_status = status; }
+private:
+  std::shared_ptr<eudaq::ConnectionInfo> m_id;
+  eudaq::Status m_status;
+};
+
 class PyRunControl : public eudaq::RunControl {
-  public:
-    PyRunControl(const std::string & listenaddress)
-      : eudaq::RunControl(listenaddress)
-    {}
+public:
+  PyRunControl(const std::string & listenaddress)
+    : eudaq::RunControl(listenaddress)
+  {}
   void OnReceive(const eudaq::ConnectionInfo & id, std::shared_ptr<eudaq::Status> status) {
-      std::cout << "Receive:    " << *status << " from " << id << std::endl;
+    std::cout << "[Run Control] Received:    " << id << std::endl;
+    for (size_t i = 0; i < m_connections.size(); ++i) {
+      if (id.Matches(m_connections[i].GetId())) {
+	m_connections[i].SetStatus(*status);
+	break;
+      }
     }
-    void OnConnect(const eudaq::ConnectionInfo & id) {
-      std::cout << "Connect:    " << id << std::endl;
+  }
+  void OnConnect(const eudaq::ConnectionInfo & id) {
+    std::cout << "[Run Control] Connection established:    " << id << std::endl;
+    for (size_t i = 0; i < m_connections.size(); ++i) {
+      if (!m_connections.at(i).IsConnected() && id.Matches(m_connections.at(i).GetId())) {
+	m_connections[i] = RCConnections(id);
+	return;
+      }
     }
-    void OnDisconnect(const eudaq::ConnectionInfo & id) {
-      std::cout << "Disconnect: " << id << std::endl;
+    m_connections.push_back(RCConnections(id));
+  }
+  void OnDisconnect(const eudaq::ConnectionInfo & id) {
+    std::cout << "[Run Control] Disconnected:    " << id << std::endl;
+    for (size_t i = 0; i < m_connections.size(); ++i) {
+      if (id.Matches(m_connections[i].GetId())) {
+	m_connections[i].SetConnected(false);
+	break;
+      }
     }
+  }
+
+  bool PrintConnections(){
+    std::cout << "Connections (" << m_connections.size() << ")" << std::endl;
+    for (uint16_t i = 0; i < m_connections.size(); ++i) {
+      std::cout << "  " << m_connections.at(i).GetId()<< ": " << m_connections.at(i).GetStatus() << std::endl;
+    }
+  }
+
   uint16_t GetRunNumber(){return RunControl::m_runnumber;}
 
   bool AllOk(){
     bool ok = true;
-    for (size_t i = 0; i < m_cmdserver->NumConnections(); ++i) {
-      if (m_cmdserver->GetConnection(i).GetState() != 2) {
+    for (size_t i = 0; i < m_connections.size(); ++i) {
+      if (m_connections.at(i).GetLevel() != eudaq::Status::LVL_OK) {
 	ok = false;
 	break;
       }
     }
     return ok;
   }
+
+  size_t NumConnections(){return m_connections.size();}
+
+private:
+  std::vector<RCConnections> m_connections;
 };
 
 // ctypes can only talk to C functions -- need to provide them through 'extern "C"'
@@ -38,6 +84,7 @@ extern "C" {
   void PyRunControl_StartRun(PyRunControl *prc){prc->StartRun();}
   void PyRunControl_StopRun(PyRunControl *prc){prc->StopRun();}
   void PyRunControl_Configure(PyRunControl *prc, char *cfg){prc->Configure(std::string(cfg));}
+  void PyRunControl_PrintConnections(PyRunControl *prc){prc->PrintConnections();}
   size_t PyRunControl_NumConnections(PyRunControl *prc){return prc->NumConnections();}
   bool PyRunControl_AllOk(PyRunControl *prc){return prc->AllOk();}
   uint16_t PyRunControl_GetRunNumber(PyRunControl *prc){return prc->GetRunNumber();}
