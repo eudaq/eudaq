79cd584d32005eb34102fd11acee196d75947699
Ulf Behrens <ulf.behrens@me.com>
2014-05-08 18:37:54 +0200

    first version

--- main/lib/src/AidaPacket.cc
+++ main/lib/src/AidaPacket.cc
@@ -2,75 +2,51 @@
 #include <iostream>
 #include <time.h>
 
-#include "eudaq/Event.hh"
+#include "eudaq/BufferSerializer.hh"
+#include "eudaq/AidaPacket.hh"
 
 
 namespace eudaq {
+	EUDAQ_DEFINE_PACKET(EventPacket, str2type( "-EVWRAP-") );
 
-  namespace {
 
-    static const char * const FLAGNAMES[] = {
-      "BORE",
-      "EORE",
-      "HITS",
-      "FAKE",
-      "SIMU"
-    };
+	void AidaPacket::SerializeHeader( Serializer& s ) const {
+		uint64_t* arr = (uint64_t *)&m_header;
+		for ( int i = 0; i < sizeof( m_header ) / sizeof( uint64_t ); i++ )
+			s.write( arr[i] );
+	}
 
-  }
+	AidaPacket::PacketHeader AidaPacket::DeserializeHeader( Deserializer& ds ) {
+		PacketHeader header;
+		uint64_t* arr = (uint64_t *)&header;
+		for ( int i = 0; i < sizeof( header ) / sizeof( uint64_t ); i++ )
+			ds.read( arr[i] );
+		return header;
+	}
 
-  Event::Event(Deserializer & ds) {
-    ds.read(m_flags);
-    ds.read(m_runnumber);
-    ds.read(m_eventnumber);
-    ds.read(m_timestamp);
-    ds.read(m_tags);
-  }
-
-  void Event::Serialize(Serializer & ser) const {
-    //std::cout << "Serialize id = " << std::hex << get_id() << std::endl;
-    ser.write(get_id());
-    ser.write(m_flags);
-    ser.write(m_runnumber);
-    ser.write(m_eventnumber);
-    ser.write(m_timestamp);
-    ser.write(m_tags);
-  }
-
-  void Event::Print(std::ostream & os) const {
-    os << "Type=" << id2str(get_id()) << ":" << GetSubType()
-      << ", Number=" << m_runnumber << "." << m_eventnumber;
-    if (m_timestamp != NOTIMESTAMP)
-      os << ", Time=0x" << to_hex(m_timestamp, 16);
-    if (m_flags) {
-      unsigned f = m_flags;
-      bool first = true;
-      for (size_t i = 0; f > 0; ++i, f >>= 1) {
-        if (f & 1) {
-          os << (first ? ", Flags=" : ",")
-            << (i < sizeof FLAGNAMES / sizeof *FLAGNAMES ? std::string(FLAGNAMES[i]) : to_string(i));
-          first = false;
-        }
-      }
+    const uint64_t * const AidaPacket::bit_mask() {
+    	static uint64_t* array = NULL;
+    	if ( !array ) {
+    		array = new uint64_t[65];
+    		array[0] = 0;
+    		array[1] = 1;
+    		for ( int i = 2; i <= 64; i++ ) {
+    			array[i] = (array[i - 1] << 1) + 1;
+    		}
+    	}
+    	return array;
     }
-    if (m_tags.size() > 0) {
-      for (map_t::const_iterator i = m_tags.begin(); i != m_tags.end(); ++i) {
-        os << (i == m_tags.begin() ? ", {" : ", ")  << i->first << "=" << i->second;
-      }
-      os << "}";
-    }
-  }
 
-  unsigned Event::str2id(const std::string & str) {
-    unsigned long result = 0;
+
+  uint64_t AidaPacket::str2type(const std::string & str) {
+    uint64_t result = 0;
     for (size_t i = 0; i < 4; ++i) {
       if (i < str.length()) result |= str[i] << (8*i);
     }
-    //std::cout << "str2id(" << str << ") = " << std::hex << result << std::dec << std::endl;
     return result;
   }
 
-  std::string Event::id2str(unsigned id) {
+  std::string AidaPacket::type2str(uint64_t id) {
     //std::cout << "id2str(" << std::hex << id << std::dec << ")" << std::flush;
     std::string result(4, '\0');
     for (int i = 0; i < 4; ++i) {
@@ -87,38 +63,70 @@ namespace eudaq {
     return result;
   }
 
-  Event & Event::SetTag(const std::string & name, const std::string & val) {
-    m_tags[name] = val;
-    return *this;
+  void AidaPacket::Print(std::ostream & os) const {
+    os << "Type=" << type2str( GetPacketType() )
+      << ", packet#=" << GetPacketNumber();
   }
 
-  std::string Event::GetTag(const std::string & name, const std::string & def) const {
-    std::map<std::string, std::string>::const_iterator i = m_tags.find(name);
-    if (i == m_tags.end()) return def;
-    return i->second;
-  }
-
-  void Event::SetTimeStampToNow()
-  {
-	  m_timestamp=static_cast<unsigned long long>(clock());
-  }
-
-  std::ostream & operator << (std::ostream &os, const Event &ev) {
-    ev.Print(os);
+  std::ostream & operator << (std::ostream &os, const AidaPacket &packet) {
+    packet.Print(os);
     return os;
   }
 
-  EventFactory::map_t & EventFactory::get_map() {
+
+
+
+  EventPacket::EventPacket( const Event & ev ) : m_ev( &ev ) {
+    	m_header.marker        = PACKET_MARKER;
+    	m_header.packetType    = get_type();
+    	m_header.packetSubType = 0;
+    	m_header.packetNumber  = m_ev->GetEventNumber();
+
+    	m_meta_data.push_back( buildMetaData( false, META_DATA.TYPE::RUN_NUMBER, m_ev->GetRunNumber() ) );
+    	m_meta_data.push_back( buildMetaData( false, META_DATA.TYPE::TRIGGER_COUNTER, m_ev->GetEventNumber() ) );
+    	m_meta_data.push_back( buildMetaData( false, META_DATA.TYPE::TRIGGER_TIMESTAMP, m_ev->GetTimestamp() ) );
+  }
+
+
+  EventPacket::EventPacket( PacketHeader& header, Deserializer & ds) {
+		m_header = header;
+		ds.read( m_meta_data );
+		m_ev = EventFactory::Create( ds );
+		ds.read( checksum );
+  }
+
+
+  void EventPacket::Serialize(Serializer & ser) const {
+	  std::cout << "Serialize ev# = " << std::hex << GetPacketNumber() << std::endl;
+	  SerializeHeader( ser );
+	  ser.write( m_meta_data );
+	  m_ev->Serialize( ser );
+	  ser.write( ser.GetCheckSum() );
+  }
+
+
+
+
+  AidaPacket * PacketFactory::Create( Deserializer & ds) {
+	  AidaPacket::PacketHeader header = AidaPacket::DeserializeHeader( ds );
+	  int id = header.packetType;
+      //std::cout << "Create id = " << std::hex << id << std::dec << std::endl;
+      packet_creator cr = GetCreator(id);
+      if (!cr) EUDAQ_THROW("Unrecognised packet type (" + to_string(id) + ")");
+      return cr( header, ds);
+  };
+
+  PacketFactory::map_t & PacketFactory::get_map() {
     static map_t s_map;
     return s_map;
   }
 
-  void EventFactory::Register(unsigned long id, EventFactory::event_creator func) {
+  void PacketFactory::Register( uint64_t id, PacketFactory::packet_creator func) {
     // TODO: check id is not already in map
     get_map()[id] = func;
   }
 
-  EventFactory::event_creator EventFactory::GetCreator(unsigned long id) {
+  PacketFactory::packet_creator PacketFactory::GetCreator(int id) {
     // TODO: check it exists...
     return get_map()[id];
   }
