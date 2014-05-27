008df302537da5046c6096cacbe3961eee60454f
Hanno Perrey <hanno.perrey@desy.de>
2014-05-27 14:26:43 +0200

    added CDash information for nightly regression tests

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -7,7 +7,7 @@
 set(CTEST_PROJECT_NAME "EUDAQ")
 set(CTEST_NIGHTLY_START_TIME "03:05:00 UTC")
 
-#set(CTEST_DROP_METHOD "http")
-#set(CTEST_DROP_SITE "aidasoft.desy.de")
-#set(CTEST_DROP_LOCATION "/CDash/submit.php?project=EUDAQ")
-#set(CTEST_DROP_SITE_CDASH TRUE)
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "aidasoft.desy.de")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=EUDAQ")
+set(CTEST_DROP_SITE_CDASH TRUE)
