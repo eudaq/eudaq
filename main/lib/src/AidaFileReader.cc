b9dfc6367521729b938c5c60c56e56adb2662bc7
behrens <ulf.behrens@desy.de>
2014-07-02 10:18:15 +0200

    not working, but compiling

--- main/lib/src/AidaFileReader.cc
+++ main/lib/src/AidaFileReader.cc
@@ -18,14 +18,17 @@ namespace eudaq {
   }
 
   bool AidaFileReader::NextPacket(size_t skip) {
+	  return false;
+/*
     std::shared_ptr<eudaq::AidaPacket> packet = nullptr;
     bool result = m_des.ReadPacket(m_ver, ev, skip);
     if (ev) m_ev =  ev;
     return result;
+*/
   }
 
   unsigned AidaFileReader::RunNumber() const {
-    return m_ev->GetRunNumber();
+    return -1; //m_ev->GetRunNumber();
   }
 
   const AidaPacket & AidaFileReader::GetPacket() const {
