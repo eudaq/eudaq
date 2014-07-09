7e33af433ce972a3e9a22f835c29fbef289a6092
behrens <ulf.behrens@desy.de>
2014-07-09 18:01:31 +0200

    module name changed to libPyIndexReader

--- etc/pybindgen/module-autoscan.py
+++ etc/pybindgen/module-autoscan.py
@@ -6,7 +6,7 @@ from pybindgen import FileCodeSink
 from pybindgen.gccxmlparser import ModuleParser
 
 def my_module_gen():
-    module_parser = ModuleParser('py_eudaq', '::')
+    module_parser = ModuleParser('libPyIndexReader', '::')
     module_parser.parse([sys.argv[1]], includes=['"eudaq/IndexReader.hh"'], pygen_sink=FileCodeSink(sys.stdout))
 
 if __name__ == '__main__':
