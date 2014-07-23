97065df6c89cbff7716616b50b7bda0daaee3f69
behrens <ulf.behrens@desy.de>
2014-07-23 13:03:38 +0200

    tuning

--- etc/pybindgen/module-autoscan.py
+++ etc/pybindgen/module-autoscan.py
@@ -6,8 +6,11 @@ from pybindgen import FileCodeSink
 from pybindgen.gccxmlparser import ModuleParser
 
 def my_module_gen():
-    module_parser = ModuleParser('libPyIndexReader', '::')
-    module_parser.parse([sys.argv[1]], includes=['"eudaq/IndexReader.hh"'], pygen_sink=FileCodeSink(sys.stdout))
+    libName = 'libPy' + sys.argv[2]
+    className = sys.argv[2] + '.hh'
+    includeFile = sys.argv[1] + '/' + className
+    module_parser = ModuleParser( libName, '::')
+    module_parser.parse([includeFile], includes=['"' + includeFile + '"'], pygen_sink=FileCodeSink(sys.stdout))
 
 if __name__ == '__main__':
     my_module_gen()
