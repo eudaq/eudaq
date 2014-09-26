82975862a22c625240bb00162e20ad7fd900094c
Simon Spannagel <simon.spannagel@desy.de>
2014-09-26 17:01:07 +0200

    CMSPixelConverter: indendation, more debug output removed

--- main/lib/plugins/CMSPixelConverterPlugin.cc
+++ main/lib/plugins/CMSPixelConverterPlugin.cc
@@ -19,96 +19,96 @@ namespace eudaq {
   // Events are not converted so far
   // This class is only here to prevent runtime errors
   class CMSPixelConverterPlugin : public DataConverterPlugin {
-    public:
-      // virtual unsigned GetTriggerID(const Event & ev) const{}; 
+  public:
+    // virtual unsigned GetTriggerID(const Event & ev) const{}; 
 
-      virtual void Initialize(const Event & bore, const Configuration & cnf)
-      {
-        std::cout<<"CMSPixelConverterPlugin::Initialize" << std::endl;
+    virtual void Initialize(const Event & bore, const Configuration & cnf)
+    {
+      std::cout<<"CMSPixelConverterPlugin::Initialize" << std::endl;
         
-        DeviceDictionary* devDict;
-        std::string roctype = bore.GetTag("ROCTYPE", "");
-        m_roctype = devDict->getInstance()->getDevCode(roctype);
-        if (m_roctype == 0x0)
-          EUDAQ_ERROR("Roctype" + to_string((int) m_roctype) + " not propagated correctly to CMSPixelConverterPlugin");
+      DeviceDictionary* devDict;
+      std::string roctype = bore.GetTag("ROCTYPE", "");
+      m_roctype = devDict->getInstance()->getDevCode(roctype);
+      if (m_roctype == 0x0)
+	EUDAQ_ERROR("Roctype" + to_string((int) m_roctype) + " not propagated correctly to CMSPixelConverterPlugin");
        
+    }
+
+    virtual bool GetStandardSubEvent(StandardEvent & out, const Event & in) const {
+
+      // transform data of form char* to vector<int16_t>
+      EUDAQ_DEBUG("m_roctype " + eudaq::to_string((int) m_roctype));
+      if(m_roctype != 0x0){
+	//CMSPixel::Log::ReportingLevel() = CMSPixel::Log::FromString("DEBUG3");
+	CMSPixel::CMSPixelEventDecoderDigital evtDecoder(1, FLAG_12BITS_PER_WORD, m_roctype);
+
+	StandardPlane plane(7, EVENT_TYPE);
+	// Set the number of pixels
+	plane.SetSizeRaw(ROC_NUMCOLS, ROC_NUMROWS);
+
+	std::vector<uint16_t> rawdata = TransformRawData(in);
+	std::vector<CMSPixel::pixel> * evt = new std::vector<CMSPixel::pixel>;
+	int status = evtDecoder.get_event(rawdata, evt);
+	EUDAQ_DEBUG("Decoding status: " + status);
+
+	// Store all decoded pixels:
+	for(std::vector<CMSPixel::pixel>::iterator it = evt->begin(); it != evt->end(); ++it){
+	  plane.PushPixel(it->col, it->row, it->raw);
+	}
+
+	plane.SetTLUEvent(GetTriggerID(in));
+	out.AddPlane(plane);
+	delete evt;
+	return true;
       }
-
-      virtual bool GetStandardSubEvent(StandardEvent & out, const Event & in) const {
-        std::cout << "Call CMSPixelConverterPlugin::GetStandardSubEvent" << std::endl;
-
-        // transform data of form char* to vector<int16_t>
-        EUDAQ_DEBUG("m_roctype " + eudaq::to_string((int) m_roctype));
-        if(m_roctype != 0x0){
-          //CMSPixel::Log::ReportingLevel() = CMSPixel::Log::FromString("DEBUG3");
-          CMSPixel::CMSPixelEventDecoderDigital evtDecoder(1, FLAG_12BITS_PER_WORD, m_roctype);
-
-          StandardPlane plane(7, EVENT_TYPE);
-          // Set the number of pixels
-          plane.SetSizeRaw(ROC_NUMCOLS, ROC_NUMROWS);
-
-	  std::vector<uint16_t> rawdata = TransformRawData(in);
-          std::vector<CMSPixel::pixel> * evt = new std::vector<CMSPixel::pixel>;
-          int status = evtDecoder.get_event(rawdata, evt);
-          EUDAQ_DEBUG("Decoding status: " + status);
-
-	  // Store all decoded pixels:
-          for(std::vector<CMSPixel::pixel>::iterator it = evt->begin(); it != evt->end(); ++it){
-            plane.PushPixel(it->col, it->row, it->raw);
-          }
-
-	  plane.SetTLUEvent(GetTriggerID(in));
-          out.AddPlane(plane);
-          delete evt;
-          return true;
-        }
-        else{
-          EUDAQ_ERROR("Invalid ROC type\n");
-          return false;
-        }
+      else{
+	EUDAQ_ERROR("Invalid ROC type\n");
+	return false;
       }
+    }
 
-    private:
-      CMSPixelConverterPlugin() : DataConverterPlugin(EVENT_TYPE),
-      m_roctype(0)
+  private:
+    CMSPixelConverterPlugin() : DataConverterPlugin(EVENT_TYPE),
+				m_roctype(0)
     {
       std::cout<<"CMSPixelConverterPlugin Event_Type: "<<EVENT_TYPE << std::endl;
 
     }
-      static std::vector<uint16_t> TransformRawData(const RawDataEvent & in) {
-        // transform data of form char* to vector<int16_t>
-        std::vector<uint16_t> rawData;
 
-        if(in.NumBlocks() != 1){
-          EUDAQ_WARN("event " + to_string(in.GetEventNumber())
-              + ": invalid number of data blocks ");        
-          return rawData;
-        }
+    static std::vector<uint16_t> TransformRawData(const RawDataEvent & in) {
+      // transform data of form char* to vector<int16_t>
+      std::vector<uint16_t> rawData;
 
-        const std::vector<uint8_t> & block = in.GetBlock(0);
-
-        int size = block.size();
-        if(size < 2){
-          EUDAQ_WARN("event " + to_string(in.GetEventNumber())
-                       + ": data block is too small");
-          return rawData;
-        }
-        int i = 0;
-        while(i < size-1){
-          uint16_t temp = ((uint16_t)block.data()[i+1] << 8) | block.data()[i];
-          rawData.push_back(temp);
-          i+=2;
-        }
-        std::cout << "rawData roc header " << std::hex << rawData[0] <<  std::dec << std::endl;
-       return rawData;
-      }
-      static std::vector<uint16_t> TransformRawData(const Event & in)
-      {
-        return TransformRawData(dynamic_cast<const RawDataEvent &>(in));
+      if(in.NumBlocks() != 1){
+	EUDAQ_WARN("event " + to_string(in.GetEventNumber())
+		   + ": invalid number of data blocks ");        
+	return rawData;
       }
 
-      static CMSPixelConverterPlugin m_instance;
-      uint8_t m_roctype;
+      const std::vector<uint8_t> & block = in.GetBlock(0);
+
+      int size = block.size();
+      if(size < 2) {
+	EUDAQ_WARN("event " + to_string(in.GetEventNumber())
+		   + ": data block is too small");
+	return rawData;
+      }
+      int i = 0;
+      while(i < size-1) {
+	uint16_t temp = ((uint16_t)block.data()[i+1] << 8) | block.data()[i];
+	rawData.push_back(temp);
+	i+=2;
+      }
+      return rawData;
+    }
+
+    static std::vector<uint16_t> TransformRawData(const Event & in)
+    {
+      return TransformRawData(dynamic_cast<const RawDataEvent &>(in));
+    }
+
+    static CMSPixelConverterPlugin m_instance;
+    uint8_t m_roctype;
   };
 
   CMSPixelConverterPlugin CMSPixelConverterPlugin::m_instance;
