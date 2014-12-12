e8dbf36a660be695d9ba6f29642403a636684e27
Felix Reidt <Felix.Reidt@cern.ch>
2014-12-12 10:33:22 +0100

    - introduced the TriggerDelay as parameter - Jacobus' golden configs

--- producers/palpidefs/scripts/config_set_generation/conf_generator.py
+++ producers/palpidefs/scripts/config_set_generation/conf_generator.py
@@ -32,6 +32,8 @@ class settings:
         # dut position and position related settings
         self.dut_pos_list=[]
         self.scs_list    =[]
+        # trigger delay for the DUTs
+        self.trg_dly=int(75)
 
     def generate_files( self ):
         for i_vbb in range(len(self.vbb_prio)):
@@ -49,8 +51,8 @@ class settings:
                             for i_dut_pos in range(len(self.dut_pos_list)):
                                 dut_pos=self.dut_pos_list[i_dut_pos]
                                 scs    =self.scs_list    [i_dut_pos]
-                                args   =(self.i_file, vbb, vcasn, ithr, vaux, vrst, idb, dut_pos, scs)
-                                cmd    ="/bin/bash conf_gen_helper.sh %d %d %d %d %d %d %d %d %d"%args
+                                args   =(self.i_file, vbb, vcasn, ithr, vaux, vrst, idb, dut_pos, scs, self.trg_dly)
+                                cmd    ="/bin/bash conf_gen_helper.sh %d %d %d %d %d %d %d %d %d %d"%args
                                 os.system(cmd)
                                 self.i_file+=1
 
@@ -59,7 +61,7 @@ class settings:
 
 s=settings()
 
-### load the standard settings
+#### load the standard settings
 # Vbb (V)
 s.vbb_list=[0,1,2,3,4,5,6]
 s.vbb_prio=[0,3,6,2,4,1,5]
@@ -90,11 +92,97 @@ s.vauxrst_prio=[0]
 # dut position and position related settings (mm)
 s.dut_pos_list=[97,295] # 97 = in the beam, 295 out of the beam
 s.scs_list    =[ 0,  1] # 0  = no S-Curve scan, 1 = do S-Curve scan
+#
+#### apply modifications
+#s.vbb_prio=[4] # -4V only
+#s.ithr_prio=[0,1,2,3,4,5] # omit Ithr=70DAC
+#s.vcasn_prio=[2,3,4]
+#s.generate_files()
+#
+#s.vbb_prio=[0,1,2,3,4,5,6] # all back-bias voltages
+#s.vcasn_prio=[2,3,4,0,1]
+#s.ithr_prio=[6]
+#s.generate_files()
+#
+#s.vbb_prio=[6] # -6V only
+#s.vcasn_prio=[0,1,3,4]    # central value was already measured
+#s.ithr_prio=[0,1,2,3,4,5] # 10-51 DAC
+#s.generate_files()
+#
+#s.vbb_prio=[0,3]
 
-### apply modifications
+#s.i_file=154
+#s.idb_list=[128,196]
+#s.idb_prio=[  1,  0]
+#s.vbb_prio=[0]
+#s.vcasn_prio=[0]
+#s.ithr_prio=[0]
+#s.vaux_list=[[117]]
+#s.vrst_list=[[117]]
+#s.vauxrst_prio=[0]
+## dut position and position related settings (mm)
+#s.dut_pos_list=[97,295] # 97 = in the beam, 295 out of the beam
+#s.scs_list    =[ 0,  1] # 0  = no S-Curve scan, 1 = do S-Curve scan
+#
+#
+#s.vbb_list=[0]
+#s.vcasn_list=[[67]]
+#s.ithr_list=[30]
+#s.trg_dly=61
+#s.generate_files()
+#
+#s.vbb_list=[0]
+#s.vcasn_list=[[70]]
+#s.ithr_list=[35]
+#s.trg_dly=61
+#s.generate_files()
+#
+#s.vbb_list=[0]
+#s.vcasn_list=[[67]]
+#s.ithr_list=[40]
+#s.trg_dly=53
+#s.generate_files()
+#
+#s.vbb_list=[0]
+#s.vcasn_list=[[70]]
+#s.ithr_list=[55]
+#s.trg_dly=45
+#s.generate_files()
+#
+#
+#s.vaux_list=[[125]]
+#s.vrst_list=[[125]]
+#
+#s.vbb_list=[3]
+#s.vcasn_list=[[145]]
+#s.ithr_list=[30]
+#s.trg_dly=81
+#s.generate_files()
+#
+#s.vbb_list=[3]
+#s.vcasn_list=[[150]]
+#s.ithr_list=[45]
+#s.trg_dly=61
+#s.generate_files()
+#
+#s.vbb_list=[3]
+#s.vcasn_list=[[145]]
+#s.ithr_list=[50]
+#s.trg_dly=45
+#s.generate_files()
+#
+#s.vbb_list=[3]
+#s.vcasn_list=[[150]]
+#s.ithr_list=[70]
+#s.trg_dly=41
+#s.generate_files()
+#
+#s.i_file-=154
+
+s.i_file=186
 s.vbb_prio=[4] # -4V only
-s.ithr_prio=[0,1,2,3,4,5] # omit Ithr=70DAC
-s.vcasn_prio=[2,3,4]
+s.ithr_prio=[4,5] # omit Ithr=70DAC
+s.vcasn_prio=[4]
 s.generate_files()
 
 s.vbb_prio=[0,1,2,3,4,5,6] # all back-bias voltages
@@ -106,6 +194,8 @@ s.vbb_prio=[6] # -6V only
 s.vcasn_prio=[0,1,3,4]    # central value was already measured
 s.ithr_prio=[0,1,2,3,4,5] # 10-51 DAC
 s.generate_files()
+
+s.i_file-=186
 
 print "%d config files produced" % s.i_file
 print "Estimated measurement time %d min (%0.1f h) assuming 7.5 min per config file" % (s.i_file*7.5, s.i_file*7.5/60.)
