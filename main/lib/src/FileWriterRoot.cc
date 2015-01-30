32966fc153187e2b4fdfa426c110a2a10c4125eb
RPeschke <Richard.Peschke@desy.de>
2015-01-30 17:35:06 +0100

    Update FileWriterRoot.cc
    removed debug output.

--- main/lib/src/FileWriterRoot.cc
+++ main/lib/src/FileWriterRoot.cc
@@ -91,12 +91,6 @@ namespace eudaq {
         id_x = plane.GetX(ipix);
         id_y = plane.GetY(ipix);
         i_time_stamp =  sev.GetTimestamp();
-		try{ 
-		i_tlu= std::stoull(sev.GetTag("TLU_trigger","15"));
-		}catch(...){
-			std::cout<<" error during converting "<<sev.GetTag("TLU_trigger","15") << " to ull"<<std::endl;
-		}
-
         i_run = sev.GetRunNumber();
         i_event = sev.GetEventNumber();                  
         m_ttree->Fill(); 
