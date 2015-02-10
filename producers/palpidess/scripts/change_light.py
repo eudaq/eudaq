91388db8c799abc79fbbc380ce7ee834108517c6
Felix Reidt <felix.reidt@cern.ch>
2015-02-10 13:48:30 +0100

    avoiding division by zero

--- producers/palpidess/scripts/change_light.py
+++ producers/palpidess/scripts/change_light.py
@@ -9,12 +9,12 @@ def main():
     vlight_dst=float(sys.argv[1]) # value to which the vlight should be ramped
     sour=serial.Serial(h.dev, h.baud_rate, rtscts=h.rtscts)
     vlight_src=h.meas_voltage(sour, h.vlight_chan)
-    if abs((vlight_dst - vlight_src)/vlight_dst) < 1.3e-3:
+    if abs((vlight_dst - vlight_src)/(vlight_dst + 1.e-3)) < 1.3e-3:
         return 0
     h.ramp_from_to(sour, h.vlight_chan, (vlight_src, vlight_dst), 100)
     time.sleep(1)
     vlight_meas=h.meas_voltage(sour, h.vlight_chan)
-    if abs((vlight_dst - vlight_meas)/vlight_dst) < 1.e-3:
+    if abs((vlight_dst - vlight_meas)/(vlight_dst + 1.e-3)) < 1.e-3:
         return 0
     else:
         return 1
