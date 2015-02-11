7f870d51609baff87f9f9bd3c778552c8411dc85
Felix Reidt <felix.reidt@cern.ch>
2015-02-12 00:43:16 +0100

    bug fix for the config generator

--- producers/palpidess/scripts/config_set_generation/conf_generator.py
+++ producers/palpidess/scripts/config_set_generation/conf_generator.py
@@ -133,14 +133,15 @@ s.trig_delay_prio=[  0,  1,  2,  3,  4,  5,  6, 7,  8  ]
 s.generate_files()
 
 # delay scan with light
+s.trig_delay_list=[ 1., 2., 3., 4., 5., 6., 7. ]
 s.trig_delay_prio=[  0,  1,  2,  3,  4,  5,  6 ]
 s.vlight_prio=[  1  ]
 s.generate_files()
 
 # fine delay scan
 s.acq_time_list=[ 1.54 ]
-s.trig_delay_list=[ 0.5, 1.0, 1.5, 2., 2.5, 3., 3.5, 4., 4.5, 5., 5.5, 6. ]
-s.trig_delay_prio=[   0,   1,   2,  3,   4,  5,   6,  7,   8,  9,  10, 11 ]
+s.trig_delay_list=[ 0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6. ]
+s.trig_delay_prio=[   0,   1,   2,   3,   4,   5,   6,   7,    8,  9,  10,  11, 12 ]
 s.generate_files()
 
 
