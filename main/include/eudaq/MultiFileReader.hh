a402fb41e4df010c3c5252f30598cf1dacda4c0f
RPeschke <Richard.Peschke@desy.de>
2015-03-24 19:12:16 +0100

    multiFileReader uses now the Interface from base file reader

--- main/include/eudaq/MultiFileReader.hh
+++ main/include/eudaq/MultiFileReader.hh
@@ -2,45 +2,50 @@
 #define MuliFileReader_h__
 
 #include "Platform.hh"
-#include "eudaq/EventSynchronisationBase.hh"
 
 
 #include <string>
 #include <memory>
 #include <vector>
 
-
-
+#include "eudaq/EventSynchronisationBase.hh"
+#include "eudaq/baseFileReader.hh"
+#include "eudaq/OptionParser.hh"
 
 
 namespace eudaq{
-	class baseFileReader;
+  class Event;
+  class DetectorEvent;
 
-	class Event;
-	class DetectorEvent;
-	class DLLEXPORT multiFileReader{
-	public:
-		multiFileReader(bool sync=true);
+  class DLLEXPORT multiFileReader :public baseFileReader{
+  public:
+    multiFileReader(bool sync = true);
+    multiFileReader(baseFileReader::Parameter_ref parameterList);
+    virtual unsigned RunNumber() const;
+    virtual bool NextEvent(size_t skip = 0);
+    virtual std::shared_ptr<eudaq::Event> GetNextEvent();
+    virtual const eudaq::Event & GetEvent() const;
+    virtual std::shared_ptr<eudaq::Event> getEventPtr() { return m_ev; }
+    const DetectorEvent & GetDetectorEvent() const;
 
-		 unsigned RunNumber() const;
-
-		bool NextEvent(size_t skip = 0);
-		std::string Filename() const { return m_filename; }
-		   const DetectorEvent & GetDetectorEvent() const;
-		   const eudaq::Event & GetEvent() const;
-	
-		void addFileReader(const std::string & filename, const std::string & filepattern = "");
-		void Interrupt() ;
-	private:
-		std::string m_filename;
-		std::shared_ptr<eudaq::DetectorEvent> m_ev;
-		std::vector<std::shared_ptr<eudaq::baseFileReader>> m_fileReaders;
-		SyncBase m_sync;
-		size_t m_eventsToSync;
-		bool m_preaparedForEvents;
-		
-	};
-
+    void addFileReader(const std::string & filename, const std::string & filepattern );
+    void addFileReader(const std::string & filename);
+    void addFileReader(std::unique_ptr<baseFileReader> FileReader);
+    void addSyncAlgorithm(std::unique_ptr<SyncBase> sync);
+    void addSyncAlgorithm(SyncBase::MainType type , SyncBase::Parameter_ref sync);
+    void Interrupt();
+    using strOption_ptr = std::unique_ptr < eudaq::Option<std::string> > ;
+    static strOption_ptr  add_Command_line_option_inputPattern(OptionParser & op);
+    static std::string help_text();
+  private:
+    bool readFiles();
+    std::shared_ptr<eudaq::Event> m_ev;
+    std::vector<std::unique_ptr<eudaq::baseFileReader>> m_fileReaders;
+    std::unique_ptr<SyncBase> m_sync;
+    size_t m_eventsToSync;
+    bool m_preaparedForEvents;
+    bool m_firstEvent=true;
+  };
 
 }
 
--- main/lib/src/MultiFileReader.cc
+++ main/lib/src/MultiFileReader.cc
@@ -1,91 +1,188 @@
 #include "eudaq/MultiFileReader.hh"
-#include "eudaq/AidaFileReader.hh"
 #include "eudaq/FileReader.hh"
 #include "eudaq/baseFileReader.hh"
 
 #include "eudaq/Event.hh"
 #include "eudaq/DetectorEvent.hh"
-void eudaq::multiFileReader::addFileReader( const std::string & filename, const std::string & filepattern /*= ""*/ )
+#include "eudaq/PluginManager.hh"
+namespace eudaq{
+void  multiFileReader::addFileReader( const std::string & filename, const std::string & filepattern )
 {
-  m_fileReaders.push_back(Factory_file_reader(filename, filepattern));
   
-  auto ev = m_fileReaders.back()->GetNextEvent();
-  
-  while (ev->IsBORE())
-  {
-    if (!m_ev)
-    {
-      
-      m_ev = std::make_shared<DetectorEvent>(ev->GetRunNumber(),0,ev->GetTimestamp());
-    }
-    m_sync.addBORE_Event(m_fileReaders.size() - 1, *ev);
-    m_ev->AddEvent(ev);
+  addFileReader(std::move(FileReaderFactory::create(filename, filepattern)));
+}
 
-    ev = m_fileReaders.back()->GetNextEvent();
-  
-  } 
-  
-  
+void multiFileReader::addFileReader(const std::string & filename)
+{
+  if (filename.size()>0)
+  {
+    addFileReader(std::move(FileReaderFactory::create(filename)));
+  }
   
 }
 
-void eudaq::multiFileReader::Interrupt()
+void  multiFileReader::addFileReader(std::unique_ptr<baseFileReader> FileReader)
+{
+  m_fileReaders.push_back(std::move(FileReader));
+
+  auto ev = m_fileReaders.back()->getEventPtr();
+
+  while (ev->IsBORE())
+  {
+    m_sync->pushEvent(ev, m_fileReaders.size() - 1);
+    ev = m_fileReaders.back()->GetNextEvent();
+  }
+
+
+  m_sync->mergeBoreEvent(m_ev);
+}
+
+void  multiFileReader::Interrupt()
 {
   for (auto& p:m_fileReaders)
   {p->Interrupt();
   }
 }
 
-bool eudaq::multiFileReader::NextEvent( size_t skip /*= 0*/ )
+bool  multiFileReader::readFiles()
 {
-  if (!m_preaparedForEvents)
+  for (size_t fileID = 0; fileID < m_fileReaders.size(); ++fileID)
   {
-    m_sync.PrepareForEvents();
-    m_preaparedForEvents=true;
-  }
-  for (size_t skipIndex=0;skipIndex<=skip;skipIndex++)
-  {
-  
-  do{
-    for (size_t fileID = 0; fileID < m_fileReaders.size(); ++fileID)
+    if (m_sync->InputIsEmpty(fileID))
     {
 
-      auto ev = m_fileReaders[fileID]->GetNextEvent();
+      std::shared_ptr<eudaq::Event> ev;
+      if (m_firstEvent)
+      {
+        ev = m_fileReaders[fileID]->getEventPtr();
 
-      if (ev==nullptr&&m_sync.SubEventQueueIsEmpty(fileID))
+      }
+      else
+      {
+        ev = m_fileReaders[fileID]->GetNextEvent();
+
+      }
+
+      if (!m_sync->pushEvent(ev, fileID))
       {
         return false;
       }
-      m_sync.AddEventToProducerQueue(fileID,ev);
+
     }
-    //m_sync.storeCurrentOrder();
-  }while (!m_sync.SyncNEvents(m_eventsToSync));
-  
-  if (!m_sync.getNextEvent(m_ev))
-  {
-    return false;
   }
+  if (m_firstEvent)
+  {
+    m_firstEvent = false;
+
+  }
+
+  return true;
+}
+
+bool  multiFileReader::NextEvent( size_t skip /*= 0*/ )
+{
+
+  for (size_t skipIndex=0;skipIndex<=skip;skipIndex++)
+  {
+  
+    do{
+
+      if (!readFiles())
+      {
+        return false;
+      }
+      //m_sync.storeCurrentOrder();
+    } while (!m_sync->getNextEvent(m_ev));
   
   }	
   return true;
 }
 
-const eudaq::DetectorEvent & eudaq::multiFileReader::GetDetectorEvent() const
+std::shared_ptr< Event>  multiFileReader::GetNextEvent()
 {
-      return dynamic_cast<const eudaq::DetectorEvent &>(*m_ev);
+
+
+  if (!NextEvent()) {
+    return nullptr;
+  }
+
+  return getEventPtr();
 }
 
-const eudaq::Event & eudaq::multiFileReader::GetEvent() const
+
+const  DetectorEvent &  multiFileReader::GetDetectorEvent() const
+{
+      return dynamic_cast<const  DetectorEvent &>(*m_ev);
+}
+
+const  Event &  multiFileReader::GetEvent() const
 {
     return *m_ev;
 }
 
-eudaq::multiFileReader::multiFileReader(bool sync) :m_sync(sync), m_eventsToSync(0), m_preaparedForEvents(0)
+ multiFileReader::multiFileReader(bool sync) : baseFileReader(""), m_eventsToSync(0), m_preaparedForEvents(0)
 {
+ // m_sync = factory_sync_class("aida", sync);
   
 }
 
-unsigned eudaq::multiFileReader::RunNumber() const
+ multiFileReader::multiFileReader(baseFileReader::Parameter_ref parameterList) :baseFileReader(parameterList)
+ {
+
+   if (parameterList.Get("sync","").size()>1 )
+   {
+     m_sync = EventSyncFactory::create(parameterList.Get("sync",""),"");
+   }
+   else
+   {
+     m_sync = EventSyncFactory::create();
+   }
+   
+   std::string delimiter = ",";
+
+
+     auto s = split(Filename(), delimiter, true);
+
+     for (auto& i : s)
+     {
+       if (i.size() > 0)
+       {
+         addFileReader(i);
+       }
+     }
+   
+ }
+
+unsigned  multiFileReader::RunNumber() const
 { 
   return m_ev->GetRunNumber();
+}
+
+void  multiFileReader::addSyncAlgorithm(std::unique_ptr<SyncBase> sync)
+{
+  if (m_sync)
+  {
+    EUDAQ_THROW("Sync algoritm already defined");
+  }
+  m_sync = std::move(sync);
+}
+
+void  multiFileReader::addSyncAlgorithm(SyncBase::MainType type /*= ""*/, SyncBase::Parameter_ref sync/*= 0*/)
+{
+  addSyncAlgorithm(EventSyncFactory::create(type, sync));
+}
+
+ multiFileReader::strOption_ptr  multiFileReader::add_Command_line_option_inputPattern(OptionParser & op)
+{
+  return  multiFileReader::strOption_ptr(new  Option<std::string>(op, "i", "inpattern", "../data/run$6R.raw", "string", "Input filename pattern"));
+
+}
+
+std::string multiFileReader::help_text()
+{
+  return "";
+}
+
+
+  RegisterFileReader(multiFileReader, "multi");
 }
