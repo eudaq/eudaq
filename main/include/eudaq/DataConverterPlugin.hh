9f64541ca898e7ea3e5f7992a08c44052b0b068c
RPeschke <Richard.Peschke@desy.de>
2015-03-24 18:42:07 +0100

    New Functionality:
    CompareTimeStampsWithJitter  class checks if two time stamps are in sync. it takes jitter and frequency differences into account.

--- main/include/eudaq/DataConverterPlugin.hh
+++ main/include/eudaq/DataConverterPlugin.hh
@@ -1,8 +1,10 @@
 #ifndef EUDAQ_INCLUDED_DataConverterPlugin
 #define EUDAQ_INCLUDED_DataConverterPlugin
 
+#include <memory>
 #include "eudaq/StandardEvent.hh"
 #include "eudaq/RawDataEvent.hh"
+
 
 
 #if USE_LCIO
@@ -17,11 +19,12 @@ using namespace IMPL;
 using namespace UTIL;
 #endif
 #include "TLUEvent.hh"
+#include <functional>
 
 #define NOTIMESTAMPSET (uint64_t)-1
 #define NOTIMEDURATIONSET 0
 
-
+#define PARAMETER_NOT_SET -1000000000000
 
 //////////////////////////////////////////////////////////////////////////
 // Compare Time stamps
@@ -46,7 +49,10 @@ namespace lcio { using namespace EVENT; }
 #include <algorithm>
 
 namespace eudaq{
+  namespace Eudaq_types{
+    using timeStamp_t = int64_t;
 
+  }
 
 	  inline int compareTLU2DUT(unsigned TLU_Trigger_Number, unsigned DUT_Trigger_number){
 	  if (DUT_Trigger_number==TLU_Trigger_Number)
@@ -136,6 +142,52 @@ namespace eudaq{
 	  return -666;
   }
 
+  class CompareTimeStampsWithJitter{
+    using timeStamp_t = Eudaq_types::timeStamp_t;
+  public:
+
+
+    typedef bool(*isSyncEvent_F)(eudaq::Event const & ev);
+
+    CompareTimeStampsWithJitter(timeStamp_t jitter_denominator, timeStamp_t jitter_offset, timeStamp_t default_delta_timestamps, timeStamp_t DUT_active_time, isSyncEvent_F isSyncEvent);
+
+    CompareTimeStampsWithJitter() = default;
+
+
+
+    void set_Jitter_offset(timeStamp_t jitter_offset);
+
+    void set_default_delta_timestamp(timeStamp_t deltaTimestamp);
+
+    void set_DUT_active_time(timeStamp_t DUT_active_time);
+    void set_Clock_diff(double clock_diff){ m_Clock_diff = clock_diff; }
+    double get_CLock_diff() const { return m_Clock_diff; }
+    timeStamp_t get_DUT_begin() const { return m_dut_begin; }
+  template <typename T>
+    void set_isSyncEventFunction(T isSyncEvent){
+    f_isSync_event=isSyncEvent;
+    }
+    int compareDUT2TLU(eudaq::Event const & ev, const eudaq::Event  & tluEvent)const;
+    timeStamp_t calc_Corrected_DUT_TIME(timeStamp_t DUT_time) const;
+  private:
+    bool isSetup() const;
+    void resync_jitter(eudaq::Event const & ev,const eudaq::Event  & tluEvent) const;
+    int compareDUT2TLU_normal_Event(eudaq::Event const & ev, const eudaq::Event  & tluEvent)const;
+
+    int compareDUT2TLU_sync_event(eudaq::Event const & ev, const eudaq::Event  & tluEvent)const;
+
+    timeStamp_t calc_Corrected_TLU_TIME_sync_event(timeStamp_t TLU_time) const;
+    timeStamp_t calc_Corrected_TLU_TIME_normal_event(timeStamp_t TLU_time) const;
+  private:
+
+    mutable  timeStamp_t m_dut_begin = 0, m_tlu_begin = 0, m_last_tlu = 0, m_dut_begin_const = 0, m_event = 1;
+    mutable double m_Clock_diff = 0;
+    timeStamp_t m_jitter_offset = PARAMETER_NOT_SET, m_default_delta_timestamps = PARAMETER_NOT_SET, m_DUT_active_time = PARAMETER_NOT_SET;
+//    bool m_useSyncEvents = false;
+    std::function<bool(eudaq::Event const &)> f_isSync_event;
+    mutable bool firstEvent = true;
+
+  };
 
   class Configuration;
 
@@ -152,11 +204,20 @@ namespace eudaq{
 
   class DataConverterPlugin {
     public:
+      using timeStamp_t = Eudaq_types::timeStamp_t;
       typedef Event::t_eventid t_eventid;
 
       virtual void Initialize(eudaq::Event const &, eudaq::Configuration const &) {}
 
       virtual unsigned GetTriggerID(eudaq::Event const &) const;
+
+      virtual timeStamp_t GetTimeStamp(const Event& ev, size_t index) const{
+        return ev.GetTimestamp(index);
+      }
+      virtual size_t GetTimeStamp_size(const Event & ev) const{
+        return ev.GetSizeOfTimeStamps();
+      }
+
 	  virtual int IsSyncWithTLU(eudaq::Event const & ev, const eudaq::Event  & tluEvent) const {
 		  // dummy comparator. it is just checking if the event numbers are the same.
 		  const TLUEvent *tlu = dynamic_cast<const eudaq::TLUEvent*>(&tluEvent);
@@ -187,6 +248,7 @@ namespace eudaq{
     virtual std::shared_ptr<eudaq::Event> ExtractEventN(std::shared_ptr<eudaq::Event> ev, size_t NumberOfROF) {
       return nullptr;
     }
+
 	  virtual bool isTLU(const Event&){ return false; }
 
 	  virtual unsigned getUniqueIdentifier(const eudaq::Event  & ev){ return m_thisCount; }
@@ -204,7 +266,7 @@ namespace eudaq{
       t_eventid m_eventtype;
 	  
 
-      /** The protected constructor which automatically registeres the plugin
+      /** The protected constructor which automatically registers the plugin
        *  at the pluginManager.
        */
       DataConverterPlugin(std::string subtype);
