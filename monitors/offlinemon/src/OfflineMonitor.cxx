9264f42916fb1a7eeb25a530aca9e8e4209e529d
RPeschke <Richard.Peschke@desy.de>
2015-03-27 13:44:53 +0100

    replaced the old function with the new read and process template

--- monitors/offlinemon/src/OfflineMonitor.cxx
+++ monitors/offlinemon/src/OfflineMonitor.cxx
@@ -12,6 +12,7 @@
 #include "../inc/makeCorrelations.h"
 #include "eudaq/PluginManager.hh"
 #include "eudaq/MultiFileReader.hh"
+#include "eudaq/readAndProcessDataTemplate.h"
 
 using namespace eudaq;
 unsigned dbg = 0; 
@@ -22,63 +23,41 @@ int main(int, char ** argv) {
 
 	start = std::clock();
   eudaq::OptionParser op("EUDAQ File Converter", "1.0", "", 1);
-  eudaq::Option<std::string> ipat(op, "i", "inpattern", "../data/run$6R.raw", "string", "Input filename pattern");
-  eudaq::Option<std::string> opat(op, "o", "outpattern", "test$6R$X", "string", "Output filename pattern");
+
+  auto events = ReadAndProcess<eudaq::FileWriter>::add_Command_line_option_EventsOfInterest(op);
+
+
+  FileWriterFactory::addComandLineOptions(op);
+  op.ExtraHelpText(FileWriterFactory::Help_text());
+
+
+  FileReaderFactory::addComandLineOptions(op);
+
+  op.ExtraHelpText(FileReaderFactory::Help_text());
+
+  
+
+  EventSyncFactory::addComandLineOptions(op);
   eudaq::Option<std::string> confFile(op, "c", "confFile", "../conf/offlinemonconf.xml", "string", "load the file that contains all the information about the correlations plots");
-  eudaq::OptionFlag async(op, "a", "nosync", "Disables Synchronisation with TLU events");
-  eudaq::Option<size_t> syncEvents(op, "n" ,"syncevents",0,"size_t","Number of events that need to be synchronous before they are used");
-  eudaq::Option<uint64_t> syncDelay(op, "d" ,"longDelay",20,"uint64_t","us time long time delay");
-  eudaq::Option<size_t> skipEvents(op, "k" ,"skipEvents",0,"size_t","Number of events to skip");
   eudaq::Option<std::string> level(op, "l", "log-level", "INFO", "level",
-      "The minimum level for displaying log messages locally");
-  op.ExtraHelpText("Available output types are: " + to_string(eudaq::FileWriterFactory::GetTypes(), ", "));
+                                   "The minimum level for displaying log messages locally");
 
   try {
     op.Parse(argv);
     EUDAQ_LOG_LEVEL(level.Value());
-	std::cout<<"syncEvents"<<syncEvents.Value()<<std::endl;
-	eudaq::multiFileReader reader(!async.Value());
-	for (size_t i = 0; i < op.NumArgs(); ++i) {
+    ReadAndProcess<mCorrelations> readProcess;
+  readProcess.setEventsOfInterest(parsenumbers(events->Value()));
 
-		reader.addFileReader(op.GetArg(i), ipat.Value());
-	}
-      mCorrelations correlator;
-	  correlator.open_confFile(confFile.Value().c_str());
-	  correlator.SetFilePattern(opat.Value());
-	  correlator.setRunNumber(reader.RunNumber());
-	  correlator.open_outFile();
-	  correlator.createHistograms();
-	  
-//      writer->SetFilePattern(opat.Value());
- //     writer->StartRun(reader.RunNumber());
-	  int event_nr=0;
-      do {
-  
+  readProcess.addFileReader(FileReaderFactory::create(op));
+  readProcess.setWriter(std::unique_ptr<  mCorrelations>(new   mCorrelations()));
+  readProcess.SetParameter("conf", confFile.Value());
+  readProcess.StartRun();
+  readProcess.process();
+  readProcess.EndRun();
 
-          if (!correlator.ProcessDetectorEvent(reader.GetDetectorEvent()))
-          {
-			  break;
-          }
-         
-      
-		  ++event_nr;
-		  if (event_nr%1000==0)
-		  {
-			 
-			  			  auto ev=reader.GetDetectorEvent().GetSubEvent<Event>(0);
-				  		  std::cout <<ev->GetSubType()<<":  "<<ev->GetEventNumber()<<"   ";
-						  auto ev1=reader.GetDetectorEvent().GetSubEvent<Event>(1);
-						  std::cout <<ev1->GetSubType()<<":  "<<ev1->GetEventNumber()<<"   ";			  
-						  auto ev2=reader.GetDetectorEvent().GetSubEvent<Event>(2);
-						  std::cout <<ev->GetSubType()<<" countedtrigger:  "<<eudaq::PluginManager::GetTriggerID(*ev2)<<"   GetEventNumber:" <<ev->GetEventNumber();
 
-	
-			
-			  std::cout <<std::endl;
-		  }
-        
-      } while (reader.NextEvent(skipEvents.Value()));
-     correlator.savePlotsAsPicture();
+
+     
     
   } catch (...) {
 	    std::cout << "Time: " << (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << std::endl;
