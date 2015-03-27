3460b0ad2e044f4e38d3df519f70dfee240a54b6
RPeschke <Richard.Peschke@desy.de>
2015-03-27 13:24:44 +0100

    changed the base file writer to make use of the new factory class
    added some helper function for the Read and Process template

--- main/lib/src/FileWriter.cc
+++ main/lib/src/FileWriter.cc
@@ -1,37 +1,96 @@
 #include "eudaq/FileWriter.hh"
 #include "eudaq/FileNamer.hh"
 #include "eudaq/Exception.hh"
+#include "eudaq/factoryDef.hh"
+#include "eudaq/OptionParser.hh"
+
 
 namespace eudaq {
 
-  namespace {
-    typedef std::map<std::string, FileWriterFactory::factoryfunc> map_t;
-
-    static map_t & FileWriterMap() {
-      static map_t m;
-      return m;
-    }
-  }
-
-  void FileWriterFactory::do_register(const std::string & name, FileWriterFactory::factoryfunc func) {
-    //std::cout << "DEBUG: Registering FileWriter: " << name << std::endl;
-    FileWriterMap()[name] = func;
-  }
-
-  FileWriter * FileWriterFactory::Create(const std::string & name, const std::string & params) {
-    map_t::const_iterator it = FileWriterMap().find(name == "" ? "native" : name);
-    if (it == FileWriterMap().end()) EUDAQ_THROW("Unknown file writer: " + name);
-    return (it->second)(params);
-  }
-
-  std::vector<std::string> FileWriterFactory::GetTypes() {
-    std::vector<std::string> result;
-    for (map_t::const_iterator it = FileWriterMap().begin(); it != FileWriterMap().end(); ++it) {
-      result.push_back(it->first);
-    }
-    return result;
-  }
-
+  
   FileWriter::FileWriter() : m_filepattern(FileNamer::default_pattern) {}
 
+  void FileWriter::WriteBaseEvent(const Event& ev)
+  {
+    auto dev = dynamic_cast<const eudaq::DetectorEvent *>(&ev);
+    if (dev){
+      WriteEvent(*dev);
+    }
+    else
+    {
+      EUDAQ_THROW("unable to convert event into detector Event. this can happen when you try to use the sync algorithm which does not create detector events.");
+    }
+  }
+
+
+  registerBaseClassDef(FileWriter);
+
+  class FileWriterFactory::Impl{
+  public:
+    std::unique_ptr<eudaq::Option<std::string>> DefaultType,DefaultOutputPattern;
+
+  };
+
+  std::unique_ptr<FileWriter> FileWriterFactory::Create(const std::string & name, const std::string & params /*= ""*/)
+  {
+    return EUDAQ_Utilities::Factory<FileWriter>::Create(name, params);
+  }
+
+  std::unique_ptr<FileWriter> FileWriterFactory::Create()
+  {
+    auto fWriter=Create(getDefaultType());
+    fWriter->SetFilePattern(getDefaultOutputPattern());
+
+    return fWriter;
+  }
+
+  std::vector<std::string> FileWriterFactory::GetTypes()
+  {
+    return EUDAQ_Utilities::Factory<FileWriter>::GetTypes();
+  }
+
+
+
+ 
+
+  std::string  FileWriterFactory::Help_text()
+  {
+    return std::string("Available output types are: " + to_string(eudaq::FileWriterFactory::GetTypes(), ", "));
+
+  }
+
+  void FileWriterFactory::addComandLineOptions(OptionParser & op)
+  {
+    getImpl().DefaultOutputPattern = std::unique_ptr<eudaq::Option<std::string>>(new eudaq::Option<std::string>(op, "o", "outpattern", "test$6R$X", "string", "Output filename pattern"));
+
+    getImpl().DefaultType = std::unique_ptr<eudaq::Option<std::string>>(new eudaq::Option<std::string>(op, "t", "type", getDefaultType(), "name", "Output file type"));
+
+  }
+
+  std::string FileWriterFactory::getDefaultType()
+  {
+    if (getImpl().DefaultType && getImpl().DefaultType->IsSet())
+    {
+      return getImpl().DefaultType->Value();
+    }
+    return "native";
+  }
+
+  std::string FileWriterFactory::getDefaultOutputPattern()
+  {
+    if (getImpl().DefaultOutputPattern && getImpl().DefaultOutputPattern->IsSet())
+    {
+      return getImpl().DefaultOutputPattern->Value();
+    }
+    return "test$6R$X";
+
+  }
+
+  FileWriterFactory::Impl& FileWriterFactory::getImpl()
+  {
+    static FileWriterFactory::Impl m_impl;
+    return m_impl;
+  }
+
+ 
 }
