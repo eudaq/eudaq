a956e901ebdbea6f1481aa1e55f98666a563dd76
RPeschke <Richard.Peschke@desy.de>
2015-03-27 13:30:18 +0100

    file writer native is now also Initializing the plugin manager. with out this step one would not be able to use the sync algorithm

--- main/lib/src/FileWriterNative.cc
+++ main/lib/src/FileWriterNative.cc
@@ -1,6 +1,7 @@
 #include "eudaq/FileNamer.hh"
 #include "eudaq/FileWriter.hh"
 #include "eudaq/FileSerializer.hh"
+#include "eudaq/PluginManager.hh"
 //#include "eudaq/Logger.hh"
 
 namespace eudaq {
@@ -10,6 +11,7 @@ namespace eudaq {
       FileWriterNative(const std::string &);
       virtual void StartRun(unsigned);
       virtual void WriteEvent(const DetectorEvent &);
+      virtual void WriteBaseEvent(const Event&);
       virtual uint64_t FileBytes() const;
       virtual ~FileWriterNative();
     private:
@@ -26,10 +28,28 @@ namespace eudaq {
 
   void FileWriterNative::StartRun(unsigned runnumber) {
     delete m_ser;
-    m_ser = new FileSerializer(FileNamer(m_filepattern).Set('X', ".raw").Set('R', runnumber));
+    m_ser = new FileSerializer(FileNamer(m_filepattern).Set('X', ".raw").Set('R', runnumber),true);
   }
 
   void FileWriterNative::WriteEvent(const DetectorEvent & ev) {
+    if (ev.IsBORE()) {
+      eudaq::PluginManager::Initialize(ev);
+    }
+    if (!m_ser) EUDAQ_THROW("FileWriterNative: Attempt to write unopened file");
+    m_ser->write(ev);
+    m_ser->Flush();
+  }
+  void FileWriterNative::WriteBaseEvent(const Event& ev){
+    
+    if (ev.IsBORE()) {
+      auto det = dynamic_cast<const DetectorEvent*>(&ev);
+      if (det)
+      {
+        eudaq::PluginManager::Initialize(*det);
+
+      }
+
+    }
     if (!m_ser) EUDAQ_THROW("FileWriterNative: Attempt to write unopened file");
     m_ser->write(ev);
     m_ser->Flush();
