c4543c7289ff009bfa07a9e2aa865cea128ed6e8
Richard Peschke <Richard.Peschke@desy.de>
2015-04-18 00:56:33 +0200

    test scriptes with config files

--- debug/startrun.bat
+++ debug/startrun.bat
@@ -3,11 +3,12 @@ taskkill /f /im TestRunControl.exe.exe
 taskkill /f /im TestLogCollector.exe.exe 
 taskkill /f /im TestDataCollector.exe.exe 
 taskkill /f /im ExampleProducer.exe.exe 
-
-start TestRunControl.exe.exe -a -c nim,ni_autotrig -d 100 -r 2
+Timeout /t 1 
+start TestRunControl.exe.exe -s -c test -d 300
 Timeout /t 1 
 start TestLogCollector.exe.exe 
 Timeout /t 1 
 start TestDataCollector.exe.exe 
 Timeout /t 5 
-start ExampleProducer.exe.exe -r tcp://192.168.56.1:44000
+start ExampleProducer.exe.exe 
+start ExampleProducer.exe.exe -n testproducer
