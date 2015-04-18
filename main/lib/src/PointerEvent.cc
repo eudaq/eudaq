dfb1ca13e29e6112088213406aae21dce8d9b506
Richard Peschke <Richard.Peschke@desy.de>
2015-04-18 16:33:14 +0200

    optimised storage by only storing the first timestamp.

--- main/lib/src/PointerEvent.cc
+++ main/lib/src/PointerEvent.cc
@@ -5,7 +5,12 @@ namespace eudaq{
 
   PointerEvent::PointerEvent(reference_t filePointer, counter_t counter) : Event(0, counter,filePointer)
   {
-
+    // The Timestamp slot is used for storing the pointer. 
+    // since there is only one pointer there 
+    // is no need to store a vector of timestamps. 
+    // This statement resets the EUDAQ 2 flag an by this only the
+    // first timestamp gets stored. 
+    ClearFlags(); 
   }
 
   PointerEvent::PointerEvent(Deserializer & des) :Event(des)
