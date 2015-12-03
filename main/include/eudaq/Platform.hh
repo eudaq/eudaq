3b021dea396b1af0305a7c9cecf746735c8c7bd5
rpeschke <richard.peschke@desy.de>
2015-12-03 19:40:45 +0100

    addedn new function to create unique_ptr pointer to base class

--- main/include/eudaq/Platform.hh
+++ main/include/eudaq/Platform.hh
@@ -1,5 +1,6 @@
 #ifndef EUDAQ_INCLUDED_Platform
 #define EUDAQ_INCLUDED_Platform
+#include <memory>
 
 #ifndef EUDAQ_PLATFORM
 #define EUDAQ_PLATFORM PF_WIN32
@@ -14,11 +15,21 @@
 
 #ifdef WIN32
 #define DLLEXPORT __declspec(dllexport)
+#include <stdint.h>
 
 #else
 
 #define DLLEXPORT
 
 #endif
+template <class T, class... Args>
+std::unique_ptr<T> __make_unique(Args&&... args){
+  return std::unique_ptr<T>(new T(args...));
+}
 
+
+template <class derived,class base, class... Args>
+std::unique_ptr<base> __make_unique_base(Args&&... args) {
+  return std::unique_ptr<base>(dynamic_cast<base>(new derived(args...)));
+}
 #endif // EUDAQ_INCLUDED_Platform
