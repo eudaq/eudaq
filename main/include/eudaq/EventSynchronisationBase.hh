90d4d1dafb6fa6b3a1f6dc58e5487b0012f7a3a8
rpeschke <richard.peschke@desy.de>
2015-12-03 15:15:28 +0100

    added Sync class

--- main/include/eudaq/EventSynchronisationBase.hh
+++ main/include/eudaq/EventSynchronisationBase.hh
@@ -1,75 +1,73 @@
 #ifndef EventSynchronisationBase_h__
 #define EventSynchronisationBase_h__
 
-#include "eudaq/DetectorEvent.hh"
-#include "eudaq/FileSerializer.hh"
+
 #include <memory>
-#include <queue>
+#include <string>
+#include "eudaq/factory.hh"
+#include "Platform.hh"
+#include "eudaq/Configuration.hh"
+#include "eudaq/Event.hh"
+
 // base class for all Synchronization Plugins
 // it is desired to be as modular es possible with this approach.
-// first step is to separate the events from different Producers.
+// first step is to separate the events from different Producers. 
 // then it will be recombined to a new event
-// The Idea is that the user can define a condition that need to be true to
-// define if an event is sync or not
+// The Idea is that the user can define a condition that need to be true to define if an event is sync or not
 
-namespace eudaq {
 
+#define registerSyncClass(DerivedFileWriter,ID)  registerClass(SyncBase,DerivedFileWriter,ID)
+
+namespace eudaq{
+
+ 
+  class OptionParser;
+  class Event;
   class DLLEXPORT SyncBase {
   public:
-    typedef std::queue<std::shared_ptr<eudaq::Event>> eventqueue_t;
+    using MainType = std::string;
+    using Parameter_t = eudaq::Configuration;
+    using Parameter_ref = const Parameter_t&;
 
-    int AddDetectorElementToProducerQueue(
-        int fileIndex, std::shared_ptr<eudaq::DetectorEvent> dev);
-    virtual bool SyncFirstEvent();
-    virtual bool SyncNEvents(size_t N);
-    virtual bool getNextEvent(std::shared_ptr<eudaq::DetectorEvent> &ev);
 
-    virtual bool
-    compareTLUwithEventQueue(std::shared_ptr<eudaq::Event> &tlu_event,
-                             SyncBase::eventqueue_t &event_queue);
-    virtual bool
-    compareTLUwithEventQueues(std::shared_ptr<eudaq::Event> &tlu_event);
+    // public interface
 
-    void storeCurrentOrder();
-    bool Event_Queue_Is_Empty();
-    bool SubEventQueueIsEmpty(int i);
-    void event_queue_pop();
-    void event_queue_pop_TLU_event();
-    void makeDetectorEvent();
-    void clearDetectorQueue();
+    SyncBase(Parameter_ref param);
+    virtual bool pushEvent(event_sp ev,size_t Index=0) = 0;
+    virtual bool getNextEvent(event_sp& ev) = 0;
 
-    /** The empty destructor. Need to add it to make it virtual.
-     */
-    virtual ~SyncBase() {}
-    SyncBase(bool sync = true);
-    void addBOREEvent(int fileIndex, const eudaq::DetectorEvent &BOREvent);
-    void PrepareForEvents();
+    virtual bool OutputIsEmpty() const = 0;
+    virtual bool InputIsEmpty() const = 0;
+    virtual bool InputIsEmpty(size_t FileID) const = 0;
 
-  protected:
-    eventqueue_t &getQueuefromId(unsigned producerID);
-    eventqueue_t &getQueuefromId(unsigned fileIndex, unsigned eventIndex);
+    Parameter_t getParameter();
+  private:
+    Parameter_t m_param;
 
-    eventqueue_t &getFirstTLUQueue();
-    unsigned getUniqueID(unsigned fileIndex, unsigned eventIndex);
-    unsigned getTLU_UniqueID(unsigned fileIndex);
-    std::map<unsigned, size_t> m_ProducerId2Eventqueue;
-    size_t m_registertProducer;
-    std::vector<size_t> m_EventsProFileReader;
-    /* This vector saves for each producer an event queue */
-
-    std::vector<eventqueue_t> m_ProducerEventQueue;
-
-    std::queue<std::shared_ptr<eudaq::DetectorEvent>> m_DetectorEventQueue;
-
-    int m_TLUs_found;
-
-    bool isAsync_;
-    size_t NumberOfEventsToSync_;
-    uint64_t longTimeDiff_;
-
-    bool m_sync;
   };
 
-} // namespace eudaq
+
+
+
+  class DLLEXPORT EventSyncFactory{
+  public:
+
+   static std::unique_ptr<SyncBase>  create(SyncBase::MainType name, SyncBase::Parameter_ref sync);
+   static std::unique_ptr<SyncBase>  create(SyncBase::MainType name );
+   static std::unique_ptr<SyncBase> create();
+   static std::vector<std::string> GetTypes();
+   static std::string  Help_text();
+   static void addComandLineOptions(eudaq::OptionParser & op);
+   static bool DefaultIsSet();
+   static std::string getDefaultSync();
+   
+  private:
+    class Impl;
+    static Impl& getImpl();
+    
+  };
+}//namespace eudaq
+
+
 
 #endif // EventSynchronisationBase_h__
