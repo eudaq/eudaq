8a05b7517c87fada1169f80f837dd2021ee4d9e8
rpeschke <richard.peschke@desy.de>
2015-12-03 19:20:01 +0100

    Implemented new print out function
    added ShallowCopy which copies the DetectorEvent but not the underling events

--- main/include/eudaq/DetectorEvent.hh
+++ main/include/eudaq/DetectorEvent.hh
@@ -7,6 +7,7 @@
 
 namespace eudaq {
 
+
   class RawDataEvent;
 
   class DLLEXPORT DetectorEvent : public Event {
@@ -15,25 +16,27 @@ namespace eudaq {
   public:
     virtual void Serialize(Serializer &) const;
     explicit DetectorEvent(unsigned runnumber, unsigned eventnumber,
-                           uint64_t timestamp)
-        : Event(runnumber, eventnumber, timestamp) {}
-    //     explicit DetectorEvent(const TLUEvent & tluev) :
-    //       Event(tluev.GetRunNumber(), tluev.GetEventNumber(),
-    //       tluev.GetTimestamp())
-    //       {}
+                           uint64_t timestamp);
+
     explicit DetectorEvent(Deserializer &);
-    void AddEvent(std::shared_ptr<Event> &evt);
+    void AddEvent(std::shared_ptr<Event> evt);
     virtual void Print(std::ostream &) const;
+    virtual void Print(std::ostream &os,size_t offset) const;
+    static event_sp ShallowCopy(const DetectorEvent& det);
 
-    /// Return "DetectorEvent" as type.
-    virtual std::string GetType() const { return "DetectorEvent"; }
 
-    size_t NumEvents() const { return m_events.size(); }
-    Event *GetEvent(size_t i) { return m_events[i].get(); }
-    const Event *GetEvent(size_t i) const { return m_events[i].get(); }
-    std::shared_ptr<Event> GetEventPtr(size_t i) const { return m_events[i]; }
+    virtual std::string GetType() const;
+    
+
+    size_t NumEvents() const;
+    Event *GetEvent(size_t i);
+    const Event *GetEvent(size_t i) const;
+    std::shared_ptr<Event> GetEventPtr(size_t i) const;
     const RawDataEvent &GetRawSubEvent(const std::string &subtype,
                                        int n = 0) const;
+
+    void clearEvents();
+
     template <typename T> const T *GetSubEvent(int n = 0) const {
       for (size_t i = 0; i < NumEvents(); i++) {
         const T *sev = dynamic_cast<const T *>(GetEvent(i));
@@ -48,9 +51,13 @@ namespace eudaq {
       return 0;
     }
 
+
   private:
     std::vector<std::shared_ptr<Event>> m_events;
+    DetectorEvent(const DetectorEvent& det);
   };
+  using ContainerEvent = DetectorEvent;
+  
 }
 
 #endif // EUDAQ_INCLUDED_TLUEvent
--- main/lib/src/DetectorEvent.cc
+++ main/lib/src/DetectorEvent.cc
@@ -12,29 +12,49 @@ namespace eudaq {
     unsigned n;
     ds.read(n);
     // std::cout << "Num=" << n << std::endl;
+    SetFlags(Event::FLAG_PACKET);
     for (size_t i = 0; i < n; ++i) {
       std::shared_ptr<Event> ev(EventFactory::Create(ds));
       m_events.push_back(ev);
     }
   }
 
-  void DetectorEvent::AddEvent(std::shared_ptr<Event> &evt) {
-    if (!evt.get())
-      EUDAQ_THROW("Adding null event!");
-    m_events.push_back(evt);
+  DetectorEvent::DetectorEvent(const DetectorEvent& det) :Event(det.GetRunNumber(), det.GetEventNumber(), det.GetTimestamp(), det.GetFlags()), m_events(det.m_events)
+  {
+    m_tags = det.m_tags;
+    m_timestamp = det.m_timestamp;
+
+  }
+
+  DetectorEvent::DetectorEvent(unsigned runnumber, unsigned eventnumber, uint64_t timestamp) : Event(runnumber, eventnumber, timestamp) {
+
+  }
+
+  void DetectorEvent::AddEvent(std::shared_ptr<Event> evt) {
+    if (!evt.get()) EUDAQ_THROW("Adding null event!");
     SetFlags(evt->GetFlags());
+    m_events.push_back(std::move(evt));
+    
   }
 
-  void DetectorEvent::Print(std::ostream &os) const {
-    Event::Print(os);
-    os << " {\n";
+  void DetectorEvent::Print(std::ostream & os) const {
+    Print(os, 0);
+  }
+
+  void DetectorEvent::Print(std::ostream &os, size_t offset) const
+  {
+    os << std::string(offset, ' ') << "<DetectorEvent> \n";
+    Event::Print(os,offset+2);
+    os <<std::string(offset+2,' ') << "<SubEvents> \n";
     for (size_t i = 0; i < NumEvents(); ++i) {
-      os << "  " << *GetEvent(i) << std::endl;
+      GetEvent(i)->Print(os,offset+4) ;
     }
-    os << "}";
+    os << std::string(offset+2, ' ') << "</SubEvents> \n";
+
+    os << std::string(offset, ' ') << "</DetectorEvent> \n";
   }
 
-  void DetectorEvent::Serialize(Serializer &ser) const {
+  void DetectorEvent::Serialize(Serializer & ser) const {
     Event::Serialize(ser);
     ser.write((unsigned)m_events.size());
     for (size_t i = 0; i < m_events.size(); ++i) {
@@ -42,10 +62,21 @@ namespace eudaq {
     }
   }
 
-  const RawDataEvent &DetectorEvent::GetRawSubEvent(const std::string &subtype,
-                                                    int n) const {
+  const Event * DetectorEvent::GetEvent(size_t i) const {
+    return m_events[i].get();
+  }
+
+  Event * DetectorEvent::GetEvent(size_t i) {
+    return m_events[i].get();
+  }
+
+  std::shared_ptr<Event> DetectorEvent::GetEventPtr(size_t i) const {
+    return m_events[i];
+  }
+
+  const RawDataEvent & DetectorEvent::GetRawSubEvent(const std::string & subtype, int n) const {
     for (size_t i = 0; i < NumEvents(); i++) {
-      const RawDataEvent *sev = dynamic_cast<const RawDataEvent *>(GetEvent(i));
+      const RawDataEvent * sev = dynamic_cast<const RawDataEvent*>(GetEvent(i));
       if (sev && sev->GetSubType() == subtype) {
         if (n > 0) {
           --n;
@@ -54,7 +85,24 @@ namespace eudaq {
         }
       }
     }
-    EUDAQ_THROW("DetectorEvent::GetRawSubEvent: could not find " + subtype +
-                ":" + to_string(n));
+    EUDAQ_THROW("DetectorEvent::GetRawSubEvent: could not find " + subtype + ":" + to_string(n));
   }
+
+  void DetectorEvent::clearEvents() {
+    m_events.clear();
+  }
+
+  event_sp DetectorEvent::ShallowCopy(const DetectorEvent& det)
+  {
+    return  event_sp(new DetectorEvent(det));
+  }
+
+  std::string DetectorEvent::GetType() const {
+    return "DetectorEvent";
+  }
+
+  size_t DetectorEvent::NumEvents() const {
+    return m_events.size();
+  }
+
 }
