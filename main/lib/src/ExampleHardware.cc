b398b7216feffd55903256fd6b1fc4cf93c0db88
rpeschke <richard.peschke@desy.de>
2015-12-03 16:51:54 +0100

    added example Hardware

--- main/lib/src/ExampleHardware.cc
+++ main/lib/src/ExampleHardware.cc
@@ -5,8 +5,9 @@ namespace eudaq {
 
   namespace {
 
-    std::vector<unsigned char> MakeRawEvent(unsigned width, unsigned height) {
-      std::vector<unsigned char> result(width * height * 2);
+    std::vector<unsigned char> MakeRawEvent(unsigned width,
+        unsigned height) {
+      std::vector<unsigned char> result(width*height*2);
       size_t offset = 0;
       for (unsigned y = 0; y < height; ++y) {
         for (unsigned x = 0; x < width; ++x) {
@@ -18,9 +19,7 @@ namespace eudaq {
       return result;
     }
 
-    std::vector<unsigned char>
-    ZeroSuppressEvent(const std::vector<unsigned char> &data, unsigned width,
-                      int threshold = 10) {
+    std::vector<unsigned char> ZeroSuppressEvent(const std::vector <unsigned char> & data, unsigned width, int threshold = 10) {
       unsigned height = data.size() / width / 2;
       std::vector<unsigned char> result;
       size_t inoffset = 0, outoffset = 0;
@@ -28,10 +27,10 @@ namespace eudaq {
         for (unsigned x = 0; x < width; ++x) {
           short charge = getlittleendian<short>(&data[inoffset]);
           if (charge > threshold) {
-            result.resize(outoffset + 6);
-            setlittleendian<unsigned short>(&result[outoffset + 0], x);
-            setlittleendian<unsigned short>(&result[outoffset + 2], y);
-            setlittleendian<short>(&result[outoffset + 4], charge);
+            result.resize(outoffset+6);
+            setlittleendian<unsigned short>(&result[outoffset+0], x);
+            setlittleendian<unsigned short>(&result[outoffset+2], y);
+            setlittleendian<short>(&result[outoffset+4], charge);
             outoffset += 6;
           }
           inoffset += 2;
@@ -39,12 +38,18 @@ namespace eudaq {
       }
       return result;
     }
+
   }
 
   ExampleHardware::ExampleHardware()
-      : m_numsensors(2), m_width(256), m_height(256), m_triggerid(0) {}
+    : m_numsensors(2),
+    m_width(256),
+    m_height(256),
+    m_triggerid(0)
+  {}
 
-  void ExampleHardware::Setup(int) {}
+  void ExampleHardware::Setup(int) {
+  }
 
   void ExampleHardware::PrepareForRun() {
     m_timer.Restart();
@@ -53,10 +58,17 @@ namespace eudaq {
   }
 
   bool ExampleHardware::EventsPending() const {
+    if (running)
+    {
     return m_timer.Seconds() > m_nextevent;
+    }
+
+    return false;
   }
 
-  unsigned ExampleHardware::NumSensors() const { return m_numsensors; }
+  unsigned ExampleHardware::NumSensors() const {
+    return m_numsensors;
+  }
 
   std::vector<unsigned char> ExampleHardware::ReadSensor(int sensorid) {
     std::vector<unsigned char> result(8);
@@ -70,8 +82,7 @@ namespace eudaq {
       result.insert(result.end(), data.begin(), data.end());
     } else {
       // Zero suppressed data
-      std::vector<unsigned char> data =
-          ZeroSuppressEvent(MakeRawEvent(m_width, m_height), m_width);
+      std::vector<unsigned char> data = ZeroSuppressEvent(MakeRawEvent(m_width, m_height), m_width);
       result.insert(result.end(), data.begin(), data.end());
       unsigned short numhits = (result.size() - 8) / 6;
       setlittleendian<unsigned short>(&result[6], 0x8000 | numhits);
@@ -79,7 +90,7 @@ namespace eudaq {
     return result;
   }
 
-  void ExampleHardware::CompletedEvent() {
+  void  ExampleHardware::CompletedEvent() {
     m_triggerid++;
     m_nextevent += 1.0;
   }
