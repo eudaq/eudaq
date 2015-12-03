a907593bce41b7ddd9bb6b80ea7e16cee3f87016
rpeschke <richard.peschke@desy.de>
2015-12-03 13:16:38 +0100

    added new base class for file reader. this makes it easier to implement new file readers in future

--- main/lib/src/FileReader.cc
+++ main/lib/src/FileReader.cc
@@ -1,60 +1,77 @@
+#include <list>
 #include "eudaq/FileReader.hh"
 #include "eudaq/FileNamer.hh"
 #include "eudaq/PluginManager.hh"
 #include "eudaq/Event.hh"
 #include "eudaq/Logger.hh"
-#include <list>
 #include "eudaq/FileSerializer.hh"
 #include "eudaq/Configuration.hh"
+#include "eudaq/factory.hh"
 
 namespace eudaq {
 
-  FileReader::FileReader(const std::string &file,
-                         const std::string &filepattern)
-      : m_filename(
-            FileNamer(filepattern).Set('X', ".raw").SetReplace('R', file)),
-        m_des(m_filename), m_ev(EventFactory::Create(m_des)), m_ver(1) {
 
-    // 		m_ev->SetTag("longTimeDelay",longTimeDelay);
-    // 		m_ev->SetTag("NumberOfEvents",syncEvents);
 
-    eudaq::Configuration conf(GetDetectorEvent().GetTag("CONFIG"));
-    conf.SetSection("EventStruct");
 
-    //       if (synctriggerid) {
-    //
-    // 	// saves this information in the BOR event. the DataConverterPlugins can
-    // extract this information during initializing.
-    // 		m_sync =std::make_shared<eudaq::SyncBase>(GetDetectorEvent());
-    //
-    //
-    //       }
+
+  FileReader::FileReader(const std::string & file, const std::string & filepattern)
+    : baseFileReader(FileNamer(filepattern).Set('X', ".raw").SetReplace('R', file)),
+    m_des(Filename()),
+    m_ev(EventFactory::Create(m_des)),
+    m_ver(1)
+  {
+
   }
-  //   FileReader::FileReader(FileReader&& fileR):m_filename(fileR.Filename()),
-  // 	  m_des(m_filename){
-  //
-  //
-  //   }
-  FileReader::~FileReader() {}
+
+  FileReader::FileReader(Parameter_ref param) :FileReader(param.Get(getKeyFileName(),""),param.Get(getKeyInputPattern(),""))
+  {
+
+  }
+
+  FileReader::~FileReader() {
+
+  }
 
   bool FileReader::NextEvent(size_t skip) {
     std::shared_ptr<eudaq::Event> ev = nullptr;
 
+
     bool result = m_des.ReadEvent(m_ver, ev, skip);
-    if (ev)
-      m_ev = ev;
+    if (ev) m_ev = ev;
     return result;
   }
 
-  unsigned FileReader::RunNumber() const { return m_ev->GetRunNumber(); }
+  unsigned FileReader::RunNumber() const {
+    return m_ev->GetRunNumber();
+  }
 
-  const Event &FileReader::GetEvent() const { return *m_ev; }
+  const Event & FileReader::GetEvent() const {
+    return *m_ev;
+  }
 
-  const DetectorEvent &FileReader::GetDetectorEvent() const {
+  const DetectorEvent & FileReader::GetDetectorEvent() const {
     return dynamic_cast<const DetectorEvent &>(*m_ev);
   }
 
-  const StandardEvent &FileReader::GetStandardEvent() const {
+  const StandardEvent & FileReader::GetStandardEvent() const {
     return dynamic_cast<const StandardEvent &>(*m_ev);
   }
+
+  std::shared_ptr<eudaq::Event> FileReader::GetNextEvent(){
+
+    if (!NextEvent()) {
+      return nullptr;
+    }
+
+    return m_ev;
+
+
+  }
+
+
+
+
+
+  RegisterFileReader(FileReader, "raw");
+
 }
