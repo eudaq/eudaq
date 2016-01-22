d7565cdb91bd362bbcdf9d823d5f7a777806f445
Andre Rummler <andre.rummler@tu-dortmund.de>
2016-01-23 00:33:14 +0100

    Compiles with EUTELESCOPE and LCIO set.

--- main/lib/plugins/SCTConverterPlugin.cc
+++ main/lib/plugins/SCTConverterPlugin.cc
@@ -41,6 +41,33 @@ namespace eudaq {
     std::string Timestamp_L0ID() { return "Timestamp.L0ID"; }
     std::string Event_L0ID() { return "Event.L0ID"; }
     std::string Event_BCID() { return "Event.BCID"; }
+  void ConvertPlaneToLCIOGenericPixel(StandardPlane &plane,
+                                      lcio::TrackerDataImpl &zsFrame) {
+    // helper object to fill the TrakerDater object
+    auto sparseFrame = eutelescope::EUTelTrackerDataInterfacerImpl<
+        eutelescope::EUTelGenericSparsePixel>(&zsFrame);
+
+    for (size_t iPixel = 0; iPixel < plane.HitPixels(); ++iPixel) {
+      eutelescope::EUTelGenericSparsePixel thisHit1(
+          plane.GetX(iPixel), plane.GetY(iPixel), plane.GetPixel(iPixel), 0);
+      sparseFrame.addSparsePixel(&thisHit1);
+    }
+  }
+  bool Collection_createIfNotExist(lcio::LCCollectionVec **zsDataCollection,
+                                   const lcio::LCEvent &lcioEvent,
+                                   const char *name) {
+
+    bool zsDataCollectionExists = false;
+    try {
+      *zsDataCollection =
+          static_cast<lcio::LCCollectionVec *>(lcioEvent.getCollection(name));
+      zsDataCollectionExists = true;
+    } catch (lcio::DataNotAvailableException &e) {
+      *zsDataCollection = new lcio::LCCollectionVec(lcio::LCIO::TRACKERDATA);
+    }
+
+    return zsDataCollectionExists;
+  }
   }
 
   using namespace sct;
--- main/lib/plugins/SCTUpgradeConverterPlugin.cc
+++ main/lib/plugins/SCTUpgradeConverterPlugin.cc
@@ -57,6 +57,34 @@ inline void helper_uchar2bool(const std::vector<unsigned char> &in, int lOffset,
 int numberOfEvents_inplane;
 
 namespace eudaq {
+  void ConvertPlaneToLCIOGenericPixel(StandardPlane &plane,
+                                      lcio::TrackerDataImpl &zsFrame) {
+    // helper object to fill the TrakerDater object
+    auto sparseFrame = eutelescope::EUTelTrackerDataInterfacerImpl<
+        eutelescope::EUTelGenericSparsePixel>(&zsFrame);
+
+    for (size_t iPixel = 0; iPixel < plane.HitPixels(); ++iPixel) {
+      eutelescope::EUTelGenericSparsePixel thisHit1(
+          plane.GetX(iPixel), plane.GetY(iPixel), plane.GetPixel(iPixel), 0);
+      sparseFrame.addSparsePixel(&thisHit1);
+    }
+  }
+  bool Collection_createIfNotExist(lcio::LCCollectionVec **zsDataCollection,
+                                   const lcio::LCEvent &lcioEvent,
+                                   const char *name) {
+
+    bool zsDataCollectionExists = false;
+    try {
+      *zsDataCollection =
+          static_cast<lcio::LCCollectionVec *>(lcioEvent.getCollection(name));
+      zsDataCollectionExists = true;
+    } catch (lcio::DataNotAvailableException &e) {
+      *zsDataCollection = new lcio::LCCollectionVec(lcio::LCIO::TRACKERDATA);
+    }
+
+    return zsDataCollectionExists;
+  }
+
   static const int dbg = 0; // 0=off, 1=structure, 2=structure+data
 
   void puschDataInStandartPlane(const std::vector<unsigned char> &inputVector,
