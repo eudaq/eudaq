3198ce76814e45ed149f540da86bfa4d6fbf46b3
yiliu <yi.liu@desy.de>
2016-06-09 01:03:17 +0200

    Merge Event.hh and Event_declarationMacros.hh

--- main/lib/core/include/eudaq/Event.hh
+++ main/lib/core/include/eudaq/Event.hh
@@ -8,13 +8,30 @@
 #include <iostream>
 #include <memory>
 
-#include "eudaq/Serializable.hh"
-#include "eudaq/Serializer.hh"
-#include "eudaq/Exception.hh"
-#include "eudaq/Utils.hh"
-#include "eudaq/Platform.hh"
-#include "eudaq/Event_declarationMacros.hh"
+#include "Serializable.hh"
+#include "Serializer.hh"
+#include "Exception.hh"
+#include "Utils.hh"
+#include "Platform.hh"
 
+#define EUDAQ_DECLARE_EVENT(type)              \
+  public:                                      \
+  static  MainType_t eudaq_static_id();	       \
+  virtual  MainType_t get_id() const {	       \
+    return eudaq_static_id();		       \
+  }					       \
+private:                                       \
+ static const int EUDAQ_DUMMY_VAR_DONT_USE = 0
+
+#define EUDAQ_DEFINE_EVENT(type, id)                \
+  type::MainType_t type::eudaq_static_id() {	    \
+    static const  type::MainType_t id_(id);	    \
+    return id_;						\
+  }							\
+  namespace _eudaq_dummy_ {				\
+    static eudaq::RegisterEventType<type> eudaq_reg;	\
+  }							\
+  static const int EUDAQ_DUMMY_VAR_DONT_USE = 0
 
 
 namespace eudaq {
