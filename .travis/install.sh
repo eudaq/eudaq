dbdf7cf69804b2ee7888fe42d65231e9aba9313a
Andre Rummler <andre.rummler@tu-dortmund.de>
2016-06-14 12:00:06 +0200

    Travis CI: QT5 added

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-  # Install package dependencies for Mac OS X:
-   brew unlink cmake
-   brew install python libusb qt
-  # Install numpy via pip:
-   easy_install pip
-   pip install numpy
+	if [[ $OPTION == 'modern' ]]; then
+		# Install package dependencies for Mac OS X:
+		brew unlink cmake
+		brew install python libusb qt5
+		export CMAKE_PREFIX_PATH=/usr/local/opt/qt5
+		# Install numpy via pip:
+		easy_install pip
+		pip install numpy
+	else
+		# Install package dependencies for Mac OS X:
+		brew unlink cmake
+		brew install python libusb qt
+		# Install numpy via pip:
+		easy_install pip
+		pip install numpy	
+	fi
+	
 else 
-  # Install package dependencies for Linux:
-  sudo apt-get install -y libusb-1.0-0 libusb-1.0-0-dev python2.7 python-numpy cmake qt4-dev-tools
+
+	if [[ $OPTION == 'modern' ]]; then
+		# Install package dependencies for Linux:
+		sudo apt-get install -y libusb-1.0-0 libusb-1.0-0-dev python2.7 python-numpy cmake qt5-default	
+	else
+		# Install package dependencies for Linux:
+		sudo apt-get install -y libusb-1.0-0 libusb-1.0-0-dev python2.7 python-numpy cmake qt4-dev-tools
+	fi
+	
 fi 
 
 
