92372ddd73879a1567e1ffd888d289ed074d7ba0
Andre Rummler <andre.rummler@tu-dortmund.de>
2016-06-14 12:54:06 +0200

    Travis CI: Install python3 on travis for modern option

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,7 +4,7 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 	if [[ $OPTION == 'modern' ]]; then
 		# Install package dependencies for Mac OS X:
 		brew unlink cmake
-		brew install python libusb qt5
+		brew install python3 libusb qt5
 		export CMAKE_PREFIX_PATH=/usr/local/opt/qt5
 		# Install numpy via pip:
 		easy_install pip
@@ -22,7 +22,7 @@ else
 
 	if [[ $OPTION == 'modern' ]]; then
 		# Install package dependencies for Linux:
-		sudo apt-get install -y libusb-1.0-0 libusb-1.0-0-dev python2.7 python-numpy cmake qt5-default	
+		sudo apt-get install -y libusb-1.0-0 libusb-1.0-0-dev python3.4 python-numpy cmake qt5-default	
 	else
 		# Install package dependencies for Linux:
 		sudo apt-get install -y libusb-1.0-0 libusb-1.0-0-dev python2.7 python-numpy cmake qt4-dev-tools
