4b456af6a0ee959913e9678133187ac07b9dcb69
Felix Reidt <Felix.Reidt@cern.ch>
2016-06-15 15:09:35 +0200

    fast exit of the script if the desired voltage is set

--- producers/palpidefs/scripts/change_back_bias.py
+++ producers/palpidefs/scripts/change_back_bias.py
@@ -8,20 +8,19 @@ import time
 def main():
     vbb_dst=float(sys.argv[1]) # value to which the back-bias should be ramped
     sour=serial.Serial(h.dev, h.baud_rate, rtscts=h.rtscts)
-    retvalue=0
     for ivbb in range (0, len(h.vbb_chan)):
       vbb_src=h.meas_voltage(sour, h.vbb_chan[ivbb])
-      if abs((vbb_dst - vbb_src)/(vbb_dst + 1.e-3)) < 1.3e-3:
-        retvalue=0
+      if abs((vbb_dst - vbb_src)/(vbb_dst + 1.e-3)) < 1.e-3:
+        return 0
       h.ramp_from_to(sour, h.vbb_chan[ivbb], (vbb_src, vbb_dst), 100)
       time.sleep(1)
       vbb_meas=h.meas_voltage(sour, h.vbb_chan[ivbb])
       if abs((vbb_dst - vbb_meas)/(vbb_dst + 1.e-3)) < 1.e-3:
-        retvalue=0
+        return 0
       else:
-        retvalue=1
+        return 1
 
-    return retvalue
+    return 1
 
 ## execute the main
 if __name__ == "__main__":
