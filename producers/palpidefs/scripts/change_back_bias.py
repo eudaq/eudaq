976fe0fdc83272108b3962adc78d7d4f637839bc
Felix Reidt <Felix.Reidt@cern.ch>
2016-06-16 10:34:11 +0200

    fixing back-bias setting script

--- producers/palpidefs/scripts/change_back_bias.py
+++ producers/palpidefs/scripts/change_back_bias.py
@@ -8,19 +8,17 @@ import time
 def main():
     vbb_dst=float(sys.argv[1]) # value to which the back-bias should be ramped
     sour=serial.Serial(h.dev, h.baud_rate, rtscts=h.rtscts)
+    ret_value = 0 # 0 = success, 1 = error
     for ivbb in range (0, len(h.vbb_chan)):
-      vbb_src=h.meas_voltage(sour, h.vbb_chan[ivbb])
-      if abs((vbb_dst - vbb_src)/(vbb_dst + 1.e-3)) < 1.e-3:
-        return 0
-      h.ramp_from_to(sour, h.vbb_chan[ivbb], (vbb_src, vbb_dst), 100)
-      time.sleep(1)
-      vbb_meas=h.meas_voltage(sour, h.vbb_chan[ivbb])
-      if abs((vbb_dst - vbb_meas)/(vbb_dst + 1.e-3)) < 1.e-3:
-        return 0
-      else:
-        return 1
-
-    return 1
+        vbb_src=h.meas_voltage(sour, h.vbb_chan[ivbb])
+        if abs((vbb_dst - vbb_src)/(vbb_dst + 1.e-3)) < 1.e-3:
+            continue
+        h.ramp_from_to(sour, h.vbb_chan[ivbb], (vbb_src, vbb_dst), 100)
+        time.sleep(1)
+        vbb_meas=h.meas_voltage(sour, h.vbb_chan[ivbb])
+        if abs((vbb_dst - vbb_meas)/(vbb_dst + 1.e-3)) > 1.e-3:
+            ret_value = 1
+    return ret_value
 
 ## execute the main
 if __name__ == "__main__":
