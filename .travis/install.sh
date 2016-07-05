c89bbe547dd08a94dfe20a80ef4d717d57fdf80f
Andre Rummler <andre.rummler@tu-dortmund.de>
2016-07-05 13:08:00 +0200

    Change how python is handled

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,7 +4,7 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 	if [[ $OPTION == 'modern' ]]; then
 		# Install package dependencies for Mac OS X:
 		brew unlink cmake
-		brew install python3 libusb qt5 libusb-compat
+		brew install libusb qt5 libusb-compat
 		export CMAKE_PREFIX_PATH=/usr/local/opt/qt5
 		# Install numpy via pip:
 		easy_install pip
@@ -12,7 +12,7 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 	else
 		# Install package dependencies for Mac OS X:
 		brew unlink cmake
-		brew install python libusb qt libusb-compat
+		brew install libusb qt libusb-compat
 		# Install numpy via pip:
 		easy_install pip
 		pip install numpy	
@@ -22,12 +22,16 @@ else
 
 	if [[ $OPTION == 'modern' ]]; then
 		# Install package dependencies for Linux:
-		sudo apt-get install -y libusb-dev libusb-1.0-0 libusb-1.0-0-dev python3.4 python-numpy cmake qt5-default linux-generic linux-headers-$(uname -r) openafs-client openafs-krb5
+		sudo apt-get install -y libusb-dev libusb-1.0-0 libusb-1.0-0-dev cmake qt5-default linux-generic linux-headers-$(uname -r) openafs-client openafs-krb5
 		sudo service openafs-client start
+		easy_install pip
+		pip install numpy
 	else
 		# Install package dependencies for Linux:
-		sudo apt-get install -y libusb-dev libusb-1.0-0 libusb-1.0-0-dev python2.7 python-numpy cmake qt4-dev-tools linux-generic linux-headers-$(uname -r) openafs-client openafs-krb5
+		sudo apt-get install -y libusb-dev libusb-1.0-0 libusb-1.0-0-dev cmake qt4-dev-tools linux-generic linux-headers-$(uname -r) openafs-client openafs-krb5
 		sudo service openafs-client start
+		easy_install pip
+		pip install numpy
 	fi
 	
 	if [[ $BUILD_manual == 'ON' ]]; then
