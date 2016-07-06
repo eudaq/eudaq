bbc3553a0b37f0e86e2cf92927b66c3da17e25c8
Andre Rummler <andre.rummler@tu-dortmund.de>
2016-07-06 11:30:49 +0200

    Fix problem with cmake on mac (framework version mismatch):     https://github.com/usnistgov/REFPROP-wrappers/blob/master/cmake/FindPythonLibsNew.cmake     https://github.com/pybind/pybind11/issues/99     http://stackoverflow.com/questions/30470628/os-x-cmake-cant-find-pythonlibs-3-4     https://github.com/Homebrew/legacy-homebrew/issues/10393#issuecomment-4108011

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,42 +6,54 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 		brew update
 		brew unlink cmake python python3
 		brew install libusb qt5 libusb-compat
+		
 		brew install python3
 		brew linkapps python3
-		virtualenv venv -p python3
-		source venv/bin/activate
+		#export python_workaround = cmake -DPYTHON_LIBRARY=$(python-config --prefix)/lib/libpython2.7.dylib -DPYTHON_INCLUDE_DIR=$(python-config --prefix)/include/python2.7
+		export OVERRIDE_PYTHON_INTERPRETER=-DPYTHON_EXECUTABLE:FILEPATH=$(python-config --prefix)"/bin/python3"
+		export OVERRIDE_PYTHON_LIBRARY_PATH=-DPYTHON_LIBRARY=$(python-config --prefix)"/lib/libpython3.4.dylib"
+		
 		export CMAKE_PREFIX_PATH=/usr/local/opt/qt5
 		# Install numpy via pip:
-		easy_install pip3
-		pip3 install numpy
+		sudo pip install --upgrade pip
+		sudo pip install -q numpy
 	else
 		# Install package dependencies for Mac OS X:
 		brew update
 		brew unlink cmake python python3
 		brew install libusb qt libusb-compat
+		
 		brew install python
 		brew linkapps python
-		virtualenv venv -p python3
-		source venv/bin/activate
+		export OVERRIDE_PYTHON_INTERPRETER=-DPYTHON_EXECUTABLE:FILEPATH=$(python-config --prefix)"/bin/python"
+		export OVERRIDE_PYTHON_LIBRARY_PATH=-DPYTHON_LIBRARY=$(python-config --prefix)"/lib/libpython2.7.dylib"
+
 		# Install numpy via pip:
-		easy_install pip
-		pip install numpy	
+		sudo pip install --upgrade pip
+		sudo pip install -q numpy	
 	fi
 	
 else 
 
 	if [[ $OPTION == 'modern' ]]; then
 		# Install package dependencies for Linux:
-		sudo apt-get install -y libusb-dev libusb-1.0-0 libusb-1.0-0-dev cmake qt5-default linux-generic linux-headers-$(uname -r) openafs-client openafs-krb5
+		sudo apt-get install -y python3 python3-pip libusb-dev libusb-1.0-0 libusb-1.0-0-dev cmake qt5-default linux-generic linux-headers-$(uname -r) openafs-client openafs-krb5
 		sudo service openafs-client start
-		easy_install pip
-		pip install numpy
+		sudo pip3 install -U setuptools
+		sudo pip3 install -U virtualenvwrapper
+		python3 -V
+		pip3 -V
+		sudo pip3 install -q numpy
+		export OVERRIDE_PYTHON_INTERPRETER="-DPYTHON_EXECUTABLE:FILEPATH=/usr/bin/python3"
+		export OVERRIDE_PYTHON_LIBRARY_PATH="-DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.4m.so.1"
 	else
 		# Install package dependencies for Linux:
-		sudo apt-get install -y libusb-dev libusb-1.0-0 libusb-1.0-0-dev cmake qt4-dev-tools linux-generic linux-headers-$(uname -r) openafs-client openafs-krb5
+		sudo apt-get install -y python2 libusb-dev libusb-1.0-0 libusb-1.0-0-dev cmake qt4-dev-tools linux-generic linux-headers-$(uname -r) openafs-client openafs-krb5
 		sudo service openafs-client start
-		easy_install pip
-		pip install numpy
+		sudo pip install --upgrade pip
+		sudo pip install -q numpy
+		export OVERRIDE_PYTHON_INTERPRETER="-DPYTHON_EXECUTABLE:FILEPATH=/usr/bin/python"
+		export OVERRIDE_PYTHON_LIBRARY_PATH="-DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython2.7.so"
 	fi
 	
 	if [[ $BUILD_manual == 'ON' ]]; then
