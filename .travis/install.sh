31c1ce4bd28a29f9cef4886feb31c894006d0b2e
Andre Rummler <andre.rummler@tu-dortmund.de>
2016-07-10 01:21:33 +0200

    test

--- .travis/install.sh
+++ .travis/install.sh
@@ -9,8 +9,9 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 		
 		brew install python3
 		brew linkapps python3
-		export python_workaround = cmake -DPYTHON_LIBRARY=$(python-config --prefix)/lib/libpython2.7.dylib -DPYTHON_INCLUDE_DIR=$(python-config --prefix)/include/python2.7
-		echo $(python-config --prefix)
+		#export python_workaround = cmake -DPYTHON_LIBRARY=$(python-config --prefix)/lib/libpython2.7.dylib -DPYTHON_INCLUDE_DIR=$(python-config --prefix)/include/python2.7
+		export OVERRIDE_PYTHON_INTERPRETER=-DPYTHON_EXECUTABLE:FILEPATH=$(python-config --prefix)/bin/python3
+		export OVERRIDE_PYTHON_LIBRARY_PATH=$(python-config --prefix)/lib/libpython3.4.dylib
 		
 		export CMAKE_PREFIX_PATH=/usr/local/opt/qt5
 		# Install numpy via pip:
@@ -24,6 +25,8 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 		
 		brew install python
 		brew linkapps python
+		export OVERRIDE_PYTHON_INTERPRETER=-DPYTHON_EXECUTABLE:FILEPATH=$(python-config --prefix)/bin/python
+		export OVERRIDE_PYTHON_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/libpython2.7.dylib
 
 		# Install numpy via pip:
 		sudo pip install --upgrade pip
@@ -41,12 +44,16 @@ else
 		python3 -V
 		pip3 -V
 		sudo pip3 install -q numpy
+		export OVERRIDE_PYTHON_INTERPRETER=-DPYTHON_EXECUTABLE:FILEPATH=/usr/bin/python3
+		export OVERRIDE_PYTHON_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/libpython3.4m.so.1
 	else
 		# Install package dependencies for Linux:
-		sudo apt-get install -y python libusb-dev libusb-1.0-0 libusb-1.0-0-dev cmake qt4-dev-tools linux-generic linux-headers-$(uname -r) openafs-client openafs-krb5
+		sudo apt-get install -y python2 libusb-dev libusb-1.0-0 libusb-1.0-0-dev cmake qt4-dev-tools linux-generic linux-headers-$(uname -r) openafs-client openafs-krb5
 		sudo service openafs-client start
 		sudo pip install --upgrade pip
 		sudo pip install -q numpy
+		export OVERRIDE_PYTHON_INTERPRETER=-DPYTHON_EXECUTABLE:FILEPATH=/usr/bin/python
+		export OVERRIDE_PYTHON_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/libpython2.7.so
 	fi
 	
 	if [[ $BUILD_manual == 'ON' ]]; then
