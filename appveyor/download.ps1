8ff8ccac6214ae805f762830b54711ef8cba2da2
Andre Rummler <andre.rummler@tu-dortmund.de>
2016-07-27 01:00:27 +0200

    appveyor

--- appveyor/download.ps1
+++ appveyor/download.ps1
@@ -10,32 +10,32 @@ function Download ($filename, $url) {
     $webclient = New-Object System.Net.WebClient
 
     #$basedir = $pwd.Path + "\"
-    $basedir = "C:\\"
-    $filepath = $basedir + $filename
+    #$basedir = "C:\\"
+    #$filepath = $basedir + $filename
     #if (Test-Path $filename) {
     #    Write-Host "Reusing" $filepath
     #    return $filepath
     #}
 
     # Download and retry up to 5 times in case of network transient errors.
-    Write-Host "Downloading" $filename "from" $url
+    Write-Host "Downloading " $url
     $retry_attempts = 4
     for ($i = 0; $i -lt $retry_attempts; $i++) {
         try {
-            $webclient.DownloadFile($url, $filepath)
+            $webclient.DownloadFile($url, $filename)
             break
         }
         Catch [Exception]{
             Start-Sleep 1
         }
     }
-    if (Test-Path $filepath) {
-        Write-Host "File saved at" $filepath
+    if (Test-Path $filename) {
+        Write-Host "File saved at" $filename
     } else {
         # Retry once to get the error message if any at the last try
-        $webclient.DownloadFile($url, $filepath)
+        $webclient.DownloadFile($url, $filename)
     }
-    return $filepath
+    return $filename
 }
 
 Download($PAR_FILENAME, $PAR_BASE_URL)
