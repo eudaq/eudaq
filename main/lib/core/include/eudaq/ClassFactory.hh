a70aa148c70afc65130bba547da4fc2c7f34a100
yiliu <yi.liu@desy.de>
2016-08-02 02:30:53 +0200

    factory: add missing #include <functional> (error in windows)

--- main/lib/core/include/eudaq/ClassFactory.hh
+++ main/lib/core/include/eudaq/ClassFactory.hh
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <sstream>
 #include <utility>
+#include <functional>
 
 #include"Platform.hh"
 
--- producers/processor/include/Factory.hh
+++ producers/processor/include/Factory.hh
@@ -4,6 +4,7 @@
 #include <memory>
 #include <iostream>
 #include <utility>
+#include <functional>
 #include "Platform.hh"
 
 namespace eudaq{
