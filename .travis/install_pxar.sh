17bdbaf148a1451970f7a2cb0652da4cc085cc4e
Andre Rummler <andre.rummler@tu-dortmund.de>
2016-08-07 00:46:07 +0200

    python

---
+++ .travis/install_pxar.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This package is necessary for the CMS pixel option
+
+if [ $TRAVIS_OS_NAME == linux ]; then sudo apt-get update && sudo apt-get install -y libusb-1.0-0 libusb-1.0-0-dev libftdi1 libftdi-dev; fi
+
+if [ $TRAVIS_OS_NAME == osx ]; then brew update && brew install libusb libftdi homebrew/science/root; fi
+
+cd --
+
+git clone https://github.com/simonspa/pxar.git
+
+cd pxar
+
+mkdir build
+
+cd build
+
+cmake ..
+
+make install
