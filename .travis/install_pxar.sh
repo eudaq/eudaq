b48eaca48e6b3bf539f793d8299d7ee5e14ce9ae
Andre Rummler <andre.rummler@tu-dortmund.de>
2016-08-07 08:15:18 +0200

    python

--- .travis/install_pxar.sh
+++ .travis/install_pxar.sh
@@ -6,7 +6,7 @@ export temporary_path=`pwd`
 
 if [ $TRAVIS_OS_NAME == linux ]; then sudo apt-get update && sudo apt-get install -y libusb-1.0-0 libusb-1.0-0-dev libftdi1 libftdi-dev; fi
 
-if [ $TRAVIS_OS_NAME == osx ]; then brew update && brew install libusb libftdi homebrew/science/root; fi
+if [ $TRAVIS_OS_NAME == osx ]; then brew update && brew install libusb libftdi; fi
 
 cd --
 
