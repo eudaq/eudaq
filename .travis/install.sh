d6ea1075ce82a188fc606a961b5e10f23921bc85
Andre Rummler <andre.rummler@tu-dortmund.de>
2016-08-14 01:22:01 +0200

    checking afs

--- .travis/install.sh
+++ .travis/install.sh
@@ -56,10 +56,10 @@ else
 		sudo service openafs-client start
 		uname -a
 		ls /etc/openafs
-		less /etc/openafs/CellServDB
-		less /etc/openafs/afs.conf
-		less /etc/openafs/afs.conf.client
-		less /etc/openafs/cacheinfo
+		cat /etc/openafs/CellServDB
+		cat /etc/openafs/afs.conf
+		cat /etc/openafs/afs.conf.client
+		cat /etc/openafs/cacheinfo
 		#sudo pip install --upgrade pip
 		#sudo pip install -q numpy
 		#pyenv rehash
