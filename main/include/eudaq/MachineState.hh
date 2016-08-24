d72be0869df9dd2d99d7a6614398291a52eb9428
yiliu <yi.liu@desy.de>
2016-08-25 00:55:55 +0200

    py: export symbol of class MachineState, PR #268

--- main/include/eudaq/MachineState.hh
+++ main/include/eudaq/MachineState.hh
@@ -4,37 +4,24 @@
 #include <string>
 #include <vector>
 #include <map>
+#include "eudaq/Platform.hh"
 #include "eudaq/ConnectionState.hh"
 #include "eudaq/TransportBase.hh"
 
-
 namespace eudaq
 {
-	class MachineState
-
-	{
-	public:
-
-		MachineState();
-		int GetState(); //Returns the state of the whole machine 
-		int GetState(ConnectionInfo id); // Returns the state of a single connection
-		//int GetState(int index); //Returns the state of connection at index
-        void SetState(ConnectionInfo* id, ConnectionState* state); // Sets the connection associated with id to state.
-														//if the connection does not exist, add it to the array
-
-		bool HasRunning(); //Returns true if there are running connections
-		void RemoveState(ConnectionInfo id); //Removes a connection when it is disconnected....
-
-		void Print();
-	private:
-		
-        std::map<ConnectionInfo, ConnectionState> connection_status_info;
-
-	};
-
-
-
-
+  class DLLEXPORT MachineState{
+  public:
+    MachineState();
+    int GetState(); //Returns the state of the whole machine 
+    int GetState(ConnectionInfo id); // Returns the state of a single connection
+    void SetState(ConnectionInfo* id, ConnectionState* state); // Sets the connection associated with id to state.
+    bool HasRunning(); //Returns true if there are running connections
+    void RemoveState(ConnectionInfo id); //Removes a connection when it is disconnected....    
+    void Print();
+  private:
+    std::map<ConnectionInfo, ConnectionState> connection_status_info;
+  };
 
 }
 
