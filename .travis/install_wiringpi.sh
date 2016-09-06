48645f430b99988bfc23be33cebdb384d08981e1
Andre Rummler <andre.rummler@tu-dortmund.de>
2016-09-06 16:50:00 +0200

    WiringPi installation repair

--- .travis/install_wiringpi.sh
+++ .travis/install_wiringpi.sh
@@ -4,6 +4,8 @@
 
 export temporary_path=`pwd`
 
+echo "Installing wiringPi library"
+
 #if [ $TRAVIS_OS_NAME == linux ]; then sudo apt-get update && sudo apt-get install -y libusb-1.0-0 libusb-1.0-0-dev libftdi1 libftdi-dev; fi
 
 #if [ $TRAVIS_OS_NAME == osx ]; then brew update && brew install libusb libftdi; fi
