2362dc7dfbbba08157800ccd1ec367d510f7b954
Felix Reidt <felix.reidt@cern.ch>
2016-09-30 11:44:44 +0200

    changed everything for the use with a 4 channel HAMEG

--- producers/palpidefs/scripts/hameg.py
+++ producers/palpidefs/scripts/hameg.py
@@ -48,7 +48,18 @@ def power_on(hameg, i_max, voltages):
     hameg.write("*RST\n")
     time.sleep(1)
     # activate digital fuse and set outputs to 0V
-    # CH3
+    # CH4
+    hameg.write("INST OUT4\n");
+    hameg.write("OUTP OFF\n")
+    hameg.write("FUSE:LINK 1\n")
+    hameg.write("FUSE:LINK 2\n")
+    hameg.write("FUSE on\n")
+    hameg.write("FUSE:DEL 50\n")
+    hameg.write("SOUR:VOLT %f\n" % voltages[3])
+    hameg.write("SOUR:CURR %f\n" % (float(i_max[3])/1000.))
+    time.sleep(0.5)
+    hameg.write("OUTP ON\n")
+    #CH3
     hameg.write("INST OUT3\n");
     hameg.write("OUTP OFF\n")
     hameg.write("FUSE:LINK 1\n")
@@ -86,7 +97,7 @@ def power_on(hameg, i_max, voltages):
 def check_trip(sour):
     # check compliance
     tripped = False
-    for c in range(3):
+    for c in range(4):
         sour.write("INST OUT%d\n" % (c+1))
         sour.write("FUSE:TRIP?\n")
         if (int(sour.readline())!=0):
@@ -110,9 +121,9 @@ def meas_voltage(sour, channel):
 
 ###############################################################################
 def measure_all(sour, of):
-    current=([0.0, 0.0, 0.0])
-    voltage=([0.0, 0.0, 0.0])
-    for c in range(3):
+    current=([0.0, 0.0, 0.0, 0.0])
+    voltage=([0.0, 0.0, 0.0, 0.0])
+    for c in range(4):
         sour.write("INST OUT%d\n" % (c+1))
         sour.write("MEAS:CURR?\n")
         current[c] = float(sour.readline())
@@ -125,13 +136,13 @@ def measure_all(sour, of):
 
 ###############################################################################
 def measure_all_print(sour):
-    current=([0.0, 0.0, 0.0])
-    voltage=([0.0, 0.0, 0.0])
-    for c in range(3):
+    current=([0.0, 0.0, 0.0, 0.0])
+    voltage=([0.0, 0.0, 0.0, 0.0])
+    for c in range(4):
         sour.write("INST OUT%d\n" % (c+1))
         sour.write("MEAS:CURR?\n")
         current[c] = float(sour.readline())
         sour.write("MEAS:VOLT?\n")
         voltage[c] = float(sour.readline())
-    print "V1 (4DUTs)\tI1\t| V2 (3DUTs)\tI2\t| Vbb\t\tIbb"
-    print "%0.4fV\t\t%0.4fA\t| %0.4fV\t%0.4fA\t| %0.4fV\t%0.4fA\n" % ( voltage[0], current[0], voltage[1], current[1], voltage[2], current[2])
+    print "V1 (4DUTs)\tI1\t| V2 (3DUTs)\tI2\t| Vbb\t\tIbb\t| VbbControl\tIbbControl"
+    print "%0.4fV\t\t%0.4fA\t| %0.4fV\t%0.4fA\t| %0.4fV\t%0.4fA\t| %0.4fV\t%0.4fA\n" % ( voltage[0], current[0], voltage[1], current[1], voltage[2], current[2], voltage[3], current[3])
