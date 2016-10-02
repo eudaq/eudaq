8536e59c73f09ea6e63e6827d099663a9440a648
Andre Rummler <andre.rummler@tu-dortmund.de>
2016-10-02 19:28:00 +0200

    more pxar work

--- appveyor/install_pxar.ps1
+++ appveyor/install_pxar.ps1
@@ -6,15 +6,23 @@ function main(){
    
    Start-Process "git" -ArgumentList "clone https://github.com/simonspa/pxar.git" -Wait;
    
+   Write-Host("Checked out pxarcore from https://github.com/simonspa/pxar.git");
+   
    cd pxar;
    
    Start-Process "git" -ArgumentList "checkout testbeam-2016" -Wait;
+   
+   Write-Host("Switched to branch checkout testbeam-2016");
    
    mkdir build;
    
    cd build;
    
+   Write-Host("Switched to build folder");   
+   
    Start-Process "cmake" -ArgumentList ".." -Wait;
+   
+   Write-Host("cmake finished");
    
    Start-Process "make" -ArgumentList "install" -Wait;
    
