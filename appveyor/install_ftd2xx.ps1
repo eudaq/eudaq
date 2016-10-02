ea03bed900229cd58beaa920eadda322c023b44e
Andre Rummler <andre.rummler@tu-dortmund.de>
2016-10-02 22:25:39 +0200

    more pxar work

--- appveyor/install_ftd2xx.ps1
+++ appveyor/install_ftd2xx.ps1
@@ -4,7 +4,7 @@ function main(){
    
    Push-Location -StackName entryPath -Path "C:\projects\eudaq\extern" ;
    
-   If (Test-Path ("${env:APPVEYOR_BUILD_FOLDER}" + "\extern\ftd2xx")) {Write-Host ("Reusing cached ftd2xx " + "${env:APPVEYOR_BUILD_FOLDER}" + "\extern\ftd2xx" + " instead of downloading from ftdichip.com")} Else {Write-Host "Downloading ftdi2xx"; appveyor\download.ps1 -downloadLocation 'http://www.ftdichip.com/Drivers/CDM/CDM v2.12.18 WHQL Certified.zip' -storageLocation 'C:\\ftd2xx.zip'; $zipargument = ("-o" + "${env:APPVEYOR_BUILD_FOLDER}" + "\extern\"); 7z -y x C:\\ftd2xx.zip $zipargument; }   
+   If (Test-Path ("${env:APPVEYOR_BUILD_FOLDER}" + "\extern\ftd2xx")) {Write-Host ("Reusing cached ftd2xx " + "${env:APPVEYOR_BUILD_FOLDER}" + "\extern\ftd2xx" + " instead of downloading from ftdichip.com")} Else {Write-Host "Downloading ftdi2xx"; download.ps1 -downloadLocation 'http://www.ftdichip.com/Drivers/CDM/CDM v2.12.18 WHQL Certified.zip' -storageLocation 'C:\\ftd2xx.zip'; $zipargument = ("-o" + "${env:APPVEYOR_BUILD_FOLDER}" + "\extern\"); 7z -y x C:\\ftd2xx.zip $zipargument; }   
    
    Pop-Location -StackName entryPath -PassThru ;
    
