041246c8c8a69a4150d766a87acec8300c4ab5e2
Andre Rummler <andre.rummler@tu-dortmund.de>
2016-10-04 01:57:27 +0200

    Added palpideds to travis CI

---
+++ .travis/install_palpidefs_driver.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This package is necessary for the palpidefs producer
+
+export temporary_path=`pwd`
+
+if [ $TRAVIS_OS_NAME == linux ]; then sudo apt-get update && sudo apt-get install -y libtinyxml-dev expect-dev libusb-1.0-0-dev; fi
+
+# http://apple.stackexchange.com/questions/193138/to-install-unbuffer-in-osx
+
+if [ $TRAVIS_OS_NAME == osx ]; then brew update && brew tap homebrew/dupes/expect && brew install tinyxml homebrew/dupes/expect libusb; fi
+
+cd --
+
+wget https://cernbox.cern.ch/index.php/s/QIRPTV84XziyQ3q/download
+
+unzip alice-its-alpide-software-master-latest.zip
+
+cd alice-its-alpide-software-master-3189f00d7515733d46a61a5ab2606e436df4955b
+
+cd pALPIDEfs-software
+
+make lib
+
+cd $temporary_path
