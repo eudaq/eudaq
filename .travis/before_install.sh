8f53bd7a3982556dfadd51d8117289eb1a3a9a34
Andre Rummler <andre.rummler@tu-dortmund.de>
2016-10-06 00:59:20 +0200

    testing new afs installation

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -6,12 +6,12 @@ echo `$CC --version`
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
 	# OS X: update brew cache:
-	brew update || brew update
+	brew update || brew upgrade
 	
 	if [[ "$CC" == "gcc" ]]; then CC=gcc-4.9; fi	
 	
 	echo `$CXX --version`
         echo `$CC --version`
-		
+
 fi
 	
--- .travis/install_cmake.sh
+++ .travis/install_cmake.sh
@@ -4,6 +4,7 @@
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 	
+	#brew uninstall cmake --force
 	brew unlink cmake
 	
 	export CMAKE_FILENAME=${CMAKE_FILENAME_MAC}
@@ -14,6 +15,7 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 	export PATH="`pwd`/${CMAKE_FILENAME%%.tar.gz}/CMake.app/Contents/bin":$PATH:	
 	echo $PATH
 	
+	brew link cmake
 else
 	sudo apt-get install --force-yes -y cmake 
 fi
