ad3ef8e4c1b5d60e18aa45344f93f51cc8a0f6ca
Andre Rummler <andre.rummler@tu-dortmund.de>
2016-10-06 16:27:21 +0200

    testing new afs installation

--- .travis/install_cmake.sh
+++ .travis/install_cmake.sh
@@ -4,22 +4,23 @@
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 	
-	brew uninstall cmake --force
+	
+	#brew uninstall cmake --force
 	#brew unlink cmake
 	
-	export CMAKE_FILENAME=${CMAKE_FILENAME_MAC}
+	#export CMAKE_FILENAME=${CMAKE_FILENAME_MAC}
 	
-	echo "Installing cmake now"
-	wget ${CMAKE_DOWNLOAD_PATH}/$CMAKE_FILENAME
-	tar xfz $CMAKE_FILENAME
+	#echo "Installing cmake now"
+	#wget ${CMAKE_DOWNLOAD_PATH}/$CMAKE_FILENAME
+	#tar xfz $CMAKE_FILENAME
 	#export PATH="`pwd`/${CMAKE_FILENAME%%.tar.gz}/CMake.app/Contents/bin":$PATH:	
 	#echo $PATH
-	mkdir -p /usr/local/Cellar/cmake
-	cp -r ${CMAKE_FILENAME%%.tar.gz}/CMake.app/Contents/* /usr/local/Cellar/cmake/
+	#mkdir -p /usr/local/Cellar/cmake
+	#cp -r ${CMAKE_FILENAME%%.tar.gz}/CMake.app/Contents/* /usr/local/Cellar/cmake/
 	
+	export PATH="/usr/local/bin:$PATH"
 	brew link cmake
+	cmake --version
 else
 	sudo apt-get install --force-yes -y cmake 
 fi
-	
-	
