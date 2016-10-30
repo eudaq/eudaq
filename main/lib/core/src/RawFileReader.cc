96cd8a526c2503fd89332d293ce71efc59eadd22
Yi Liu <yi.liu@desy.de>
2016-10-30 07:20:14 +0100

    RawFileReader: merge hh and cc,  since it is maintained by Factory<FileReader>

--- main/lib/core/src/RawFileReader.cc
+++ main/lib/core/src/RawFileReader.cc
@@ -1,36 +1,41 @@
 #include <list>
-#include "RawFileReader.hh"
 #include "FileNamer.hh"
+#include "FileSerializer.hh"
+#include "FileReader.hh"
 #include "PluginManager.hh"
 #include "Event.hh"
 #include "Logger.hh"
-#include "FileSerializer.hh"
 #include "Configuration.hh"
 
 namespace eudaq {
+  class RawFileReader;
   namespace{
     auto dummy0 = Factory<FileReader>::Register<RawFileReader, std::string&>(cstr2hash("RawFileReader"));
     auto dummy1 = Factory<FileReader>::Register<RawFileReader, std::string&&>(cstr2hash("RawFileReader"));
     auto dummy2 = Factory<FileReader>::Register<RawFileReader, std::string&>(cstr2hash("raw"));
     auto dummy3 = Factory<FileReader>::Register<RawFileReader, std::string&&>(cstr2hash("raw"));
   }
-  
-  RawFileReader::RawFileReader(const std::string & file, const std::string & filepattern)
-    : FileReader(FileNamer(filepattern).Set('X', ".raw").SetReplace('R', file)),
-    m_des(Filename()),m_ver(1){
-    uint32_t id;
-    m_des.PreRead(id);
-    m_ev = Factory<eudaq::Event>::Create<Deserializer&>(id, m_des);
-  }
+
+  class DLLEXPORT RawFileReader : public FileReader {
+  public:
+    RawFileReader(const std::string& filename);
+    ~RawFileReader(){};
+    
+    unsigned RunNumber() const final override;
+    bool NextEvent(size_t skip = 0)final override;
+    EventSPC GetNextEvent()final override;
+    void Interrupt() final override { m_des.Interrupt(); }
+
+  private:
+    FileDeserializer m_des;
+    EventSP m_ev;
+    unsigned m_ver;
+  };
 
   RawFileReader::RawFileReader(const std::string& file):FileReader(file), m_des(Filename()),m_ver(1){
     uint32_t id;
     m_des.PreRead(id);
     m_ev = Factory<eudaq::Event>::Create<Deserializer&>(id, m_des);
-  }
-
-  RawFileReader::~RawFileReader() {
-
   }
 
   bool RawFileReader::NextEvent(size_t skip) {
