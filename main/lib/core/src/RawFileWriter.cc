9c44e956148138a61971d206a2b3ba7237f2b776
Yi Liu <yi.liu@desy.de>
2016-10-30 07:36:03 +0100

    RawFileWriter: rename class name as the souce file name

--- main/lib/core/src/RawFileWriter.cc
+++ main/lib/core/src/RawFileWriter.cc
@@ -4,52 +4,51 @@
 #include "PluginManager.hh"
 
 namespace eudaq {
-  class FileWriterNative;
+  class RawFileWriter;
 
   namespace{
-    auto dummy0 = Factory<FileWriter>::Register<FileWriterNative, std::string&>(cstr2hash("native"));
-    auto dummy1 = Factory<FileWriter>::Register<FileWriterNative, std::string&&>(cstr2hash("native"));
-    auto dummy2 = Factory<FileWriter>::Register<FileWriterNative, std::string&>(cstr2hash("RawFileWriter"));
-    auto dummy3 = Factory<FileWriter>::Register<FileWriterNative, std::string&&>(cstr2hash("RawFileWriter"));
+    auto dummy0 = Factory<FileWriter>::Register<RawFileWriter, std::string&>(cstr2hash("native"));
+    auto dummy1 = Factory<FileWriter>::Register<RawFileWriter, std::string&&>(cstr2hash("native"));
+    auto dummy2 = Factory<FileWriter>::Register<RawFileWriter, std::string&>(cstr2hash("RawFileWriter"));
+    auto dummy3 = Factory<FileWriter>::Register<RawFileWriter, std::string&&>(cstr2hash("RawFileWriter"));
   }
   
-  class FileWriterNative : public FileWriter {
+  class RawFileWriter : public FileWriter {
   public:
-    FileWriterNative(const std::string &);
+    RawFileWriter(const std::string &);
     virtual void StartRun(unsigned);
     virtual void WriteEvent(const DetectorEvent &);
     virtual uint64_t FileBytes() const;
-    virtual ~FileWriterNative();
+    virtual ~RawFileWriter();
 
   private:
     FileSerializer *m_ser;
   };
 
-  FileWriterNative::FileWriterNative(const std::string & param) : m_ser(0){
-    //EUDAQ_DEBUG("Constructing FileWriterNative(" + to_string(param) + ")");
+  RawFileWriter::RawFileWriter(const std::string & param) : m_ser(0){
+    //EUDAQ_DEBUG("Constructing RawFileWriter(" + to_string(param) + ")");
   }
 
-  void FileWriterNative::StartRun(unsigned runnumber) {
+  void RawFileWriter::StartRun(unsigned runnumber) {
     delete m_ser;
     m_ser = new FileSerializer(
         FileNamer(m_filepattern).Set('X', ".raw").Set('R', runnumber));
   }
 
-  void FileWriterNative::WriteEvent(const DetectorEvent &ev) {
+  void RawFileWriter::WriteEvent(const DetectorEvent &ev) {
     if (!m_ser)
-      EUDAQ_THROW("FileWriterNative: Attempt to write unopened file");
+      EUDAQ_THROW("RawFileWriter: Attempt to write unopened file");
     if (ev.IsBORE()) {
       eudaq::PluginManager::Initialize(ev);
     }
-    if (!m_ser) EUDAQ_THROW("FileWriterNative: Attempt to write unopened file");
+    if (!m_ser) EUDAQ_THROW("RawFileWriter: Attempt to write unopened file");
     m_ser->write(ev);
     m_ser->Flush();
   }
   
-
-  FileWriterNative::~FileWriterNative() {
+  RawFileWriter::~RawFileWriter() {
     delete m_ser;
   }
 
-  uint64_t FileWriterNative::FileBytes() const { return m_ser ? m_ser->FileBytes() : 0; }
+  uint64_t RawFileWriter::FileBytes() const { return m_ser ? m_ser->FileBytes() : 0; }
 }
