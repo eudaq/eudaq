5d17df444ae8fea56e9dc99912e8d50a071cf305
Andre Rummler <andre.rummler@tu-dortmund.de>
2016-11-27 01:11:28 +0100

    Copied FindZest from v1.7

--- user/telescope/cmake/FindZestSC1.cmake
+++ user/telescope/cmake/FindZestSC1.cmake
@@ -5,19 +5,18 @@
 #  ZESTSC1_LIBRARIES - The libraries needed to use ZestSC1
 #  ZESTSC1_DEFINITIONS - Compiler switches required for using ZestSC1
 
-
 macro(find_zestsc1_in_extern arg)
 # disable a warning about changed behaviour when traversing directories recursively (wrt symlinks)
 IF(COMMAND cmake_policy)
   CMAKE_POLICY(SET CMP0009 NEW)
   CMAKE_POLICY(SET CMP0011 NEW) # disabling a warning about policy changing in this scope
 ENDIF(COMMAND cmake_policy)
-
-file(GLOB_RECURSE extern_file ${CMAKE_CURRENT_LIST_DIR}/../extern/*ZestSC1.h)
+# determine path to zestsc1 package in ./extern folder
+file(GLOB_RECURSE extern_file ${PROJECT_SOURCE_DIR}/extern/*ZestSC1.h)
 if (extern_file)
   # should have found multiple files of that name, take root of folder (no 'windows*7/inc' string)
   FOREACH (this_file ${extern_file})
-  	  IF(NOT "${this_file}" MATCHES ".*windows.?7/Inc/")
+  	  IF( (NOT "${this_file}" MATCHES ".*windows.?7/Inc/") AND (NOT "${this_file}" MATCHES ".*windows/Inc/") AND (NOT "${this_file}" MATCHES ".*macosx/Inc/") AND (NOT "${this_file}" MATCHES ".*linux/Inc/"))
 	        SET(zest_inc_path "${this_file}")
 	  ENDIF()
   ENDFOREACH(this_file)
@@ -77,10 +76,26 @@ endmacro()
 
 find_zestsc1_in_extern("")
 
+# could not find the package at the usual locations -- try to copy from AFS if accessible
 if (NOT ZESTSC1_LIBRARY)
-  MESSAGE(WARNING "Could not find ZestSC1 driver package required by tlu producer. Please refer to the documentation on how to obtain the software.")
-else()
-  set(ZestSC1_FOUND TRUE)
+  IF (EXISTS "/afs/desy.de/group/telescopes/tlu/ZestSC1")
+    MESSAGE(STATUS "Could not find ZestSC1 driver package required by tlu producer; downloading it now via AFS to ./extern ....")
+    if(DEFINED ENV{TRAVIS})
+    MESSAGE(STATUS "Running on travis and therefore downloading only the absolutely necessary part of the ZestSC1 driver.")
+    file(MAKE_DIRECTORY ${PROJECT_SOURCE_DIR}/extern/ZestSC1)
+    copy_files("/afs/desy.de/group/telescopes/tlu/ZestSC1/Inc" ${PROJECT_SOURCE_DIR}/extern/ZestSC1)
+    if("$ENV{TRAVIS_OS_NAME}" STREQUAL "linux")
+	copy_files("/afs/desy.de/group/telescopes/tlu/ZestSC1/linux" ${PROJECT_SOURCE_DIR}/extern/ZestSC1)
+    else()
+	copy_files("/afs/desy.de/group/telescopes/tlu/ZestSC1/macosx" ${PROJECT_SOURCE_DIR}/extern/ZestSC1)
+    endif()    
+    else()
+    copy_files("/afs/desy.de/group/telescopes/tlu/ZestSC1" ${PROJECT_SOURCE_DIR}/extern)
+    endif()
+    find_zestsc1_in_extern(NO_DEFAULT_PATH)
+  ELSE()
+    MESSAGE(WARNING "Could not find ZestSC1 driver package required by tlu producer. Please refer to the documentation on how to obtain the software.")
+  ENDIF()
 endif()
 
 set(ZESTSC1_LIBRARIES ${ZESTSC1_LIBRARY} )
