33f8c13487c43b821b31d978d5409f798abfe952
Andre Rummler <andre.rummler@tu-dortmund.de>
2016-12-08 16:59:37 +0100

    CI

--- .travis/install_afs.sh
+++ .travis/install_afs.sh
@@ -11,8 +11,8 @@
 echo "Entered install_afs.sh"
 echo "Installing afs"
 
-export OPENAFS_DOWNLOAD_PATH_MAC=https://www.auristor.com/downloads/auristor/osx/macos-10.11
-export OPENAFS_FILENAME_MAC=AuriStor-client-0.128-ElCapitan.dmg
+export OPENAFS_DOWNLOAD_PATH_MAC=https://www.auristor.com/downloads/auristor/osx/macos-10.12
+export OPENAFS_FILENAME_MAC=AuriStor-client-0.128-Sierra.dmg
 
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
