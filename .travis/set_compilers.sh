5ff8b3360c7376c66c9ae0d174226862882cba63
Andre Rummler <andre.rummler@tu-dortmund.de>
2016-12-09 12:46:48 +0100

    CI set compiler

--- .travis/set_compilers.sh
+++ .travis/set_compilers.sh
@@ -20,7 +20,7 @@ export DEPS_DIR=$HOME/dependencies
     
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
-	#echo "Setting C-Compiler to 4.9 for OSX"
+	echo "Setting C-Compiler for OSX"
 	#if [[ "$CC" == "gcc" ]]; then CC=gcc-4.9; fi	
 	
 else	
