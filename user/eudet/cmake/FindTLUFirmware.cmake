909f532e3dcc86b6e89ebdcbcb35cea374f4720a
Yi Liu <yi.liu@desy.de>
2016-12-12 12:59:20 +0100

    user/eudet: path of extern dependences is changed to user/eudet/extern

--- user/eudet/cmake/FindTLUFirmware.cmake
+++ user/eudet/cmake/FindTLUFirmware.cmake
@@ -4,8 +4,9 @@
 #  TLUFIRMWARE_DEFINITIONS - Compiler switches required for using TLUFirmware
 
 macro(find_bitfiles_in_extern arg)
-  find_path(TLUFIRMWARE_PATH TLU_Toplevel.bit
-    HINTS ${PROJECT_SOURCE_DIR}/extern/tlufirmware ${arg})
+  find_path(TLUFIRMWARE_PATH TLU_Toplevel.bit HINTS
+    ${CMAKE_CURRENT_LIST_DIR}/../extern/tlufirmware ${arg}
+    ${PROJECT_SOURCE_DIR}/extern/tlufirmware ${arg})
 endmacro()
 
 find_bitfiles_in_extern("")
@@ -13,8 +14,8 @@ find_bitfiles_in_extern("")
 # could not find the package at the usual locations -- try to copy from AFS if accessible
 if (NOT TLUFIRMWARE_PATH)
   IF (EXISTS "/afs/desy.de/group/telescopes/tlu/tlufirmware")
-    MESSAGE(STATUS "Could not find TLUFirmware bitfiles required by tlu producer; downloading them now via AFS to ./extern ....")
-    copy_files("/afs/desy.de/group/telescopes/tlu/tlufirmware" ${PROJECT_SOURCE_DIR}/extern)
+    MESSAGE(STATUS "Could not find TLUFirmware bitfiles required by tlu producer; downloading them now via AFS to ${TLUFIRMWARE_PATH} ....")
+    copy_files("/afs/desy.de/group/telescopes/tlu/tlufirmware" ${CMAKE_CURRENT_LIST_DIR}/../extern)
     find_bitfiles_in_extern(NO_DEFAULT_PATH)
   ELSE()
     MESSAGE(WARNING "Could not find TLUFirmware bitfiles required by tlu producer. Please refer to the documentation on how to obtain them.")
