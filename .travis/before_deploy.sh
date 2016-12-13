9860ce2e370649c5cbc403286054f88c8288964c
Andre Rummler <andre.rummler@tu-dortmund.de>
2016-12-13 13:57:01 +0100

    CI binary upload

--- .travis/before_deploy.sh
+++ .travis/before_deploy.sh
@@ -6,8 +6,9 @@ FILE_TO_UPLOAD_2=""
 mkdir ~/packaging
 
 if [[ $BUILD_manual == 'ON' ]]; then
-	cp $TRAVIS_BUILD_DIR/doc/manual/EUDAQUserManual.pdf ~/packaging/EUDAQUserManual-$TRAVIS_TAG.pdf
+	cp $TRAVIS_BUILD_DIR/doc/manual/EUDAQUserManual.pdf ~/packaging/"EUDAQUserManual-$TRAVIS_TAG.pdf"
 	export FILE_TO_UPLOAD_1="~/packaging/EUDAQUserManual-$TRAVIS_TAG.pdf"
+	ls ~/packaging
 	ls $FILE_TO_UPLOAD_1
 else
 	export FILE_TO_UPLOAD_1=""
