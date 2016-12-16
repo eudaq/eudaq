3b545f0e4ae9dc1cc71016cc47d8578c6c0d3e59
Felix Reidt <felix.reidt@cern.ch>
2016-12-16 13:32:46 +0100

    removed the code for the back-bias relais

--- producers/palpidefs/scripts/change_back_bias.py
+++ producers/palpidefs/scripts/change_back_bias.py
@@ -10,10 +10,6 @@ def main():
     sour=serial.Serial(h.dev, h.baud_rate, rtscts=h.rtscts)
     ret_value = 0 # 0 = success, 1 = error
 
-    # activate the back bias line
-    if vbb_dst>0:
-        h.ramp_from_to(sour, 3, (0., 5.), 1)
-
     for ivbb in range (0, len(h.vbb_chan)):
         vbb_src=h.meas_voltage(sour, h.vbb_chan[ivbb])
         if abs((vbb_dst - vbb_src)/(vbb_dst + 1.e-3)) < 1.e-3:
@@ -23,10 +19,6 @@ def main():
         vbb_meas=h.meas_voltage(sour, h.vbb_chan[ivbb])
         if abs((vbb_dst - vbb_meas)/(vbb_dst + 1.e-3)) > 1.e-3:
             ret_value = 1
-
-    # deactivate Vbb line and short it
-    if vbb_dst<0.1:
-        h.ramp_from_to(sour, 3, (5., 0.), 1)
 
     return ret_value
 
