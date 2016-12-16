bb6ea6719f09a3fa51d2a18f18be5af5d4938085
Felix Reidt <felix.reidt@cern.ch>
2016-12-16 17:47:53 +0100

    fixing appveyor script

--- appveyor/install_palpidefs_driver.ps1
+++ appveyor/install_palpidefs_driver.ps1
@@ -4,18 +4,18 @@ function main(){
      
    Push-Location -StackName entryPath -Path "C:\projects\eudaq\extern" ;
    
-   If (Test-Path ("${env:APPVEYOR_BUILD_FOLDER}" + "\extern\alice-its-alpide-software-master-3189f00d7515733d46a61a5ab2606e436df4955b")) {
-	Write-Host ("Reusing cached alice-its-alpide-software-master-latest " + "${env:APPVEYOR_BUILD_FOLDER}" + "\extern\alice-its-alpide-software-master-3189f00d7515733d46a61a5ab2606e436df4955b" + " instead of downloading from cernbox")
+   If (Test-Path ("${env:APPVEYOR_BUILD_FOLDER}" + "\extern\alice-its-alpide-software-TestBeamStable_ALPIDE_2016-56948c980cccf12408059628d758d86a39f27454")) {
+	Write-Host ("Reusing cached alice-its-alpide-software-master-latest " + "${env:APPVEYOR_BUILD_FOLDER}" + "\extern\alice-its-alpide-software-TestBeamStable_ALPIDE_2016-56948c980cccf12408059628d758d86a39f27454" + " instead of downloading from cernbox")
 	} 
 	Else {
 	Write-Host "Downloading alice-its-alpide-software-master from cernbox"; 
 	C:\\projects\eudaq\appveyor\download.ps1 -downloadLocation 'https://cernbox.cern.ch/index.php/s/QIRPTV84XziyQ3q/download' -storageLocation 'C:\\alice-its-alpide-software-master-latest.zip'; 
 	$zipargument = ("-o" + "${env:APPVEYOR_BUILD_FOLDER}" + "\extern\"); 
 	7z -y x C:\\alice-its-alpide-software-master-latest.zip $zipargument;  
-	#Rename-Item -path ("${env:APPVEYOR_BUILD_FOLDER}" + "\extern\alice-its-alpide-software-master-3189f00d7515733d46a61a5ab2606e436df4955b") -newName ("${env:APPVEYOR_BUILD_FOLDER}" + "\extern\alice-its-alpide-software-master-latest")
+	#Rename-Item -path ("${env:APPVEYOR_BUILD_FOLDER}" + "\extern\alice-its-alpide-software-TestBeamStable_ALPIDE_2016-56948c980cccf12408059628d758d86a39f27454") -newName ("${env:APPVEYOR_BUILD_FOLDER}" + "\extern\alice-its-alpide-software-master-latest")
 	}
    
-   cd alice-its-alpide-software-master-3189f00d7515733d46a61a5ab2606e436df4955b;
+   cd alice-its-alpide-software-TestBeamStable_ALPIDE_2016-56948c980cccf12408059628d758d86a39f27454;
    
    cd pALPIDEfs-software
    
