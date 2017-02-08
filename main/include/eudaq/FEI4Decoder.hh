02d1a66caf158f5444d813b78175ed146c9cff87
tbisanz <tobias.bisanz@phys.uni-goettingen.de>
2017-02-08 13:44:20 +0100

    FEI4Decoder now works for Gen2 data

--- main/include/eudaq/FEI4Decoder.hh
+++ main/include/eudaq/FEI4Decoder.hh
@@ -1,5 +1,10 @@
 #ifndef EUDAQ_INCLUDED_FEI4Decoder
-#define EUDAQ_INCLUDED_FEI4Decoder
+#define EUDAQ_INCLUDED_FEI4Decoder 1
+
+#include <bitset>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 namespace eudaq {
 
@@ -15,69 +20,14 @@ const uint8_t value_record = header | 0x4;		//0b11101 100
 const uint8_t service_record = header | 0x7;	//0b11101 111
 
 struct APIXPix {
-	int x, y, tot1, tot2, channel, lv1;
-	APIXPix(int x, int y, int tot1, int tot2, int lv1, int channel): 
-		x(x), y(y), tot1(tot1), tot2(tot2), lv1(lv1), channel(channel) {};
+	int x, y, tot, channel, lv1;
+	APIXPix(int x, int y, int tot, int lv1, int channel): 
+		x(x), y(y), tot(tot), lv1(lv1), channel(channel) {};
 };
 
-std::vector<APIXPix> decodeFEI4Data(std::vector<unsigned char> const & data) {
-	std::vector<APIXPix> result;
-
-	for(size_t index = 0;  index < data.size(); index+=4) {
-      uint32_t i =	( static_cast<uint32_t>(data[index+3]) << 24 ) | 
-                	( static_cast<uint32_t>(data[index+2]) << 16 ) | 
-					( static_cast<uint32_t>(data[index+1]) << 8 ) | 
-					( static_cast<uint32_t>(data[index+0]) );
-
-		uint8_t channel = selectBits(i, 24, 8);
-		uint8_t data_headers = 0;
-
-		if(channel >> 7) { //Trigger
-            data_headers = 0;
-			uint32_t trigger_number = selectBits(i, 0, 31); //testme
-		} else {
-			uint8_t type = selectBits(i, 16, 8);
-
-			switch(type) {
-				case data_header: {
-					//int bcid = selectBits(i, 0, 10);
-					//int lv1id = selectBits(i, 10, 5);
-					//int flag = selectBits(i, 15, 1);
-					data_headers++;
-					break;
-				}
-				case address_record: {
-					//int type = selectBits(i, 15, 1);
-					//int address = selectBits(i, 0, 15);
-					break;
-				}
-				case value_record: {
-					//int value = selectBits(i, 0, 16);
-					break;
-				}
-				case service_record: {
-					//int code = selectBits(i, 10, 6);
-					//int count = selectBits(i, 0, 10);
-					break;
-				}
-				default: { //data record
-					unsigned tot2 = selectBits(i, 0, 4);
-					unsigned tot1 = selectBits(i, 4, 4);
-					unsigned row = selectBits(i, 8, 9) - 1;
-					unsigned column = selectBits(i, 17, 7) - 1;
-
-					if(column < 80 && ((tot2 == 15 && row < 336) || (tot2 < 15 && row < 335) )) {
-						result.emplace_back(column, row, tot1, tot2, data_headers-1, channel);
-					} else { // invalid data record
-						//invalid_dr ++;
-					}
-					break;
-				}
-			}
-		}
-	}
-	return result;
-}
+std::vector<int> getChannels(std::vector<unsigned char> const & data);
+std::vector<APIXPix> decodeFEI4DataGen2(std::vector<unsigned char> const & data);
+std::vector<APIXPix> decodeFEI4Data(std::vector<unsigned char> const & data);
 
 }
 
---
+++ main/lib/src/FEI4Decoder.cc
@@ -0,0 +1,149 @@
+#include "eudaq/FEI4Decoder.hh"
+
+namespace eudaq{
+
+
+std::vector<int> getChannels(std::vector<unsigned char> const & data) {
+    std::vector<int> channels;
+    for(size_t index = 0;  index < data.size(); index+=4) {
+        uint32_t i =( static_cast<uint32_t>(data[index+3]) << 24 ) | 
+                	( static_cast<uint32_t>(data[index+2]) << 16 ) | 
+					( static_cast<uint32_t>(data[index+1]) << 8 ) | 
+					( static_cast<uint32_t>(data[index+0]) );
+
+		uint8_t channel = selectBits(i, 24, 8);
+
+		if(!(channel >> 7)) { //Trigger
+			if(std::find(channels.begin(), channels.end(), static_cast<int>(channel)) == channels.end()) {
+				channels.emplace_back(static_cast<int>(channel));
+			}
+		} 
+	}
+	return channels;
+}
+
+std::vector<APIXPix> decodeFEI4DataGen2(std::vector<unsigned char> const & data) {
+	std::vector<APIXPix> result;
+	size_t no_data_headers = 0;
+
+	for(size_t index = 0;  index < data.size(); index+=4) {
+        uint32_t i =( static_cast<uint32_t>(data[index+3]) << 24 ) | 
+                	( static_cast<uint32_t>(data[index+2]) << 16 ) | 
+					( static_cast<uint32_t>(data[index+1]) << 8 ) | 
+					( static_cast<uint32_t>(data[index+0]) );
+
+
+        uint8_t type = selectBits(i, 16, 8);
+        switch(type) {
+			case data_header: {
+				//int bcid = selectBits(i, 0, 10);
+				//int lv1id = selectBits(i, 10, 5);
+				//int flag = selectBits(i, 15, 1);
+				no_data_headers++;
+				break;
+			}
+			case address_record: {
+				//int type = selectBits(i, 15, 1);
+				//int address = selectBits(i, 0, 15);
+				break;
+			}
+			case value_record: {
+				//int value = selectBits(i, 0, 16);
+				break;
+			}
+			case service_record: {
+				//int code = selectBits(i, 10, 6);
+				//int count = selectBits(i, 0, 10);
+				break;
+			}
+			default: { //data record
+				unsigned tot2 = selectBits(i, 0, 4);
+				unsigned tot1 = selectBits(i, 4, 4);
+				unsigned row = selectBits(i, 8, 9) - 1;
+				unsigned column = selectBits(i, 17, 7) - 1;
+
+				if(column < 80 && ((tot2 == 0xF && row < 336) || (tot2 < 0xF && row < 335) )) {
+					//If tot2 != 0b1111 (0xF) then the tot2 is the tot code for pixel (col, row+1)
+                    auto lv1 = static_cast<unsigned>(no_data_headers)-1;
+                    if(lv1 == -1) {
+                        std::cout << "Error decoding LV1, would be -1 for pixel (x,y,tot1,tot2): " << column << ", " << row << ", " << tot1 << ", " << tot2 << std::endl;
+                    } lv1 = 0;
+                    if( tot2 != 0xF) result.emplace_back(column, row+1, tot2, lv1, 0);
+    				result.emplace_back(column, row, tot1, lv1, 0);
+				} else { // invalid data record
+						//invalid_dr ++;
+				}
+				break;
+			}
+        }
+    }
+
+	return result;
+}
+
+std::vector<APIXPix> decodeFEI4Data(std::vector<unsigned char> const & data) {
+	std::vector<APIXPix> result;
+	std::array<size_t,8> no_data_headers;
+
+	for(size_t index = 0;  index < data.size(); index+=4) {
+        uint32_t i =( static_cast<uint32_t>(data[index+3]) << 24 ) | 
+                	( static_cast<uint32_t>(data[index+2]) << 16 ) | 
+					( static_cast<uint32_t>(data[index+1]) << 8 ) | 
+					( static_cast<uint32_t>(data[index+0]) );
+
+		uint8_t channel = selectBits(i, 24, 8);
+
+		if(channel >> 7) { //Trigger
+			
+			no_data_headers.fill(0);
+			uint32_t trigger_number = selectBits(i, 0, 31); //testme
+		} else {
+			uint8_t type = selectBits(i, 16, 8);
+			switch(type) {
+				case data_header: {
+					//int bcid = selectBits(i, 0, 10);
+					//int lv1id = selectBits(i, 10, 5);
+					//int flag = selectBits(i, 15, 1);
+					no_data_headers.at(channel)++;
+					break;
+				}
+				case address_record: {
+					//int type = selectBits(i, 15, 1);
+					//int address = selectBits(i, 0, 15);
+					break;
+				}
+				case value_record: {
+					//int value = selectBits(i, 0, 16);
+					break;
+				}
+				case service_record: {
+					//int code = selectBits(i, 10, 6);
+					//int count = selectBits(i, 0, 10);
+					break;
+				}
+				default: { //data record
+
+					unsigned tot2 = selectBits(i, 0, 4);
+					unsigned tot1 = selectBits(i, 4, 4);
+					unsigned row = selectBits(i, 8, 9) - 1;
+					unsigned column = selectBits(i, 17, 7) - 1;
+
+					if(column < 80 && ((tot2 == 0xF && row < 336) || (tot2 < 0xF && row < 335) )) {
+						//If tot2 != 0b1111 (0xF) then the tot2 is the tot code for pixel (col, row+1)
+                        auto lv1 = static_cast<unsigned>(no_data_headers.at(channel)-1);
+                        if(lv1 > 15) lv1 = 15;
+						if( tot2 != 0xF) result.emplace_back(column, row+1, tot2, lv1, channel);
+						result.emplace_back(column, row, tot1, lv1, channel);
+					} else { // invalid data record
+						//invalid_dr ++;
+					}
+					break;
+				}
+			}
+		}
+	}
+	return result;
+}
+
+
+} // NAMESPACE eudaq
