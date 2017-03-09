a9d0ccc0f76176fe6c9f6f775c1b253594e3dfbc
Yi Liu <yi.liu@desy.de>
2017-03-09 14:27:00 +0100

    gui/runcontrol: clean up

--- gui/include/RunControlModel.hh
+++ gui/include/RunControlModel.hh
@@ -3,58 +3,24 @@
 #include <vector>
 #include <memory>
 
-class RunControlConnection {
-public:
-  RunControlConnection(eudaq::ConnectionSPC id);
-  QString operator[](int) const;
-  static int NumColumns();
-  static const char *ColumnName(int i);
-  int GetLevel() const { return m_status->GetLevel(); }
-  bool IsConnected() const { return !m_id.unique(); }
-  eudaq::ConnectionSPC GetId() const { return m_id; }
-  void SetStatus(eudaq::StatusSPC status) { m_status = status; }
-
-private:
-  eudaq::ConnectionSPC m_id;
-  eudaq::StatusSPC m_status;
-};
-
-class ConnectionSorter {
-public:
-  ConnectionSorter(std::vector<RunControlConnection> *messages)
-    : m_msgs(messages), m_col(0), m_asc(true){}
-  void SetSort(int col, bool ascending);
-  bool operator()(size_t lhs, size_t rhs);
-
-private:
-  std::vector<RunControlConnection> *m_msgs;
-  int m_col;
-  bool m_asc;
-};
-
 class RunControlModel : public QAbstractListModel {
   Q_OBJECT
-
 public:
   RunControlModel(QObject *parent = 0);
   int rowCount(const QModelIndex &parent = QModelIndex()) const override;
   int columnCount(const QModelIndex &parent = QModelIndex()) const override;
   QVariant data(const QModelIndex &index, int role) const override;
   QVariant headerData(int section, Qt::Orientation orientation,
-                      int role = Qt::DisplayRole) const override;
+                      int role) const override;
   void sort(int column, Qt::SortOrder order) override;
   
   void newconnection(eudaq::ConnectionSPC id);
   void disconnected(eudaq::ConnectionSPC id);
-  int GetLevel(const QModelIndex &index) const;
   void UpdateDisplayed();
   bool CheckConfigured();
   void SetStatus(eudaq::ConnectionSPC id, eudaq::StatusSPC status);
-
 private:
-  std::vector<RunControlConnection> m_data;
-  std::vector<size_t> m_disp;
-  ConnectionSorter m_sorter;
+  const std::vector<QString> m_str_header={"type", "name", "state", "connection", "info"};
   std::map<eudaq::ConnectionSPC, eudaq::StatusSPC> m_con_status;
-  
+  std::mutex m_mtx_con;
 };
--- gui/src/RunControlModel.cc
+++ gui/src/RunControlModel.cc
@@ -4,160 +4,94 @@
 #include <string>
 #include "qmetatype.h"
 
-namespace {
-  static const char *const g_columns[] =
-    {"type", "name", "state", "connection", "info"};
-}
-
-RunControlConnection::RunControlConnection(eudaq::ConnectionSPC id)
-  : m_id(id) {}
-
-QString RunControlConnection::operator[](int i) const {
-  switch (i) {
-  case 0:
-    return m_id->GetType().c_str();
-  case 1:
-    return m_id->GetName().c_str();
-  case 2:{
-    if(m_status){
-      // std::cout<<">>>1"<<std::endl;
-      // m_status->Print(std::cout);      
-      return m_id.unique()?"DEAD":to_string(*m_status).c_str();
-    }
-    // std::cout<<">>>0"<<std::endl;
-    // return m_id.unique()?"DEAD":to_string(*m_status).c_str();
-    // else
-    return "NONE_STATUS";
-  }
-  case 3:
-    return m_id->GetRemote().c_str();
-  default:
-    return "";
-  }
-}
-
-int RunControlConnection::NumColumns(){
-  return sizeof g_columns / sizeof *g_columns;
-}
-
-const char *RunControlConnection::ColumnName(int i) {
-  if (i < 0 || i >= NumColumns()) {
-    return "";
-  }
-  return g_columns[i];
-}
-
-void ConnectionSorter::SetSort(int col, bool ascending) {
-  m_col = col;
-  m_asc = ascending;
-}
-
-bool ConnectionSorter::operator()(size_t lhs, size_t rhs) {
-  QString l = (*m_msgs)[lhs][m_col];
-  QString r = (*m_msgs)[rhs][m_col];
-  return m_asc ^ (QString::compare(l, r, Qt::CaseInsensitive) < 0);
-}
-
 RunControlModel::RunControlModel(QObject *parent)
-  : QAbstractListModel(parent), m_sorter(&m_data){
+  : QAbstractListModel(parent){
   qRegisterMetaType<QVector<int> >("QVector<int>");
-
 }
 
-void RunControlModel::newconnection(eudaq::ConnectionSPC id) {
-  for (size_t i = 0; i < m_data.size(); ++i) {
-    if (!m_data[i].IsConnected() && id == m_data[i].GetId()) {
-      m_data[i] = RunControlConnection(id);
-      UpdateDisplayed();
-      return;
-    }
-  }
-
-  m_data.push_back(RunControlConnection(id));
-  std::vector<size_t>::iterator it =
-    std::lower_bound(m_disp.begin(), m_disp.end(), m_data.size() - 1, m_sorter);
-  size_t pos = it - m_disp.begin();
-  beginInsertRows(QModelIndex(), pos, pos);
-  m_disp.insert(it, m_data.size() - 1);
+void RunControlModel::newconnection(eudaq::ConnectionSPC id){
+  m_con_status[id].reset();  
+  beginInsertRows(QModelIndex(), 0, 0);
   endInsertRows();
+  UpdateDisplayed();
 }
 
-void RunControlModel::disconnected(eudaq::ConnectionSPC id) {
-  for (size_t i = 0; i < m_data.size(); ++i) {
-    if (id == m_data[i].GetId()){
-      // m_data[i].SetConnected(false);
-      //TODO: yiliu urgent
-      break;
-    }
-  }
+void RunControlModel::disconnected(eudaq::ConnectionSPC id){
+  m_con_status.erase(id);
   UpdateDisplayed();
 }
 
 void RunControlModel::SetStatus(eudaq::ConnectionSPC id,
-                                eudaq::StatusSPC status) {
-  for (size_t i = 0; i < m_data.size(); ++i) {
-    if (id == m_data[i].GetId()){
-      m_data[i].SetStatus(status);
-      break;
-    }
-  }
+                                eudaq::StatusSPC status){
+  m_con_status.at(id) = status;  
   UpdateDisplayed();
 }
 
 void RunControlModel::UpdateDisplayed() {
-  if (m_disp.size() > 0) {
-    std::sort(m_disp.begin(), m_disp.end(), m_sorter);
-    emit dataChanged(createIndex(0, 0), createIndex(m_data.size() - 1, 0));
-  }
+  // if (m_disp.size() > 0) {
+    // std::sort(m_disp.begin(), m_disp.end(), m_sorter);
+    emit dataChanged(createIndex(0, 0), createIndex(m_con_status.size() - 1, 0));
+  // }
 }
 
 bool RunControlModel::CheckConfigured() {
-  std::string connectionState;
-  for(RunControlConnection i: m_data){
-    connectionState = i[2].toStdString();
-    if(connectionState.size()<5)
-      return false;
-  }
   return true;
 }
 
-int RunControlModel::GetLevel(const QModelIndex &index) const {
-  const RunControlConnection &conn = m_data[m_disp[index.row()]];
-  if (conn.IsConnected())
-    return conn.GetLevel();
-  return eudaq::Status::LVL_DEBUG;
-}
-
 int RunControlModel::rowCount(const QModelIndex & /*parent*/) const {
-  return m_data.size();
+  return m_con_status.size();
 }
 
 int RunControlModel::columnCount(const QModelIndex & /*parent*/) const {
-  return RunControlConnection::NumColumns();
+  return m_str_header.size();
 }
 
 QVariant RunControlModel::data(const QModelIndex &index, int role) const {
   if (role != Qt::DisplayRole || !index.isValid())
     return QVariant();
 
-  if (index.column() < columnCount() && index.row() < rowCount())
-    return m_data[m_disp[index.row()]][index.column()];
-
-  return QVariant();
+  auto col_n = index.column();
+  auto row_n = index.row();
+  
+  if(row_n<m_con_status.size() && col_n<m_str_header.size()){
+    auto it = m_con_status.begin();
+    for(size_t i = 0; i< row_n; i++){
+      it++;
+    }
+    auto &con = it->first;
+    auto &sta = it->second;
+    switch(col_n){
+    case 0:
+      return con->GetType().c_str();
+    case 1:
+      return con->GetName().c_str();
+    case 2:{
+      if(sta)
+	return con.unique()?"DEAD":to_string(*sta).c_str();
+      return "NONE_STATUS";
+    }
+    case 3:
+      return con->GetRemote().c_str();
+    default:
+      return "";
+    }
+  }
+  else
+    return QVariant();
 }
 
 QVariant RunControlModel::headerData(int section, Qt::Orientation orientation,
-                                     int role) const {
+                                     int role) const{
   if (role != Qt::DisplayRole)
     return QVariant();
 
-  if (orientation == Qt::Horizontal && section < columnCount())
-    return RunControlConnection::ColumnName(section);
-
+  if (orientation == Qt::Horizontal && section < m_str_header.size()){
+    return m_str_header[section];
+  }
   return QVariant();
 }
 
 void RunControlModel::sort(int column, Qt::SortOrder order) {
-  m_sorter.SetSort(column, order == Qt::AscendingOrder);
-  UpdateDisplayed();
+  // m_sorter.SetSort(column, order == Qt::AscendingOrder);
+  // UpdateDisplayed();
 }
