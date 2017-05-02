1fd4405a455157b36410830ab99041dcedbde1f3
Yi Liu <yi.liu@desy.de>
2017-05-02 02:05:15 +0200

    main/exe: euCliConverter

---
+++ main/exe/src/euCliConverter.cxx
@@ -0,0 +1,43 @@
+#include "eudaq/OptionParser.hh"
+#include "eudaq/DataConverter.hh"
+#include "eudaq/FileWriter.hh"
+#include "eudaq/FileReader.hh"
+#include <iostream>
+
+int main(int /*argc*/, const char **argv) {
+  eudaq::OptionParser op("EUDAQ Command Line DataConverter", "2.0", "The dataonverter lauhcher of EUDAQ");
+  eudaq::Option<std::string> file_input(op, "i", "input", "", "string",
+					"input file");
+  eudaq::Option<std::string> file_output(op, "o", "output", "", "string",
+					 "output file");
+  eudaq::OptionFlag iprint(op, "ip", "iprint", "enable print of input Event");
+  
+  op.Parse(argv);
+
+  std::string infile_path = file_input.Value();
+  std::string outfile_path = file_output.Value();
+  std::string type_in = infile_path.substr(infile_path.find_last_of(".")+1);
+  std::string type_out = outfile_path.substr(outfile_path.find_last_of(".")+1);
+  bool print_ev_in = iprint.Value();
+  
+  if(type_in=="raw")
+    type_in = "native";
+  if(type_out=="raw")
+    type_out = "native";
+  
+  eudaq::FileReaderUP reader;
+  eudaq::FileWriterUP writer;
+  reader = eudaq::Factory<eudaq::FileReader>::MakeUnique(eudaq::str2hash(type_in), infile_path);
+  if(!type_out.empty())
+    writer = eudaq::Factory<eudaq::FileWriter>::MakeUnique(eudaq::str2hash(type_out), outfile_path);
+  while(1){
+    auto ev = reader->GetNextEvent();
+    if(!ev)
+      break;
+    if(print_ev_in)
+      ev->Print(std::cout);
+    if(writer)
+      writer->WriteEvent(ev);
+  }
+  return 0;
+}
