71270833980957b0a62e64cbeeebd010a28a9ab9
MengqingWu <wumengtsing@gmail.com>
2017-05-19 14:11:55 +0200

    form a separate package to develop code for aida telescope

---
+++ user/aidastrip/misc/example_startrun.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+./euRun -a tcp://44000
+#./euCliMonitor -n Ex0Monitor -t mon
+#./euCliCollector -n Ex0TgDataCollector -t tg
+#./euCliProducer -n Ex0Producer -t p0
+./euCliProducer -n myEx0Producer -t p1
