71ccc016ea721512e31b2ecff320977bf1aff7d8
Andre Rummler <andre.rummler@tu-dortmund.de>
2017-06-24 17:50:35 +0200

    CI: More CACTUS stuff

--- .travis/install_cactus.sh
+++ .travis/install_cactus.sh
@@ -13,7 +13,7 @@ cd $TRAVIS_BUILD_DIR/extern/
 
 if [ $TRAVIS_OS_NAME == linux ]; then 
 	
-	sudo apt-get install -y bzip2-devel zlib-devel ncurses-devel openssl openssl-devel python-devel curl curl-devel graphviz graphviz-devel libuuid-devel qt qt-devel PyQt PyQt-devel wxPython gd gd-devel cmake;
+	#sudo apt-get install -y bzip2-devel zlib-devel ncurses-devel openssl openssl-devel python-devel curl curl-devel graphviz graphviz-devel libuuid-devel qt qt-devel PyQt PyQt-devel wxPython gd gd-devel cmake;
 	
 	if [ -d "$TRAVIS_BUILD_DIR/extern/uhal_2_4_2" ]; then
 	
@@ -21,20 +21,16 @@ if [ $TRAVIS_OS_NAME == linux ]; then
 		
 		ls $TRAVIS_BUILD_DIR/extern/uhal_2_4_2
 		
-		zip -r aliceitsalpidesoftwaremaster.zip aliceitsalpidesoftwaremaster
-		
 	else
 
-		echo "palpidefs source not restored from cache - downloading from CERNBOX and unpacking"
+		echo "palpidefs source not restored from cache - checking out from CERN svn"
 		
 		svn co https://svn.cern.ch/reps/cactus/tags/ipbus_sw/uhal_2_4_2
-		
-		cp uhal_2_4_2
 	
 	fi
 
-	echo `ls `
-	
+	cd uhal_2_4_2
+		
 	make Set=uhal
 
 fi
@@ -42,7 +38,8 @@ fi
 
 if [ $TRAVIS_OS_NAME == osx ]; then 
 	
-	sudo apt-get install -y bzip2-devel zlib-devel ncurses-devel openssl openssl-devel python-devel curl curl-devel graphviz graphviz-devel libuuid-devel qt qt-devel PyQt PyQt-devel wxPython gd gd-devel cmake;
+	
+	#sudo apt-get install -y bzip2-devel zlib-devel ncurses-devel openssl openssl-devel python-devel curl curl-devel graphviz graphviz-devel libuuid-devel qt qt-devel PyQt PyQt-devel wxPython gd gd-devel cmake;
 	
 	if [ -d "$TRAVIS_BUILD_DIR/extern/uhal_2_4_2" ]; then
 	
@@ -50,25 +47,19 @@ if [ $TRAVIS_OS_NAME == osx ]; then
 		
 		ls $TRAVIS_BUILD_DIR/extern/uhal_2_4_2
 		
-		zip -r aliceitsalpidesoftwaremaster.zip aliceitsalpidesoftwaremaster
-		
 	else
 
-		echo "palpidefs source not restored from cache - downloading from CERNBOX and unpacking"
+		echo "palpidefs source not restored from cache - checking out from CERN svn"
 		
 		svn co https://svn.cern.ch/reps/cactus/tags/ipbus_sw/uhal_2_4_2
-		
-		cp uhal_2_4_2
 	
 	fi
 
-	echo `ls `
-	
+	cd uhal_2_4_2
+		
 	make Set=uhal
 
 fi
-
-pwd
 
 cd $temporary_path
 
