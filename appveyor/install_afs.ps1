b19d24e23a27a7529ce7c39c93cfe0c7c7ec9c54
Andre Rummler <andre.rummler@tu-dortmund.de>
2017-09-15 20:45:46 +0200

    CI: Adding output to afs installation on appveyor

--- appveyor/install_afs.ps1
+++ appveyor/install_afs.ps1
@@ -1,4 +1,5 @@
 function main () {
+    Write-Host("AFS installation started");
 
     $clnt = new-object System.Net.WebClient; 
 
@@ -18,7 +19,7 @@ function main () {
 	Write-Host("It seems that not all files for afs installation were downloaded properly - trying alternative.");	$clnt.DownloadFile("https://www.auristor.com/downloads/openafs/windows/x64/yfs-openafs-en_US-AMD64-1_7_3301.msi","C:\\yfs-openafs-en_US-AMD64-1_7_3301.msi");
 	Start-Process "C:\\yfs-openafs-en_US-AMD64-1_7_3301.msi" -ArgumentList "/quiet /qn /norestart /log install.log ADDLOCAL=feaNetIDMgrPlugin,feaClient,feaOpenAFS AcceptLicense=1" -Wait;
     }
-    
+    Write-Host("AFS installation finished");    
 }
 
 main
