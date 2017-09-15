b682406d77d96e4881bdbc7f5374121951f13bb1
Andre Rummler <andre.rummler@tu-dortmund.de>
2017-09-15 20:38:06 +0200

    CI: Update afs on appveyor

--- appveyor/install_afs.ps1
+++ appveyor/install_afs.ps1
@@ -3,15 +3,15 @@ function main () {
     $clnt = new-object System.Net.WebClient; 
 
     try{
-       $clnt.DownloadFile("https://www.secure-endpoints.com/binaries/heimdal/Heimdal-AMD64-full-1-6-2-0.msi","C:\\Heimdal-AMD64-full-1-6-2-0.msi");
+       $clnt.DownloadFile("https://www.secure-endpoints.com/binaries/heimdal/Heimdal-AMD64-full-7-4-0-40.msi","C:\\Heimdal-AMD64-full-7-4-0-40.msi");
     }
     catch {}
     
     $clnt.DownloadFile("http://dl.openafs.org/dl/openafs/1.7.31/winxp/openafs-en_US-64bit-1-7-3100.msi","C:\\openafs-en_US-64bit-1-7-3100.msi");
 	$clnt.DownloadFile("http://dl.openafs.org/dl/openafs/1.7.31/winxp/openafs-32bit-tools-en_US-1-7-3100.msi","C:\\openafs-32bit-tools-en_US-1-7-3100.msi");
    
-    If( Test-Path("C:\\Heimdal-AMD64-full-1-6-2-0.msi") ) {
-	Start-Process "C:\\Heimdal-AMD64-full-1-6-2-0.msi" -ArgumentList "/quiet /qn /norestart /log install.log ADDLOCAL=ALL" -Wait;
+    If( Test-Path("C:\\Heimdal-AMD64-full-7-4-0-40.msi") ) {
+	Start-Process "C:\\Heimdal-AMD64-full-7-4-0-40.msi" -ArgumentList "/quiet /qn /norestart /log install.log ADDLOCAL=ALL" -Wait;
 	Start-Process "C:\\openafs-en_US-64bit-1-7-3100.msi" -ArgumentList "/quiet /qn /norestart /log install.log ADDLOCAL=ALL" -Wait;
 	Start-Process "C:\\openafs-32bit-tools-en_US-1-7-3100.msi" -ArgumentList "/quiet /qn /norestart /log install.log ADDLOCAL=ALL" -Wait;
     }  Else {
