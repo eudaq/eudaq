f1cb267aaadad010d51ba943026c5ff33b015d4e
yiliu <yi.liu@desy.de>
2017-10-16 23:15:12 +0200

    lib/core: AsyncSending and std::feature in DataSender

--- main/lib/core/include/eudaq/DataSender.hh
+++ main/lib/core/include/eudaq/DataSender.hh
@@ -5,20 +5,32 @@
 #include "eudaq/Platform.hh"
 #include "eudaq/Event.hh"
 #include <string>
+#include <future>
+#include <thread>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
 
 namespace eudaq {
 
 class TransportClient;
 
   class DLLEXPORT DataSender {
-    public:
+  public:
       DataSender(const std::string & type, const std::string & name);
+      ~DataSender();
       void Connect(const std::string & server);
       void SendEvent(EventSPC ev);
-    private:
+  private:
+      bool AsyncSending();
       std::string m_type, m_name;
       std::unique_ptr<TransportClient> m_dataclient;
       uint64_t m_packetCounter;
+      std::future<bool> m_fut_async;
+      bool m_is_connected;
+      std::mutex m_mx_qu_ev; 
+      std::queue<EventSPC> m_qu_ev;
+      std::condition_variable m_cv_not_empty;
   };
 
 }
