26ff417b7c3c1826eb416133202f9ffdf6f8ce3d
yiliu <yi.liu@desy.de>
2017-10-16 23:15:12 +0200

    lib/core: AsyncSending and std::feature in DataSender

--- main/lib/core/src/DataSender.cc
+++ main/lib/core/src/DataSender.cc
@@ -12,7 +12,28 @@ namespace eudaq {
     m_name(name),
     m_packetCounter(0) {}
 
+
+  DataSender::~DataSender(){
+    if(m_fut_async.valid()){
+      m_fut_async.get();
+    }
+  }
+  
   void DataSender::Connect(const std::string & server) {
+    m_is_connected = false;
+    try{
+      if(m_fut_async.valid()){
+	m_fut_async.get();
+	//previous connection is closed.
+      }
+    }
+    catch(...){
+      EUDAQ_WARN("connection execption from disconnetion");
+    }
+    
+    std::unique_lock<std::mutex> lk(m_mx_qu_ev);    
+    m_qu_ev = std::queue<EventSPC>();    
+    lk.unlock();
     m_dataclient.reset(TransportClient::CreateClient(server));
     std::string packet;
     if (!m_dataclient->ReceivePacket(&packet, 1000000)) EUDAQ_THROW("No response from DataCollector server");
@@ -41,17 +62,38 @@ namespace eudaq {
     if (!m_dataclient->ReceivePacket(&packet, 1000000)) EUDAQ_THROW("No response from DataCollector server");
     i1 = packet.find(' ');
     if (std::string(packet, 0, i1) != "OK") EUDAQ_THROW("Connection refused by DataCollector server: " + packet);
+
+    m_is_connected = true;
+    m_fut_async = std::async(std::launch::async, &DataSender::AsyncSending, this);
   }
 
-  void DataSender::SendEvent(EventSPC ev) {
-    if (!m_dataclient) EUDAQ_THROW("Transport not connected error");
-    //EUDAQ_DEBUG("Serializing event");
-    BufferSerializer ser;
-    ev->Serialize(ser);
-    //EUDAQ_DEBUG("Sending event");
-    m_packetCounter += 1;
-    m_dataclient->SendPacket(ser);
-    //EUDAQ_DEBUG("Sent event");
+  void DataSender::SendEvent(EventSPC ev){
+    if (!m_dataclient)
+      EUDAQ_THROW("Transport not connected error");
+    std::unique_lock<std::mutex> lk(m_mx_qu_ev);
+    m_qu_ev.push(ev);
+    if(m_qu_ev.size() > 10000){
+      m_qu_ev.pop();
+      EUDAQ_WARN("Buffer of sending event is full.");
+    }
+    lk.unlock();
+    m_cv_not_empty.notify_all();
   }
 
+  bool DataSender::AsyncSending(){
+    while(m_is_connected){//TODO: 
+      std::unique_lock<std::mutex> lk(m_mx_qu_ev);
+      if(m_qu_ev.empty()){
+	m_cv_not_empty.wait(lk);
+      }
+      auto ev = m_qu_ev.front();
+      m_qu_ev.pop();
+      lk.unlock();
+      BufferSerializer ser;
+      ev->Serialize(ser);
+      m_packetCounter += 1;
+      m_dataclient->SendPacket(ser);
+    }
+  }
+  
 }
