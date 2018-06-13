7433dbb410f7c0f7fee526dba2a753e993e4c920
Jan Dreyling-Eschweiler <jan.dreyling-eschweiler@desy.de>
2018-06-13 23:34:53 +0200

    ENH: edit example_startup.sh for out-of-box installation and execution

--- user/example/misc/example_startrun.sh
+++ user/example/misc/example_startrun.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
-./euRun -n Ex0RunControl -a tcp://44000 &
+BINPATH=../../../bin
+$BINPATH/euRun -n Ex0RunControl -a tcp://44000 &
 sleep 1
-./euLog -a tcp://44001&
+$BINPATH/euLog -a tcp://44001&
 sleep 1
-./euCliMonitor -n Ex0Monitor -t my_mon  -a tcp://45001 &
-./euCliCollector -n Ex0TgDataCollector -t my_dc -a tcp://45002 &
-./euCliProducer -n Ex0Producer -t my_pd0 &
-./euCliProducer -n Ex0Producer -t my_pd1 &
+$BINPATH/euCliMonitor -n Ex0Monitor -t my_mon  -a tcp://45001 &
+$BINPATH/euCliCollector -n Ex0TgDataCollector -t my_dc -a tcp://45002 &
+$BINPATH/euCliProducer -n Ex0Producer -t my_pd0 &
+$BINPATH/euCliProducer -n Ex0Producer -t my_pd1 &
