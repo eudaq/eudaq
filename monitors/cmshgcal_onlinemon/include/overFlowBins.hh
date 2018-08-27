3d72ce7e676a149ef9fb4cdad4892b89b8af5b74
asteencern <arnaud.steen@cern.ch>
2018-08-27 17:51:23 +0200

    remove cmshgcal monitoring stuff (it will be added later)

--- monitors/cmshgcal_onlinemon/include/overFlowBins.hh
+++
@@ -1,8 +0,0 @@
-#ifndef Overflow_HH_
-#define Overflow_HH_
-
-#include <TH1.h>
-
-void handleOverflowBins(TH1* hist, bool doOver=true, bool doUnder=false, bool doErr=false);
-
-#endif /* Overflow_HH_ */
--- monitors/cmshgcal_onlinemon/src/OnlineHistograms.cc
+++
@@ -1,5 +0,0 @@
-#include "OnlineHistograms.hh"
-#include <TDirectory.h>
-#include "OnlineMon.hh"
-
-using namespace std;
