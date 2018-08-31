ba7b67a1ff32c1c014c697fab5c60c155566767d
asteencern <arnaud.steen@cern.ch>
2018-08-31 18:14:47 +0200

    Update HGCalRawEvent2StdEventConverter.cc (first version working fine)

--- user/cmshgcal/module/src/HGCalRawEvent2StdEventConverter.cc
+++ user/cmshgcal/module/src/HGCalRawEvent2StdEventConverter.cc
@@ -14,6 +14,8 @@
 #define HighEnergyThr 500
 #define STDPLANE_SIZE 30 //13 low gain, 13 high gain, toa fall and rise, tot fast and slow
 
+#define RUNMODE 0 // 0 for pedestal data, 1 for TOA selection, 2 for Time sample 3 selection
+
 struct HGCalHit
 {
   HGCalHit(uint16_t m_board, uint16_t m_chip, uint16_t m_channel) : board(m_board), chip(m_chip), channel(m_channel){;}
@@ -70,69 +72,76 @@ bool HGCalRawEvent2StdEventConverter::Converting(eudaq::EventSPC d1, eudaq::StdE
   auto ev = std::dynamic_pointer_cast<const eudaq::RawEvent>(d1);
   size_t nblocks = ev->NumBlocks();
   int event_nr=ev->GetEventNumber();
+
   std::ostringstream os(std::ostringstream::ate);
-  
+
   unpacker _unpacker;
   uint32_t firstWord[nblocks][5];
   
+  int ormId;
   for( int iblo=0; iblo<nblocks; iblo++ ){
     if( iblo%2==0 ){
       std::vector<unsigned char> block = ev->GetBlock(iblo);
       std::vector<uint32_t> rawData32;
-      std::cout << "block.size() = " << std::dec << block.size() << std::endl;
+      //std::cout << "block.size() = " << std::dec << block.size() << std::endl;
       rawData32.resize(block.size() / sizeof(uint32_t));
       std::memcpy(&rawData32[0], &block[0], block.size());
-      std::cout << "rawData32.size() = " << std::dec << rawData32.size() << std::endl;
-      std::bitset<32> bits(rawData32[1]);
-      unsigned int nlayer=bits.count()/4;
-      std::cout << "orm id = " << std::dec << rawData32[0] << "\t nlayer = " << nlayer << std::endl;
+      //std::cout << "rawData32.size() = " << std::dec << rawData32.size() << std::endl;
+      ormId=rawData32[0];
+      // std::bitset<32> bits(rawData32[1]);
+      // unsigned int nlayer=bits.count()/4;
+      // std::cout << "orm id = " << std::dec << rawData32[0] << "\t nlayer = " << nlayer << std::endl;
     }
-  }
-  for( int iblo=0; iblo<nblocks; iblo++ ){
-    if( iblo%2==0 ) continue;
-    std::vector<unsigned char> block = ev->GetBlock(iblo);
-    std::vector<uint32_t> rawData32;
-    std::string decompressed=compressor::decompress(block);
-    std::vector<uint8_t> decompData( decompressed.begin(), decompressed.end() );
-    rawData32.resize(decompData.size() / sizeof(uint32_t));
-    std::memcpy(&rawData32[0], &decompData[0], decompData.size());
+    else{
+      std::vector<unsigned char> block = ev->GetBlock(iblo);
+      std::vector<uint32_t> rawData32;
+      std::string decompressed=compressor::decompress(block);
+      std::vector<uint8_t> decompData( decompressed.begin(), decompressed.end() );
+      rawData32.resize(decompData.size() / sizeof(uint32_t));
+      std::memcpy(&rawData32[0], &decompData[0], decompData.size());
 
-    firstWord[iblo][0]=rawData32[0];
-    firstWord[iblo][1]=rawData32[1];
-    firstWord[iblo][2]=rawData32[2];
-    firstWord[iblo][3]=rawData32[3];
-    firstWord[iblo][4]=rawData32[4];
-    _unpacker.unpack_and_fill(rawData32);
+      firstWord[iblo][0]=rawData32[0];
+      firstWord[iblo][1]=rawData32[1];
+      firstWord[iblo][2]=rawData32[2];
+      firstWord[iblo][3]=rawData32[3];
+      firstWord[iblo][4]=rawData32[4];
+      _unpacker.unpack_and_fill(rawData32);
     
-    std::cout << "data size = " << std::dec << _unpacker.getChipData().size()*1924*2 << std::endl; //N CHIP * 1924 *2 BYTES (16 bits)
-    for( std::vector<Skiroc2CMSData>::iterator it=_unpacker.getChipData().begin(); it!=_unpacker.getChipData().end(); ++it ){
-      int key=(*it).chipID();
-      int chipIndex=key%100;
-      int ormId=key/100;
-      int layer=m_orm_module_sum.at(ormId)+chipIndex/4;
-      int chip=3-chipIndex%4;
-      std::vector<HGCalHit> avec=GetZeroSuppressHits( (*it),layer,chip );
-      os.str("");
-      os<<"Hexaboard-RBD"<<ormId;
-      std::unique_ptr<eudaq::StandardPlane> plane(new eudaq::StandardPlane(layer, "HexaBoardRawDataEvent", os.str()));
-      plane->SetSizeZS(STDPLANE_X_SIZE,STDPLANE_Y_SIZE,avec.size(),STDPLANE_SIZE);
-      int nhit=0;
-      for(std::vector<HGCalHit>::iterator jt=avec.begin(); jt!=avec.end(); ++jt){
-       	std::cout << (*jt) << std::endl;
-	for(int ts=0; ts<HGC_NUMBER_OF_TS; ts++){
-	  plane->SetPixel(nhit,chip,(*jt).channel,(*jt).lowgain[ts],false,ts);
-	  plane->SetPixel(nhit,chip,(*jt).channel,(*jt).highgain[ts],false,ts+HGC_NUMBER_OF_TS);
-	}
-	plane->SetPixel(nhit,chip,(*jt).channel,(*jt).toa_fall,false,HGC_NUMBER_OF_TS*2+0);
-	plane->SetPixel(nhit,chip,(*jt).channel,(*jt).toa_rise,false,HGC_NUMBER_OF_TS*2+1);
-	plane->SetPixel(nhit,chip,(*jt).channel,(*jt).tot_fast,false,HGC_NUMBER_OF_TS*2+2);
-	plane->SetPixel(nhit,chip,(*jt).channel,(*jt).tot_slow,false,HGC_NUMBER_OF_TS*2+3);
-	nhit++;
+      std::map< int,std::vector<HGCalHit> > hitMap;
+      for( std::vector<Skiroc2CMSData>::iterator it=_unpacker.getChipData().begin(); it!=_unpacker.getChipData().end(); ++it ){
+	int key=(*it).chipID();
+	int chipIndex=key%100;
+	int layer=m_orm_module_sum.at(ormId)+chipIndex/4;
+	int chip=3-chipIndex%4;
+	std::vector<HGCalHit> hits=GetZeroSuppressHits( (*it),layer,chip );
+	if( hitMap.find(layer)==hitMap.end() )
+	  hitMap.insert( std::pair<int,std::vector<HGCalHit> >(layer,hits) );
+	else
+	  hitMap[layer].insert( hitMap[layer].end(), hits.begin(), hits.end() );
       }
-      d2->AddPlane(*plane);
+      for( std::map< int,std::vector<HGCalHit> >::iterator it=hitMap.begin(); it!=hitMap.end(); ++it ){
+	os.str("");
+	os<<"HexaBoard-RDB"<<ormId;
+	std::unique_ptr<eudaq::StandardPlane> plane(new eudaq::StandardPlane(it->first, "HexaBoardRawDataEvent", os.str()));
+	plane->SetSizeZS(STDPLANE_X_SIZE,STDPLANE_Y_SIZE,it->second.size(),STDPLANE_SIZE);
+	int nhit=0;
+	for(std::vector<HGCalHit>::iterator jt=it->second.begin(); jt!=it->second.end(); ++jt){
+	  //std::cout << (*jt) << std::endl;
+	  for(int ts=0; ts<HGC_NUMBER_OF_TS; ts++){
+	    plane->SetPixel(nhit,(*jt).chip,(*jt).channel,(*jt).lowgain[ts],false,ts);
+	    plane->SetPixel(nhit,(*jt).chip,(*jt).channel,(*jt).highgain[ts],false,ts+HGC_NUMBER_OF_TS);
+	  }
+	  plane->SetPixel(nhit,(*jt).chip,(*jt).channel,(*jt).toa_fall,false,HGC_NUMBER_OF_TS*2+0);
+	  plane->SetPixel(nhit,(*jt).chip,(*jt).channel,(*jt).toa_rise,false,HGC_NUMBER_OF_TS*2+1);
+	  plane->SetPixel(nhit,(*jt).chip,(*jt).channel,(*jt).tot_fast,false,HGC_NUMBER_OF_TS*2+2);
+	  plane->SetPixel(nhit,(*jt).chip,(*jt).channel,(*jt).tot_slow,false,HGC_NUMBER_OF_TS*2+3);
+	  nhit++;
+	}
+	d2->AddPlane(*plane);
+      }
     }
   }
-  if (event_nr%100==0) {
+  if (event_nr%1000==0) {
     std::cout<<"Processing event "<<std::dec<<event_nr<<" with "<<nblocks<<" blocks" << std::endl;
     for( int iblo=0; iblo<nblocks; iblo++ )
       if( iblo%2!=0 )
@@ -150,20 +159,19 @@ std::vector<HGCalHit> HGCalRawEvent2StdEventConverter::GetZeroSuppressHits(const
     int hgTS3=skdata.ADCHigh(ichan,3);//expeced max
     int hgTS4=skdata.ADCHigh(ichan,4);//still close to max
     int hgTS6=skdata.ADCHigh(ichan,6);//undershoot
-    if( hgTS3-hgTS0>HighEnergyThr ||
-    	(hgTS3-hgTS0>HalfMipCut && hgTS4-hgTS6>0 && hgTS0-hgTS6>0) ){
-      //if(skdata.TOAFall(ichan)>4){
-      HGCalHit hit(layer, chip, ichan);
-      for(int ts=0; ts<HGC_NUMBER_OF_TS; ts++){
-	hit.highgain[ts]=skdata.ADCHigh(ichan,ts);
-	hit.lowgain[ts]=skdata.ADCHigh(ichan,ts);
-      }
-      hit.tot_slow=skdata.TOTSlow(ichan);
-      hit.tot_fast=skdata.TOTFast(ichan);
-      hit.toa_fall=skdata.TOAFall(ichan);
-      hit.toa_rise=skdata.TOARise(ichan);
-      zshits.push_back(hit);
+    if( RUNMODE==0 ){;}
+    else if( RUNMODE==1 && skdata.TOAFall(ichan)==4 ) continue;
+    else if( RUNMODE==2 && hgTS3-hgTS0<HighEnergyThr && (hgTS3-hgTS0<HalfMipCut || hgTS4-hgTS6<0 || hgTS0-hgTS6<0) ) continue;
+    HGCalHit hit(layer, chip, ichan);
+    for(int ts=0; ts<HGC_NUMBER_OF_TS; ts++){
+      hit.highgain[ts]=skdata.ADCHigh(ichan,ts);
+      hit.lowgain[ts]=skdata.ADCHigh(ichan,ts);
     }
+    hit.tot_slow=skdata.TOTSlow(ichan);
+    hit.tot_fast=skdata.TOTFast(ichan);
+    hit.toa_fall=skdata.TOAFall(ichan);
+    hit.toa_rise=skdata.TOARise(ichan);
+    zshits.push_back(hit);
   }
   return zshits;
 }
