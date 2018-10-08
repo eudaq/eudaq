ee444d615037563c22414739009edb6744c3189e
HGCAL DAQ <svhgcal01@cern.ch>
2018-10-08 17:06:00 +0200

    [dqm] remove HGCalLayerSum collection, requires plane selection

--- monitors/cmshgcal_onlinemon/src/OnlineMon.cxx
+++ monitors/cmshgcal_onlinemon/src/OnlineMon.cxx
@@ -92,9 +92,9 @@ RootMonitor::RootMonitor(const std::string & runcontrol,
   _colls.push_back(hexaCollection);
   // _colls.push_back(hexaCorrelationCollection);
   _colls.push_back(wcCollection);
-  _colls.push_back(cmshgcalLayerSumCollection);
-  // _colls.push_back(tdchitsCollection);
-  // _colls.push_back(wccorrCollection);
+  //_colls.push_back(cmshgcalLayerSumCollection);
+  _colls.push_back(tdchitsCollection);
+  _colls.push_back(wccorrCollection);
   //_colls.push_back(beamTelescopeHitCollection);
   //_colls.push_back(beamTelescopeCorrCollection);
   //_colls.push_back(dwcToHGCALCorrelationCollection);
@@ -104,10 +104,10 @@ RootMonitor::RootMonitor(const std::string & runcontrol,
   if (_offline <= 0) {
     hexaCollection->setRootMonitor(this);
     // hexaCorrelationCollection->setRootMonitor(this);
-     wcCollection->setRootMonitor(this);
-     cmshgcalLayerSumCollection->setRootMonitor(this);
-    // tdchitsCollection->setRootMonitor(this);
-    // wccorrCollection->setRootMonitor(this);
+    wcCollection->setRootMonitor(this);
+    // cmshgcalLayerSumCollection->setRootMonitor(this);
+    tdchitsCollection->setRootMonitor(this);
+    wccorrCollection->setRootMonitor(this);
     // beamTelescopeHitCollection->setRootMonitor(this);
     // beamTelescopeCorrCollection->setRootMonitor(this);
     // //dwcToHGCALCorrelationCollection->setRootMonitor(this);
