5a0933d2a351b9ead20c0d556df10880b35d9ef7
Shilpi <shilpi.jain@cern.ch>
2018-10-14 12:16:53 +0200

    Committing what I forgot!

--- monitors/cmshgcal_onlinemon/include/CMSHGCalLayerSumHistos.hh
+++ monitors/cmshgcal_onlinemon/include/CMSHGCalLayerSumHistos.hh
@@ -8,6 +8,7 @@
 
 #include "eudaq/StandardEvent.hh"
 
+#include "SimpleStandardEvent.hh"
 #include "overFlowBins.hh"
 #include "NoisyChannel.hh"
 
@@ -16,8 +17,9 @@ class RootMonitor;
 class CMSHGCalLayerSumHistos {
 public:
   //CMSHGCalLayerSumHistos(RootMonitor *mon, NoisyChannelList noisyCells);
-  CMSHGCalLayerSumHistos(RootMonitor *mon);
+  CMSHGCalLayerSumHistos(RootMonitor *mon,const  eudaq::StandardEvent &ev);
   void Fill(const eudaq::StandardEvent &event, int evNumber=-1);
+  //void Fill(const SimpleStandardEvent &ev, int evNumber=-1);
   void Reset();
   void Write();
   
@@ -32,9 +34,26 @@ public:
   TH2F *getEnergyVSNhitHisto(){return h_energy_nhit;}
   TH2F *getEnergyVSCoGZHisto(){return h_energy_cogz;}
   TProfile *getLongitudinalProfile(){return h_energy_layer->ProfileX();}
+  ///return per-plane histo
+  TH1F *getEnergyMIPHisto_pp(unsigned int iplane){return h_energyMIP_pp[iplane];}
+  TH1I *getEnergyLGHisto_pp(unsigned int iplane){return h_energyLG_pp[iplane];}
+  TH1I *getEnergyHGHisto_pp(unsigned int iplane){return h_energyHG_pp[iplane];}
+  TH1I *getEnergyTOTHisto_pp(unsigned int iplane){return h_energyTOT_pp[iplane];}
+  TH1I *getNhitHisto_pp(unsigned int iplane){return h_nhit_pp[iplane];}
+  TH1I *getNhitEEHisto_pp(unsigned int iplane){return h_nhitEE_pp[iplane];}
+  TH1I *getNhitFH0Histo_pp(unsigned int iplane){return h_nhitFH0_pp[iplane];}
+  TH1I *getNhitFH1Histo_pp(unsigned int iplane){return h_nhitFH1_pp[iplane];}
+  TH2F *getEnergyVSNhitHisto_pp(unsigned int iplane){return h_energy_nhit_pp[iplane];}
+  unsigned int getNplanes() const;
+
+
   void setRootMonitor(RootMonitor *mon) { m_mon = mon; }
+
+
+
   
 private:
+  unsigned int nplanes;
   RootMonitor *m_mon;
   NoisyChannelList m_noisyCells;
   int m_mainFrameTS;
@@ -55,6 +74,20 @@ private:
   TH2F *h_energy_nhit;
   TH2F *h_energy_cogz;
   TH2F *h_energy_layer;
+
+  //per-plane histos
+  TH1F **h_energyMIP_pp;
+  TH1I **h_energyLG_pp;
+  TH1I **h_energyHG_pp;
+  TH1I **h_energyTOT_pp;
+  TH1I **h_nhit_pp;
+  TH1I **h_nhitEE_pp;
+  TH1I **h_nhitFH0_pp;
+  TH1I **h_nhitFH1_pp;
+  TH2F **h_energy_nhit_pp;
+
+
+
 };
 
 #ifdef __CINT__
