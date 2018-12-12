576c67731b3fd55a45692be2da45f4c32234babc
MengqingWu <wumengtsing@gmail.com>
2018-12-12 17:07:34 +0100

    py shared lib dir problem fixed

---
+++ user/example/python/setup_eudaq.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh                                                                                         
+
+export EUDAQ_DIR=/opt/eudaq2/
+export PYTHONPATH=${EUDAQ_DIR}/lib:${PYTHONPATH}
+
+export LD_LIBRARY_PATH=${EUDAQ_DIR}/lib:${LD_LIBRARY_PATH}
+
+#export ROGUE_DIR=${PWD}/rogue/
+#export PYTHONPATH=${EUDAQ_DIR}/python:${ROGUE_DIR}/python:${PYTHONPATH}
