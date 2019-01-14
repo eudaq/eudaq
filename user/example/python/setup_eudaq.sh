7f552c5f090fcef9d6a956a3de678725d2d41e19
MengqingWu <mengqing.wu@desy.de>
2019-01-14 13:59:52 +0100

    update for tutorial

--- user/example/python/setup_eudaq.sh
+++ user/example/python/setup_eudaq.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env sh                                                                                         
 
-export EUDAQ_DIR=/opt/eudaq2/
+#export EUDAQ_DIR=/opt/eudaq2/
+export EUDAQ_DIR=${PWD}/../../..
 export PYTHONPATH=${EUDAQ_DIR}/lib:${PYTHONPATH}
 
 export LD_LIBRARY_PATH=${EUDAQ_DIR}/lib:${LD_LIBRARY_PATH}
-
-#export ROGUE_DIR=${PWD}/rogue/
-#export PYTHONPATH=${EUDAQ_DIR}/python:${ROGUE_DIR}/python:${PYTHONPATH}
