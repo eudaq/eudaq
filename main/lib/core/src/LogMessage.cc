3ad2ae1ad9d2c7310a918a3de66f0cf0dc07603f
Lennart Huth <huth@phsi.uni-heidelberg.de>
2019-01-28 14:07:15 +0100

    attempt to fix it on windows

--- main/lib/core/src/LogMessage.cc
+++ main/lib/core/src/LogMessage.cc
@@ -76,6 +76,16 @@ namespace eudaq {
 
   void LogMessage::Print(std::ostream &os) const {
       // print the time bold
+#ifdef _WIN32
+      os <<"["<< m_time.Formatted()<<"]";
+      if (m_sendertype != "")
+          os << " [" << GetSender()<<" ]"<<std::flush;
+      else
+          os <<"\x1B[31;1m"<< " [unknown sender]"<< "\x1B[0m"<<std::flush;
+      // the actual message and a new line
+      os << GetMessage()<<std::flush;
+
+#else
       os << "\x1B[0m"<<"\x1B[1m" <<"["<< m_time.Formatted()<<"]"<< "\x1B[0m"<<std::flush;
       // we can add some colors for different levels:
       //  debug < black, info = green, warning = yellow, error >= red
@@ -95,6 +105,7 @@ namespace eudaq {
           os <<"\x1B[31;1m"<< " [unknown sender]"<< "\x1B[0m"<<std::flush;
       // the actual message and a new line
       os << GetMessage()<<std::flush;
+ #endif
   }
 
 
