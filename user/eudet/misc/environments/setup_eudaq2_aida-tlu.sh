c6f6a878519da4295cb8cd85d4f8a0c59ed9e251
Jan Dreyling-Eschweiler <jan.dreyling-eschweiler@desy.de>
2019-01-29 17:22:18 +0100

    user/eudet: updated start scripts with IP env-variable

--- user/eudet/misc/environments/setup_eudaq2_aida-tlu.sh
+++ user/eudet/misc/environments/setup_eudaq2_aida-tlu.sh
@@ -1,10 +1,17 @@
-# for Ubuntu 18.04
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/cactus/lib
+# IP variables for start scripts
+export RUNCONTROLIP=127.0.0.1
+export TLUIP=127.0.0.1
+export NIIP=127.0.0.1
 
+echo "Check pathes in telescope.ini and in the conf-files"
 
 # eudaq2 binaries
-export PATH=$PATH:/opt/eudaq2/bin
+export EUDAQ=/opt/eudaq2/
+export PATH=$PATH:$EUDAQ/bin
 
+## for AIDA TLU
+# for Ubuntu 18.04
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/cactus/lib
 # start controlhub for stable AIDA TLU TCP/IP communication
 /opt/cactus/bin/controlhub_start
 /opt/cactus/bin/controlhub_status
