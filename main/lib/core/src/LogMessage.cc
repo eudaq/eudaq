e303b4d185ca687b42bd1babedc875cc0b27e71b
lhuth <huth@physi.uni-heidelberg.de>
2019-01-31 18:10:24 +0100

    Lhuth devel (#519)
    * added some colors to logger cout and put the data/time first
    
    * reomved unsused code line
    
    * attempt to fix it on windows
    
    * attempt to fix it on windows
    
    * merge eudaq/master and ready to submit

--- main/lib/core/src/LogMessage.cc
+++ main/lib/core/src/LogMessage.cc
@@ -76,7 +76,18 @@ namespace eudaq {
 
   void LogMessage::Print(std::ostream &os) const {
       // print the time bold
-      os << "\x1B[0m"<<"\x1B[1m" <<"["<< m_time.Formatted()<<"]"<< "\x1B[0m"<<std::flush;
+
+#ifdef _WIN32
+      os <<"["<< m_time.Formatted()<<"] ";
+      if (m_sendertype != "")
+          os << " [" << GetSender()<<"] "<<std::flush;
+      else
+          os <<" [unknown sender] "<<std::flush;
+      // the actual message and a new line
+      os << GetMessage()<<std::flush;
+
+#else
+      os << "\x1B[0m"<<"\x1B[1m" <<"["<< m_time.Formatted()<<"] "<< "\x1B[0m"<<std::flush;
       // we can add some colors for different levels:
       //  debug < black, info = green, warning = yellow, error >= red
       if(GetLevel()<LVL_INFO)
@@ -95,6 +106,7 @@ namespace eudaq {
           os <<"\x1B[31;1m"<< " [unknown sender]"<< "\x1B[0m"<<std::flush;
       // the actual message and a new line
       os << GetMessage()<<std::flush;
+ #endif
   }
 
 
