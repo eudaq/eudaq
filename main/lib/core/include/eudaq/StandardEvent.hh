b88e5ca7fa9b46cd9cbf7b6cf7d974df13ff83e3
Simon Spannagel <simon.spannagel@cern.ch>
2019-02-21 18:21:06 +0100

    StandardEvent: extend by time stanmps in real-world nanoseconds

--- main/lib/core/include/eudaq/StandardEvent.hh
+++ main/lib/core/include/eudaq/StandardEvent.hh
@@ -10,12 +10,12 @@
 
 namespace eudaq {
   class StandardEvent;
-  
+
   using StdEventUP = Factory<StandardEvent>::UP;
   using StdEventSP = Factory<StandardEvent>::SP;
   using StdEventSPC = Factory<StandardEvent>::SPC;
   using StandardEventSP = StdEventSP;
-  using StandardEventSPC = StdEventSPC;  
+  using StandardEventSPC = StdEventSPC;
 
   class DLLEXPORT StandardEvent : public Event {
     public:
@@ -28,12 +28,37 @@ namespace eudaq {
     StandardPlane &GetPlane(size_t i);
     virtual void Serialize(Serializer &) const;
     virtual void Print(std::ostream & os,size_t offset = 0) const;
-    
-    static StdEventSP MakeShared();    
+
+    /**
+     * @brief Funtion to retrieve begin of this event in nanosecons
+     * @return Start time of this event
+     */
+    double GetTimeBegin() const;
+
+    /**
+     * @brief Funtion to retrieve end of this event in nanosecons
+     * @return End time of this event
+     */
+    double GetTimeEnd() const;
+
+    /**
+     * @brief Set begin time of event in nanoseconds
+     * @param begin Begin of events in nanoseconds
+     */
+    void SetTimeBegin(double begin) { time_begin = begin; };
+
+    /**
+     * @brief Set end time of event in nanoseconds
+     * @param end End of events in nanoseconds
+     */
+     void SetTimeEnd(double end) { time_end = end; };
+
+    static StdEventSP MakeShared();
     static const uint32_t m_id_factory = cstr2hash("StandardEvent");
 
   private:
     std::vector<StandardPlane> m_planes;
+    double time_begin, time_end;
   };
 
   inline std::ostream &operator<<(std::ostream &os, const StandardPlane &pl) {
