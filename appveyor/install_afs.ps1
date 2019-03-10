55c3a626099a4e3b93d244782ec028b1332d0ad1
Andre Rummler <andre.rummler@tu-dortmund.de>
2019-03-10 22:24:52 +0100

    CI: ROOT installation

--- appveyor/install_afs.ps1
+++ appveyor/install_afs.ps1
@@ -2,7 +2,8 @@ function main () {
 
     $clnt = new-object System.Net.WebClient; 
 
-    Write-Host("It seems that not all files for afs installation were downloaded properly - trying alternative.");	$clnt.DownloadFile("https://www.auristor.com/downloads/openafs/windows/x64/yfs-openafs-en_US-AMD64-1_7_3301.msi","C:\\yfs-openafs-en_US-AMD64-1_7_3301.msi");
+    Write-Host("Downloading file for afs installation.");	
+    $clnt.DownloadFile("https://www.auristor.com/downloads/openafs/windows/x64/yfs-openafs-en_US-AMD64-1_7_3301.msi","C:\\yfs-openafs-en_US-AMD64-1_7_3301.msi");
     Start-Process "C:\\yfs-openafs-en_US-AMD64-1_7_3301.msi" -ArgumentList "/quiet /qn /norestart /log install.log ADDLOCAL=feaNetIDMgrPlugin,feaClient,feaOpenAFS AcceptLicense=1" -Wait;
     
 }
