287ad2f277421e176d53417223ac13468fdc0245
Lennart Huth <lennart.huth@desy.de>
2019-04-11 11:03:23 +0200

    added dummies for MuPix8 Monitoring"

---
+++ user/MuPix8/module/src/MuPix8RawEvent2StdEventConverter.cc
@@ -0,0 +1,44 @@
+#include "eudaq/StdEventConverter.hh"
+#include "eudaq/RawEvent.hh"
+
+#include "library/telescope_frame.hpp"
+
+class MuPix8RawEvent2StdEventConverter: public eudaq::StdEventConverter{
+public:
+  bool Converting(eudaq::EventSPC d1, eudaq::StdEventSP d2, eudaq::ConfigSPC conf) const override;
+  static const uint32_t m_id_factory = eudaq::cstr2hash("MuPix8Raw");
+};
+
+namespace{
+  auto dummy0 = eudaq::Factory<eudaq::StdEventConverter>::
+    Register<MuPix8RawEvent2StdEventConverter>(MuPix8RawEvent2StdEventConverter::m_id_factory);
+}
+
+bool MuPix8RawEvent2StdEventConverter::Converting(eudaq::EventSPC d1, eudaq::StdEventSP d2, eudaq::ConfigSPC conf) const{
+  auto ev = std::dynamic_pointer_cast<const eudaq::RawEvent>(d1);
+  size_t nblocks= ev->NumBlocks();
+  auto block_n_list = ev->GetBlockNumList();
+  for(auto &block_n: block_n_list){
+    auto block =  ev->GetBlock(block_n);
+    TelescopeFrame * tf = new TelescopeFrame();
+    if(!tf->from_uint8_t(ev->GetBlock(block_n)))
+        EUDAQ_ERROR("Cannot read TelescopeFrame");
+    std::cout << tf->num_hits() << std::endl;
+    if(block.size() < 2)
+      EUDAQ_THROW("Unknown data");
+    uint8_t x_pixel = block[0];
+    uint8_t y_pixel = block[1];
+    std::vector<uint8_t> hit(block.begin()+2, block.end());
+    if(hit.size() != x_pixel*y_pixel)
+      EUDAQ_THROW("Unknown data");
+    eudaq::StandardPlane plane(block_n, "my_MuPix8_plane", "my_MuPix8_plane");
+    plane.SetSizeZS(hit.size(), 1, 0);
+    for(size_t i = 0; i < y_pixel; ++i) {
+      for(size_t n = 0; n < x_pixel; ++n){
+	plane.PushPixel(n, i , hit[n+i*x_pixel]);
+      }
+    }
+    d2->AddPlane(plane);
+  }
+  return true;
+}
