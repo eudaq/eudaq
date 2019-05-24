6669dc9e3166f91dae00fe786ae26b03333a687e
Jan Dreyling-Eschweiler <jan.dreyling-eschweiler@desy.de>
2019-05-24 14:17:00 +0200

    user/tlu: add firmware address table v24; update minimal example scripts

--- user/tlu/misc/aida_tlu/start_eudaq.sh
+++ user/tlu/misc/aida_tlu/start_eudaq.sh
@@ -1,3 +1,18 @@
-# start EUDAQ RunCOntrol and AidaTluProducer locally
+#!/bin/bash
+
+# which subnet
+RCPORT=44000
+LOCAL=127.0.0.1
+AIDALOCAL=192.168.200.1
+
+if [ ! $1 ]; then
+    echo "Please specify local subnet as first argument"
+    echo "e.g. $LOCAL"
+    echo "e.g. $AIDALOCAL"
+    return
+fi
+
+# start EUDAQ RunControl and AidaTluProducer locally
 ../../../../bin/euRun &
-../../../../bin/euCliProducer -n AidaTluProducer -t aida_tlu &
+sleep 1
+../../../../bin/euCliProducer -n AidaTluProducer -t aida_tlu -r tcp://$1:$RCPORT &
