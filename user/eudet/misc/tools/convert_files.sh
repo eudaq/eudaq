63243ec68391f3e5094eb96a96d0a276a02b6c1c
Jan Dreyling-Eschweiler <jan.dreyling-eschweiler@desy.de>
2019-05-24 16:26:41 +0200

    user/eudet: updated scripts

--- user/eudet/misc/tools/convert_files.sh
+++ user/eudet/misc/tools/convert_files.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+# convert all raw files to slcio without date-suffix 
+
 CONVERTER=/opt/eudaq2/bin/euCliConverter 
+
 for i in *.raw; do
     #echo `basename $i .raw`
     echo 'processing' $i
