ce4a89ebb8ea373e8cf0dbcc8f24df2ef4cb6dff
Jan Dreyling-Eschweiler <jan.dreyling-eschweiler@desy.de>
2019-05-24 14:17:00 +0200

    user/tlu: add firmware address table v24; update minimal example scripts

--- user/tlu/misc/aida_tlu/setup.sh
+++ user/tlu/misc/aida_tlu/setup.sh
@@ -1,6 +1,7 @@
-## for AIDA TLU
-# for Ubuntu 18.04
+#!/bin/bash
+
+# Add Cactus library, e.g. needed for Ubuntu 18.04
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/cactus/lib
-# start controlhub for stable AIDA TLU TCP/IP communication
+# start controlhub for AIDA TLU TCP/IP communication
 /opt/cactus/bin/controlhub_start
 /opt/cactus/bin/controlhub_status
