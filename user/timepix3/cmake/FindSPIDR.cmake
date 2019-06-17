4e6957cdc1489a5189690e76ce1d84ed47b37d78
Simon Spannagel <simon.spannagel@cern.ch>
2019-06-17 10:52:18 +0200

    Timepix3: clean up SPIDR discovery and XercesC inclusion

--- user/timepix3/cmake/FindSPIDR.cmake
+++ user/timepix3/cmake/FindSPIDR.cmake
@@ -1,29 +1,15 @@
 # - Try to find SPIDR
+# Once done this will define
+#  SPIDR_FOUND - System has SPIDR
+#  SPIDR_INCLUDE_DIR - The SPIDR main include directory
+#  SPIDR_LIB - The library for SPIDR
 
 MESSAGE(STATUS "Looking for SPIDR...")
 
-file(GLOB_RECURSE spidr_include $ENV{HOME}/SPIDR/software/SpidrTpx3Lib/SpidrController.h $ENV{SPIDRPATH}/software/SpidrTpx3Lib/SpidrController.h)
-if(spidr_include)
-    get_filename_component(spidr_lib_path "${spidr_include}" PATH)
-    get_filename_component(spidr_lib_path "${spidr_lib_path}" PATH)
-    set(SPIDR_ROOT ${spidr_lib_path} )
-    MESSAGE(STATUS "Found SPIDR headers in ${SPIDR_ROOT}")
-else(spidr_include)
-    file(GLOB_RECURSE extern_file ${PROJECT_SOURCE_DIR}/extern/SPIDR/software/trunk/SpidrTpx3Lib/SpidrController.h)
-       if (extern_file)
-           # strip the file and 'include' path away:
-    	   get_filename_component(extern_lib_path "${extern_file}" PATH)
-	   get_filename_component(extern_lib_path "${extern_lib_path}" PATH)
-	   MESSAGE(STATUS "Found SPIDR package in 'extern' subfolder: ${extern_lib_path}")
-    	   set(SPIDR_ROOT ${extern_lib_path})
-       endif(extern_file)
-endif(spidr_include)
+FIND_PATH(SPIDR_INCLUDE_DIR NAMES "SpidrController.h" PATHS "/usr/include" "${SPIDRINCLUDE}" "$ENV{SPIDRPATH}/software/SpidrTpx3Lib")
+FIND_LIBRARY(SPIDR_LIB NAMES "SpidrTpx3Lib" HINTS "/usr/lib" "${SPIDRLIBS}" "$ENV{SPIDRPATH}/software/Release")
 
-if (NOT SPIDR_ROOT)
-   MESSAGE(ERROR " Could not find SPIDR package required by Timepix3 producer. Please copy 'https://svn.cern.ch/reps/SPIDR/software/trunk/SpidrTpx3Lib' either to $HOME/SPIDR or '../extern/SPIDR' directory")
-endif()
+MESSAGE(STATUS "-- SPIDR include at ${SPIDR_INCLUDE_DIR} -- Found")
+MESSAGE(STATUS "-- SPIDR library at ${SPIDR_LIB} -- Found")
 
-set( SPIDR_INCLUDE ${SPIDR_ROOT}/SpidrTpx3Lib )
-set( SPIDR_LIB_PATH ${SPIDR_ROOT}/Release )
-
-mark_as_advanced(SPIDR_LIB SPIDR_ROOT )
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(SPIDR REQUIRED_VARS SPIDR_LIB SPIDR_INCLUDE_DIR FAIL_MESSAGE "Could not find SPIDR, make sure all necessary components are compiled and that the variable SPIDRPATH points to the installation location:\n$ export SPIDRPATH=/your/path/to/SPIDR\n")
