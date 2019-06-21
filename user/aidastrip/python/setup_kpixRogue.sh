967a95c634d52e74f0541d8720372fd6117dd19b
MengqingWu <wumengtsing@gmail.com>
2019-06-21 18:58:13 +0200

    converter update, algorithm roughly setup, needs to work on code

--- user/aidastrip/python/setup_kpixRogue.sh
+++ user/aidastrip/python/setup_kpixRogue.sh
@@ -1,10 +1,22 @@
-#!/usr/bin/env sh                                                                                         
-export KPIX_DIR=/home/lycoris-admin/software/kpixDaq/kpix_20181127/software/
+#!/usr/bin/env sh
 
+if [ -z "$LD_LIBRARY_PATH" ]
+then
+   LD_LIBRARY_PATH=""
+fi
+
+if [ -z "$PYTHONPATH" ]
+then
+   PYTHONPATH=""
+fi
+
+#1 Setup Kpix, Rogue, and Surf DIRs
+export KPIX_DIR=/home/lycoris-admin/software/kpixDaq/kpix/software/
 export ROGUE_DIR=${KPIX_DIR}/rogue/
-
 export SURF_DIR=${KPIX_DIR}/../firmware/submodules/surf/
-       
 
+#2 Setup python path
 export PYTHONPATH=${ROGUE_DIR}/python:${KPIX_DIR}/python:${SURF_DIR}/python:${PYTHONPATH}
+
+#3 Setup library path
 export LD_LIBRARY_PATH=${ROGUE_DIR}/lib:${LD_LIBRARY_PATH}
