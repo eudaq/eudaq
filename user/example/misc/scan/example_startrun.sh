9bb2c13193e0dd20c7afc3ac0c019bc7ef582f54
Lennart Huth <lennart.huth@desy.de>
2019-07-10 10:23:07 +0200

    increased sleeping time in example script and added two eudaq logs

--- user/example/misc/scan/example_startrun.sh
+++ user/example/misc/scan/example_startrun.sh
@@ -2,9 +2,9 @@
 BINPATH=../../../../bin
 #$BINPATH/euRun &
 $BINPATH/euRun -n Ex0RunControl &
-sleep 1
+sleep 2
 $BINPATH/euLog &
-sleep 1
+sleep 2
 $BINPATH/euCliMonitor -n Ex0Monitor -t my_mon & 
 $BINPATH/euCliCollector -n Ex0TgDataCollector -t my_dc &
 $BINPATH/euCliProducer -n Ex0Producer -t my_pd0 &
