eef8a5966a8d500a3efc3e71475941a9710c78cc
Mengqing Wu <mengqing.wu@desy.de>
2019-08-20 12:44:46 +0200

    online monitor prototype out, without clustering because S/N cut can not applied due to missing online Noise calculation

--- user/aidastrip/module/src/kpixRawEvent2StdEventConverter.cc
+++ user/aidastrip/module/src/kpixRawEvent2StdEventConverter.cc
@@ -82,7 +82,7 @@ public:
 private:
 	int getStdPlaneID(uint kpix) const;
 	int getStdKpixID(uint hitX, int planeID) const;
-	std::map<std::pair<int,int>, double> createMap(const char* filename);//~LoCo 05/08
+	int createMap(const char* filename);//~LoCo 05/08
 	//~LoCo 07/08 ConvertADC2fC: called inside parseFrame. 09/08 inside parseSample
 	double ConvertADC2fC( int channelID, int planeID, int hitVal ) const;
 
@@ -94,7 +94,8 @@ private:
 	unordered_map<uint, uint> _lkpix2strip = kpix_left();
 	unordered_map<uint, uint> _rkpix2strip = kpix_right();
 	//~LoCo 05/08: can call map in monitor with .at
-	std::map<std::pair<int,int>, double> Calib_map = createMap("/home/lorenzo/data/real_calib/calib_normalgain.txt");
+	//std::map<std::pair<int,int>, double> m_calib_map = createMap("/home/lorenzo/data/real_calib/calib_normalgain.txt");
+  mutable std::map<std::pair<int,int>, double> m_calib_map;
 
 	mutable bool m_isSelfTrig;
 	mutable eudaq::Database m_sampleDB;
@@ -111,17 +112,22 @@ namespace{
 }
 
 kpixRawEvent2StdEventConverter::kpixRawEvent2StdEventConverter() {
+	
+	createMap("/opt/data/calib_normalgain.csv");
 
-	TTree* pedestal_tree = nullptr;
+		
+	TTree* pedestal_tree;
 	double pedestal_median=0, pedestal_MAD=0;
 	int kpix_num=-1, channel_num=-1, bucket_num=-1 ;
 	
 	TH1::AddDirectory(kFALSE);
 
-	std::string pedfile_name = "/opt/data/eudaq2-dev/Run_20190802_115859.dat.tree_pedestal.root";
+	
+	std::string pedfile_name ="/opt/data/eudaq2-dev/Run_20190802_121655.dat.tree_pedestal.root";
+	  //"/opt/data/eudaq2-dev/Run_20190802_115859.dat.tree_pedestal.root";
 	std::string pedtree_name = "pedestal_tree";
 	TFile* rFile = new TFile(pedfile_name.c_str(),"read");
-	
+
 	//~MQ: check if there is a tree inside the rfile, if not create:
 	if ( rFile->Get(pedtree_name.c_str()) ) {
 		rFile->GetObject(pedtree_name.c_str(),  pedestal_tree);
@@ -136,7 +142,9 @@ kpixRawEvent2StdEventConverter::kpixRawEvent2StdEventConverter() {
 		EUDAQ_ERROR("your pedestal tree file is not valid, check it!");
 	}
 
+			
 	for(Long64_t entry = 0; entry< pedestal_tree->GetEntries(); entry++){
+
 		pedestal_tree->GetEntry(entry);
 		if (bucket_num != 0 ) continue;
 		if (pedestal_MAD == 0 ) continue;
@@ -151,9 +159,8 @@ kpixRawEvent2StdEventConverter::kpixRawEvent2StdEventConverter() {
 	}
 
 	rFile->Close();
-	delete pedestal_tree;
-	delete rFile;
 
+	delete rFile;
 }
 
 kpixRawEvent2StdEventConverter::~kpixRawEvent2StdEventConverter(){}
@@ -170,7 +177,7 @@ bool kpixRawEvent2StdEventConverter::Converting(eudaq::EventSPC d1, eudaq::StdEv
 	
 	// TODO: currently is hard coded to change trigger mode
 	//string triggermode = d1->GetTag("triggermode", "internal");
-    string triggermode = "external";
+	string triggermode = "external";
 	m_isSelfTrig = triggermode == "internal" ? true:false ;
 	
 	if (m_isSelfTrig)
@@ -234,6 +241,7 @@ void kpixRawEvent2StdEventConverter::parseFrame(eudaq::StdEventSP d2, KpixEvent 
 	uint         is;           // global for loop int
 	double       bunchClk;
 	uint         subCount;
+	bool         emptyframe=true;
 
 	if (m_isSelfTrig){
 		for (is = 0; is < cycle.count(); is++){
@@ -254,19 +262,22 @@ void kpixRawEvent2StdEventConverter::parseFrame(eudaq::StdEventSP d2, KpixEvent 
 	 * 1) ext trig:  fill the sample map
 	 * 2) self trig: push pixel here
 	 */
-	printf("[dev] kpix ID,  channel, strip,  bucket,   ADC\n");
+	printf("[dev] kpix ID,  channel, strip,  bucket,   ADC,   charge\n");
+	std::cout << "num of samples? " << cycle.count() << std::endl;
 	for (is=0; is<cycle.count(); is++){
-		
-		sample = cycle.sample(is);
-		if (sample ->getSampleType() != KpixSample::Data || sample ->getKpixBucket() !=0 ) continue;  // ignore non-DATA sample, ignore bucket != 0 sample
 
+		sample = cycle.sample(is);
+		
+		if (sample ->getSampleType() != KpixSample::Data ) continue;  // ignore non-DATA sample, ignore bucket != 0 sample
+		if ( sample ->getKpixBucket() !=0) continue;
+
+		
+		
 		// cut out not wanted samples
 		auto hit = parseSample(sample);
 		if ( hit.kpix == -1 ) continue ; // ignore empty sample or bad self-trig sample;
-		
-		
 		if (m_isSelfTrig){
-			
+
 			auto planeID = getStdPlaneID(hit.kpix);
 			auto hitX    = hit.strip;
 			
@@ -287,11 +298,12 @@ void kpixRawEvent2StdEventConverter::parseFrame(eudaq::StdEventSP d2, KpixEvent 
 			                );    // use bucket as input for frame_num
 		}
 		else {
+			emptyframe = false;
+			
 			// fill charge to m_sampleDB with pedestal median subtracted charge 
 			if ( m_sampleDB.count( std::make_pair(hit.kpix, hit.channel) ) ){
 				
 				double charge_corr_ped = hit.charge - m_sampleDB[std::make_pair(hit.kpix, hit.channel)].median;
-					
 				m_sampleDB[std::make_pair(hit.kpix, hit.channel)].charge = charge_corr_ped;
 				m_KpixChargeList_perCycle[hit.kpix].push_back(charge_corr_ped);
 			}
@@ -299,24 +311,47 @@ void kpixRawEvent2StdEventConverter::parseFrame(eudaq::StdEventSP d2, KpixEvent 
 		
 	}// - sample loop over
 
-	if (!m_isSelfTrig){
+	if ( (!m_isSelfTrig) && (!emptyframe)   ){
+		
 		// find common mode median for each kpix and remove it
 		// add up all the corrected charge for each kpix
 		for (is = 0; is<_numofkpix; is++){
-			
-			m_common_mode_perCycle[is] = median(m_KpixChargeList_perCycle[is]);
-			
+		  m_common_mode_perCycle.push_back( median(&m_KpixChargeList_perCycle[is]) );
 		}
+		for (is = 0; is<m_common_mode_perCycle.size(); is++){
+		  cout << "kpix - " << is 
+		       << ", CM noise - " << m_common_mode_perCycle.at(is)
+		       <<endl;
+		}
+		
 		// 2nd loop over all samples: correct charge from common mode noise
 		for (auto && entry : m_sampleDB){
+			
 			int kpix = entry.first.first;
-			double charge_corr_CM = entry.second.charge - m_common_mode_perCycle[kpix];
+			auto planeID = getStdPlaneID(kpix);
+			
+			int channel = entry.first.second;
+			int strip = 9999;
+			if (kpix%2 == 0) strip = _lkpix2strip.at(channel);
+			else strip = _rkpix2strip.at(channel);
+			
+			if (strip == 9999 ) continue; // ignore non-connected strips
+			else strip = RotateStrip(strip, planeID);
+			
+			//~MQ: correct common mode noise
+			double charge_corr_CM = entry.second.charge - m_common_mode_perCycle.at(kpix);
 			// TODO: plot  with cluster_analysis.cxx output :
 			// - noise
 			// - charge_corr_CM
-
+			// temperarily ignore S/N but use a harsh charge level cut
 			
-			
+			if (charge_corr_CM > 1.5 ){
+			//if (true){
+			  std::cout << "[+] plane : "<< planeID << ", hitX at : " << strip << std::endl;
+			  auto &plane = d2->GetPlane( getStdPlaneID(kpix) );
+			  plane.PushPixel(strip , 1, 1);
+			}
+			                        
 		}
 		
 		
@@ -324,11 +359,11 @@ void kpixRawEvent2StdEventConverter::parseFrame(eudaq::StdEventSP d2, KpixEvent 
 
 	}
 	
-	
 	// debug:
-	auto &plane = d2->GetPlane(0);
-	std::cout << "debug: Num of Hit pixels: " << plane.HitPixels() << std::endl;
-	
+	for (is = 0; is < 6;  is++){
+	  auto &plane = d2->GetPlane(is);
+	  std::cout << "debug: Num of Hit pixels: " << plane.HitPixels() << std::endl;
+	}
 	return;
 }
 	
@@ -343,26 +378,21 @@ eudaq::Hit kpixRawEvent2StdEventConverter::parseSample(KpixSample* sample) const
 	  - decode the 2 * 32bit kpix [sample]
 	  - return tuple with positive kpix id, otherwise kpix id is negative
 	*/
-	uint         kpix;
-	bool         badflag;
-	uint         channel;
+	bool         badflag=false;
 	int          strip = 9999;
-	uint         bucket;
-	uint         value;
-	double       tstamp;
 	double       hitCharge=-1;//~LoCo: to get ADC charge in fC
 	
-	kpix    = sample->getKpixAddress();
-	channel = sample->getKpixChannel();
-	value   = sample->getSampleValue();
-	tstamp  = sample->getSampleTime();
-	
+	uint kpix    = sample->getKpixAddress();
+	uint channel = sample->getKpixChannel();
+	uint value   = sample->getSampleValue();
+	uint tstamp  = sample->getSampleTime();
+	uint bucket  = sample->getKpixBucket();
 
 	if (sample->getSampleType() != KpixSample::Data)
 		throw "Debug: you have non DATA kpix sample leaked in parseSample() check your code!" ;
 	
 
-	if (sample->getKpixBucket() != 0 )
+	if (bucket != 0 )
 		throw "Debug: you have non bucket0 sample leaked in parseSample() check your code!";
 	
 	
@@ -380,24 +410,27 @@ eudaq::Hit kpixRawEvent2StdEventConverter::parseSample(KpixSample* sample) const
 		badflag = true;
 	}
 	
-	if (strip != 9999)
-		std::cout<<"\t"
-		         << kpix     << "   "
-		         << channel  << "   "
-		         << strip    << "   "
-		         << bucket   << "   "
-		         << value    << "   " 
-		         << std::endl;
-	
-	
 	//~LoCo: to return fC value
 	hitCharge = ConvertADC2fC(channel, kpix, value);
+	if (strip != 9999)
+	  std::cout<<"\t"
+		   << kpix      << "   "
+		   << channel   << "   "
+		   << strip     << "   "
+		   << bucket    << "   "
+		   << value     << "   "
+		   << hitCharge << "   "
+		   << std::endl;
 
+	
 	eudaq::Hit hit;
-	hit.kpix =  badflag ?  -1 :kpix;
+	hit.kpix =  kpix;
 	hit.channel = channel;
 	hit.strip = strip;
 	hit.charge = hitCharge;
+	
+	if (badflag)
+		hit.kpix = -1;
 
 	return hit;
 }
@@ -411,74 +444,70 @@ int kpixRawEvent2StdEventConverter::getStdPlaneID(uint kpix) const{
   if ( kpix == 2  | kpix == 3  )  return 1; 
   if ( kpix == 4  | kpix == 5  )  return 2;
   
-  if ( kpix == 6  | kpix == 7  )  return 5;
+  if ( kpix == 6  | kpix == 7  )  return 3;
   if ( kpix == 8  | kpix == 9  )  return 4;
-  if ( kpix == 10 | kpix == 11 )  return 3; 
+  if ( kpix == 10 | kpix == 11 )  return 5; 
 }
 
 // ~CREATEMAP~
 
+
 //~LoCo 05/08. File format: kpix\t channel \t bucket=0\t slope
-std::map<std::pair<int,int>, double> kpixRawEvent2StdEventConverter::createMap(const char* filename){//~LoCo 02/08: Lookup table
-	
-	std::ifstream myfile_calib;
-	myfile_calib.open(filename);
-	std::map<std::pair<int,int>, double> Calib_map;
-	
-	if (! myfile_calib) return Calib_map;  
-	
-	int map_a=-1, map_c, map_tmp;
-	uint map_b;
-	int map_check1=0, map_check2=0;
-	double map_d;
-	
-	//Create Map. ~LoCo 07/08: key is <kpix,channel ID>, value is slope
-	while ( true ) {
-		
-		map_tmp=map_a;
-		myfile_calib >> map_a;//this is kpix ID
-		
-		if( myfile_calib.eof() ) break;
-		
-		if ( map_a != map_tmp) map_check1++;
-		
-		myfile_calib >> map_b;//this is strip ID, goes from 0 to 919 (left) or 920 to 1839 (right). 9999 doesn't matter at all!
-		myfile_calib >> map_c;//this is bucket, has to be 0
-		myfile_calib >> map_d;//this is slope
-		
-		if ( map_b == 1023 ) map_check2++;
-		
-		if( myfile_calib.eof() ) break;
-		
-		if ( map_c != 0 ) continue;
-		
-		//Finally can create map.
-		
-		Calib_map[std::make_pair(map_a, map_b)] = map_d;
-		
-	}
-	
-	myfile_calib.close();
-	
-	std::cout<<"TEST DEBUGGING OUTPUT CALIBMAP" << std::endl;
-	
-	return Calib_map;
+int kpixRawEvent2StdEventConverter::createMap(const char* filename){//~LoCo 02/08: Lookup table
+  
+  std::ofstream output("mapfile.csv");
+  std::ifstream myfile_calib(filename);
+  if (!myfile_calib.is_open()){
+    std::cout << "CreateMap has a wrong file! check your code." << std::endl;
+    return 0;
+  }
+  
+  std::string line;
+  while( std::getline(myfile_calib, line) ){
+    std::stringstream liness(line);
+    std::string cell;
+    std::vector<std::string> vec;
+    while( std::getline(liness, cell, ',') ){
+      if ( !isdigit(cell[0]) ) continue; // title string
+      output<<cell<< ',';
+      vec.push_back( cell );
+    }
+    output<<'\n';
+    if (vec.size() < 4){
+      // printf("weird size of vec: ");
+      // for (const auto & i : vec)
+      // 	printf("%s, ", i.c_str());
+      // printf("\n");
+      continue;
+    }
+    else
+      m_calib_map[ std::make_pair(::atoi(vec[0].c_str()),
+				  ::atoi(vec[1].c_str()))] = ::atof(vec[3].c_str());
+  }
+  
+  myfile_calib.close();
+  output<< "finished\n";
+  output.close();
+  std::cout<<"TEST DEBUGGING OUTPUT CALIBMAP" << std::endl;
+  
+  return 1;
 }
 
-// ~CONVERTADC2FC~
 
 //~LoCo 02/08: convert hitVal (ADC) to fC with Lookup table. 07/08: function to pass array with channel values
 //~LoCo 09/08 REWRITTEN: works with channel, not strip
 double kpixRawEvent2StdEventConverter::ConvertADC2fC( int channelID, int kpixID, int hitVal ) const{
 
 	double hitCharge=-1;
+	if (! m_calib_map.count(std::make_pair(kpixID, channelID)) ) return 0;
 	
-	if ( Calib_map.at(std::make_pair(kpixID,channelID)) > 1.0 ) {
+	if ( m_calib_map.at(std::make_pair(kpixID,channelID)) > 0.0 ) {
 		
-		hitCharge = hitVal/Calib_map.at(std::make_pair(kpixID,channelID));
+		hitCharge = hitVal/m_calib_map.at(std::make_pair(kpixID,channelID));
 	}
 	else {
-		std::cout << "!!NOTICE: channel excluded from conversion" << std::endl;
+		std::cout << kpixID <<", " << channelID 
+			<< "bad slope => "<< m_calib_map.at(std::make_pair(kpixID,channelID)) << std::endl;
 	}
 	
 	return hitCharge;
