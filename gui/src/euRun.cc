2e1d5c62d2b2b410f23a3979ea1d060a85063b5b
Paul Schuetze <paul.schuetze@desy.de>
2020-06-03 14:45:00 +0200

    Scan: When starting the next run in a scan, ensure that everyone is running before checking for the stop condition. Suggested by L. Huth

--- gui/src/euRun.cc
+++ gui/src/euRun.cc
@@ -651,6 +651,15 @@ void RunControlGUI::nextStep()
 
         std::this_thread::sleep_for(std::chrono::seconds(3));
         on_btnStart_clicked();
+
+        while(!allConnectionsInState(eudaq::Status::STATE_RUNNING)){
+            updateInfos();
+            QCoreApplication::processEvents();
+            std::this_thread::sleep_for (std::chrono::seconds(1));
+            std::cout << "Waiting until all components are running"<<std::endl;
+        }
+        std::this_thread::sleep_for (std::chrono::seconds(1));
+
         if(m_scan.scanIsTimeBased())
         {
             m_scanningTimer.start(1000*m_scan.timePerStep());
