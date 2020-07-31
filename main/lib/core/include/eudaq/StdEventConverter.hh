5a584f53a74ace5eb05f5c2e5bdc773ecc1e4aa7
Simon Spannagel <simon.spannagel@cern.ch>
2020-07-31 15:41:07 +0200

    StdEventConverter: add exceptions to communicate with calling code

--- main/lib/core/include/eudaq/StdEventConverter.hh
+++ main/lib/core/include/eudaq/StdEventConverter.hh
@@ -1,4 +1,4 @@
-#ifndef STDEVENTCONVERTER_HH_ 
+#ifndef STDEVENTCONVERTER_HH_
 #define STDEVENTCONVERTER_HH_
 
 #include "eudaq/Platform.hh"
@@ -8,14 +8,30 @@
 
 namespace eudaq{
   class StdEventConverter;
-#ifndef EUDAQ_CORE_EXPORTS  
+#ifndef EUDAQ_CORE_EXPORTS
   extern template class DLLEXPORT Factory<StdEventConverter>;
   extern template DLLEXPORT
   std::map<uint32_t, typename Factory<StdEventConverter>::UP(*)()>&
   Factory<StdEventConverter>::Instance<>();
 #endif
   using StdEventConverterUP = Factory<StdEventConverter>::UP;
-  
+
+  // Exceptions to communicate information from conversion process:
+  class DecodingIssue : public Exception {
+  public:
+    DecodingIssue(const std::string& msg) : Exception(msg) {};
+  };
+
+  class DataInvalid : public DecodingIssue {
+  public:
+    DataInvalid(const std::string& msg) : DecodingIssue(msg) {};
+  };
+
+  class DataDiscarded : public DecodingIssue {
+  public:
+    DataDiscarded(const std::string& msg) : DecodingIssue(msg) {};
+  };
+
   class DLLEXPORT StdEventConverter:public DataConverter<Event, StandardEvent>{
   public:
     StdEventConverter() = default;
