bf957ce4afae1fa54e865290f6815564e4413b91
Magnus Mager <Magnus.Mager@cern.ch>
2021-09-20 20:35:36 +0200

    Fixed race time condition

--- main/lib/core/src/RunControl.cc
+++ main/lib/core/src/RunControl.cc
@@ -350,12 +350,21 @@ namespace eudaq {
       }
     }
 
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    for(auto &conn :conn_to_stop){
-      if(conn->GetType() == "DataCollector"){
-	SendCommand("STOP", "", conn);
+    for(auto &conn :conn_to_stop)
+      if(conn->GetType() == "Producer" &&
+	 conn->GetName() != producer_first_stop){
+        while(1){
+          auto st =  GetConnectionStatus(conn)->GetState();
+          if (st != Status::STATE_RUNNING) break;
+          if(std::chrono::steady_clock::now() > tp_timeout) 
+            EUDAQ_ERROR("Timesout waiting stopping status from "+ conn->GetName());
+          std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
       }
-    }
+
+    for(auto &conn :conn_to_stop)
+      if(conn->GetType() == "DataCollector")
+	SendCommand("STOP", "", conn);
 
     //TODO: make sure datacollector is stoped after producer. waiting. check status
     std::this_thread::sleep_for(std::chrono::seconds(1));
