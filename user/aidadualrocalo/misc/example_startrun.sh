028bd10be711f002bd34d1557937eb2f607dd84c
s6anloes <a.loeschcke@gmail.com>
2022-07-27 09:10:28 +0200

    Updated example_startrun.sh

--- user/aidadualrocalo/misc/example_startrun.sh
+++ user/aidadualrocalo/misc/example_startrun.sh
@@ -5,5 +5,6 @@ $BINPATH/euRun &
 sleep 1
 $BINPATH/euLog &
 sleep 1
+#$BINPATH/euCliMonitor -n Ex0Monitor -t my_mon & 
 $BINPATH/euCliCollector -n DualROCaloDataCollector -t my_dc &
 $BINPATH/euCliProducer -n DualROCaloProducer -t my_pd0 &
