c2b9215e38964167d9911e5777eb4ec128b5220c
Miljenko Suljic <miljenko.suljic@cern.ch>
2022-08-14 22:25:40 +0200

    Logging improvement in example python producer & DC

--- user/example/python/ExamplePyProducer.py
+++ user/example/python/ExamplePyProducer.py
@@ -1,36 +1,52 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 # load binary lib/pyeudaq.so
 import pyeudaq
+from pyeudaq import EUDAQ_INFO, EUDAQ_ERROR
 import time
+
+def exception_handler(method):
+    def inner(*args, **kwargs):
+        try:
+            return method(*args, **kwargs)
+        except Exception as e:
+            EUDAQ_ERROR(str(e))
+            raise e
+    return inner
 
 class ExamplePyProducer(pyeudaq.Producer):
     def __init__(self, name, runctrl):
         pyeudaq.Producer.__init__(self, 'PyProducer', name, runctrl)
         self.is_running = 0
-        print ('New instance of ExamplePyProducer')
+        EUDAQ_INFO('New instance of ExamplePyProducer')
 
+    @exception_handler
     def DoInitialise(self):        
-        print ('DoInitialise')
+        EUDAQ_INFO('DoInitialise')
         #print 'key_a(init) = ', self.GetInitItem("key_a")
 
+    @exception_handler
     def DoConfigure(self):        
-        print ('DoConfigure')
+        EUDAQ_INFO('DoConfigure')
         #print 'key_b(conf) = ', self.GetConfigItem("key_b")
 
+    @exception_handler
     def DoStartRun(self):
-        print ('DoStartRun')
+        EUDAQ_INFO('DoStartRun')
         self.is_running = 1
         
+    @exception_handler
     def DoStopRun(self):        
-        print ('DoStopRun')
+        EUDAQ_INFO('DoStopRun')
         self.is_running = 0
 
+    @exception_handler
     def DoReset(self):        
-        print ('DoReset')
+        EUDAQ_INFO('DoReset')
         self.is_running = 0
 
+    @exception_handler
     def RunLoop(self):
-        print ("Start of RunLoop in ExamplePyProducer")
+        EUDAQ_INFO("Start of RunLoop in ExamplePyProducer")
         trigger_n = 0;
         while(self.is_running):
             ev = pyeudaq.Event("RawEvent", "sub_name")
@@ -47,7 +63,7 @@ class ExamplePyProducer(pyeudaq.Producer):
             self.SendEvent(ev)
             trigger_n += 1
             time.sleep(1)
-        print ("End of RunLoop in ExamplePyProducer")
+        EUDAQ_INFO("End of RunLoop in ExamplePyProducer")
 
 if __name__ == "__main__":
     myproducer = ExamplePyProducer("myproducer", "tcp://localhost:44000")
