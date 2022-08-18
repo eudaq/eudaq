df104abb63c148e868d5b4d703889632ec3ea54f
s6anloes <a.loeschcke@gmail.com>
2022-08-18 10:01:29 +0100

    Used eudet telescope converter as template

--- user/aidadualrocalo/module/src/DualROCaloRawEvent2StdEventConverter.cc
+++ user/aidadualrocalo/module/src/DualROCaloRawEvent2StdEventConverter.cc
@@ -1,6 +1,7 @@
 #include "eudaq/StdEventConverter.hh"
 #include "eudaq/RawEvent.hh"
 
+/*
 class DualROCaloRawEvent2StdEventConverter: public eudaq::StdEventConverter{
 public:
   bool Converting(eudaq::EventSPC d1, eudaq::StdEventSP d2, eudaq::ConfigSPC conf) const override;
@@ -50,7 +51,7 @@ bool DualROCaloRawEvent2StdEventConverter::Converting(eudaq::EventSPC d1, eudaq:
     std::cout<<"Converting:: Accessing new block!" << std::endl;
     if(event_size != block.size())
       EUDAQ_THROW("Unknown data");
-    */
+    * /
     uint8_t boardID = uint8_t(block[2]);
     //std::cout<<"Converting:: Accessing new block!" << std::endl;
     //std::cout<<"Converting:: block size is = " << std::to_string(block.size()) << std::endl;
@@ -74,7 +75,7 @@ bool DualROCaloRawEvent2StdEventConverter::Converting(eudaq::EventSPC d1, eudaq:
       channel_mask_bits[b] = ((1 << (b % 8)) & (channel_mask[b/8])) >> (b % 8);
       if(channel_mask_bits[b] == 1) num_active_channels++;
     }
-    */
+    * /
     //std::cout<<"Converting:: Decoded new block!" << std::endl;
 
     std::vector<uint8_t> hits(block.begin(), block.end());
@@ -85,7 +86,7 @@ bool DualROCaloRawEvent2StdEventConverter::Converting(eudaq::EventSPC d1, eudaq:
     uint16_t expected_size = num_active_channels*data_size;
     if(hits.size() != expected_size)
       EUDAQ_THROW("Unknown data");
-      */
+      * /
 
     //std::cout<<"Converting:: Creating Planes!" << std::endl;
     eudaq::StandardPlane plane_lg(block_n, "DualROCalo", "DualROCalo");
@@ -123,3 +124,91 @@ bool DualROCaloRawEvent2StdEventConverter::Converting(eudaq::EventSPC d1, eudaq:
   
   return true;
 }
+*/
+
+class DualROCaloRawEvent2StdEventConverter: public eudaq::StdEventConverter{
+  typedef std::vector<uint8_t>::const_iterator datait;
+public:
+  bool Converting(eudaq::EventSPC d1, eudaq::StandardEventSP d2, eudaq::ConfigurationSPC conf) const override;
+  static const uint32_t m_id_factory = eudaq::cstr2hash("DualROCaloEvent");
+};
+
+namespace{
+  auto dummy0 = eudaq::Factory<eudaq::StdEventConverter>::
+    Register<DualROCaloRawEvent2StdEventConverter>(DualROCaloRawEvent2StdEventConverter::m_id_factory);
+}
+
+bool DualROCaloRawEvent2StdEventConverter::Converting(eudaq::EventSPC d1, eudaq::StandardEventSP d2, eudaq::ConfigurationSPC conf) const{
+
+  //TODO: number of telescope plane may be less than 6. Decode additional tags
+  auto ev = std::dynamic_pointer_cast<const eudaq::RawEvent>(d1);
+  if(!ev)
+    return false;
+
+    // Identify the detetor type
+  d2->SetDetectorType("DualROCalo");
+
+
+  //d2->SetTimeBegin(d1->GetTimestampBegin());
+  //d2->SetTimeEnd(d1->GetTimestampEnd());
+
+  if(!d2->IsFlagPacket()){
+    d2->SetFlag(d1->GetFlag());
+    d2->SetRunN(d1->GetRunN());
+    d2->SetEventN(d1->GetEventN());
+    d2->SetStreamN(d1->GetStreamN());
+    d2->SetTriggerN(d1->GetTriggerN(), d1->IsFlagTrigger());
+    d2->SetTimestamp(d1->GetTimestampBegin(), d1->GetTimestampEnd(), d1->IsFlagTimestamp());
+  }
+
+  auto &rawev = *ev;
+  /*if (rawev.NumBlocks() < 2 || rawev.GetBlock(0).size() < 20 ||
+      rawev.GetBlock(1).size() < 20) {
+    EUDAQ_WARN("Ignoring bad event " + std::to_string(rawev.GetEventNumber()));
+    return false;
+  }*/
+  //auto use_all_hits = (conf != nullptr ? bool(conf->Get("use_all_hits",0)) : false);
+
+  const std::vector<uint8_t> &data0 = rawev.GetBlock(0);
+  //uint32_t header0 = eudaq::getlittleendian<uint32_t>(&data0[0]);
+  //uint32_t header1 = eudaq::getlittleendian<uint32_t>(&data1[0]);
+  //uint16_t pivot = eudaq::getlittleendian<uint16_t>(&data0[4]);
+  //uint16_t tluid = eudaq::getlittleendian<uint16_t>(&data0[6]);
+  datait it0 = data0.begin();
+  //uint32_t board = 0;
+  eudaq::StandardPlane plane(0, "Board", "DualROCalo");
+  plane.SetSizeZS(8, 8, 0);
+
+  while (it0 < data0.end()) {
+    
+    uint8_t channel_id = eudaq::getlittleendian<uint8_t>(&(*(it0)));
+    uint16_t lg_adc_value = eudaq::getlittleendian<uint16_t>(&(*(it0+2)));
+    uint16_t hg_adc_value = eudaq::getlittleendian<uint16_t>(&(*(it0+4)));
+
+    uint8_t n = channel_id;
+    uint16_t x = n % 8;
+    uint16_t y = (uint16_t) n/8;
+
+    //std::cout << "Converting:: lg value = " << std::to_string(lg_adc_value) << std::endl;
+    std::cout<<"Converting:: n = " << std::to_string(n)<<" x = " << std::to_string(x)<<" y = " << std::to_string(y) << " adc = " << std::to_string(lg_adc_value) <<  std::endl;
+
+
+    plane.PushPixel(x, y, lg_adc_value);
+
+    it0 += 6;
+    
+    //plane.SetPivotPixel((9216 + pivot + PIVOTPIXELOFFSET) % 9216);
+    //DecodeFrame(plane, 0, &it0[8], len0, use_all_hits);
+    //DecodeFrame(plane, 1, &it1[8], len1, use_all_hits);
+    
+
+
+  }
+
+  d2->AddPlane(plane);
+  std::cout << "Added Plane!" << std::endl;
+  //eudaq::mSleep(1000);
+
+  return true;
+}
+
