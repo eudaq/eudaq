710cf5fbfe03b5a7511f0c88dac15596ecaea91d
dinardo <mauro.dinardo@cern.ch>
2023-04-28 23:14:42 +0200

    Fixed bug in planeID for quad - to be tested

--- main/lib/core/src/StandardEvent.cc
+++ main/lib/core/src/StandardEvent.cc
@@ -49,10 +49,10 @@ namespace eudaq {
 
   size_t StandardEvent::NumPlanes() const { return m_planes.size(); }
 
-  StandardPlane &StandardEvent::GetPlane(size_t i) { return m_planes.at(i); }
+  StandardPlane &StandardEvent::GetPlane(size_t i) { return m_planes[i]; }
 
   const StandardPlane &StandardEvent::GetPlane(size_t i) const {
-    return m_planes.at(i);
+    return m_planes[i];
   }
 
   uint64_t StandardEvent::GetTimeBegin() const {
