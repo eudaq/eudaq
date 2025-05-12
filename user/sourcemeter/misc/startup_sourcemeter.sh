0a6e9d3aef6ad9cca7eb3b606dd919ccf2669a24
Annika Vauth <annika.vauth@desy.de>
2025-05-12 19:16:51 +0200

    update example config to contain more of the parameters

--- user/sourcemeter/misc/startup_sourcemeter.sh
+++ user/sourcemeter/misc/startup_sourcemeter.sh
@@ -1,7 +1,11 @@
 # Define port
 RPCPORT=44000
-RUNCONTROLIP=192.168.24.1
+RUNCONTROLIP=127.0.0.1 
+#192.168.24.1
 
-# start the python procuder 
-xterm -T "sourcemeter Producer" -e 'python ${EUDAQDIR}/user/sourcemeter/python SourcemeterPyProducer.py-r tcp://${RUNCONTROLIP}:${RPCPORT}' &
+# EUDAQDIR needs to be set independently. Then, add it to pythonpath if it's not in there already
+test "${PYTHONPATH#*"${EUDAQDIR}/lib"}" != "$PYTHONPATH" || export PYTHONPATH="${EUDAQDIR}/lib:$PYTHONPATH"
+
+# start the python procuder
+python ${EUDAQDIR}/user/sourcemeter/python/SourcemeterPyProducer.py -r tcp://${RUNCONTROLIP}:${RPCPORT}
 
