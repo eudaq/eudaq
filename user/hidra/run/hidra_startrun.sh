3a5b170d760417ed5ed88f2626f36d052f141043
DAQ for HIDRA <eudaq@briseide.pv.infn.it>
2026-04-20 17:23:19 +0200

    Implemented Log Collector

--- user/hidra/run/hidra_startrun.sh
+++ user/hidra/run/hidra_startrun.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 BINPATH=../../../bin
 $BINPATH/euRun -n HidraRunControl &
- 
+sleep 1
+$BINPATH/euLog &
 sleep 1
 $BINPATH/euCliCollector -n HidraDataCollector -t my_dc &
 sleep 1
